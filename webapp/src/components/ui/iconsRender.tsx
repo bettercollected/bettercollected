b4a439854a1805d74f8842516a9b3c683a647305
rupan.chaulagain <rupanchaulagain.rc@gmail.com>
2022-10-13 06:33:05 +0545

    added social media icons to the login page

---
+++ webapp/src/components/ui/iconsRender.tsx
@@ -0,0 +1,26 @@
+import GoogleIcon from "@mui/icons-material/Google";
+
+/**
+ * Created By: Rupan Chaulagain
+ * Date: 2022-10-13
+ * Time: 06:19
+ * Project: formintegratorwebapp
+ * Organization: Sireto Technology
+ */
+
+export default function IconsRender({ IconsArray }: any) {
+  return (
+    <div className={"flex gap-1"}>
+      {IconsArray.map((icon: any, index: number) => (
+        <div
+          key={index}
+          className={
+            "p-2 border-gray-100 rounded-lg rounded-tr-none rounded-bl-none border-solid  border-[2px]"
+          }
+        >
+          {icon.Icon}
+        </div>
+      ))}
+    </div>
+  );
+}
