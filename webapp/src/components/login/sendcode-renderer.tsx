5423e10eb41825b7b9d2b32379986ab650d1f752
Ankit Sapkota <ankit.sapkota555@gmail.com>
2022-12-15 12:45:26 +0545

    Google login WIP

--- webapp/src/components/login/sendcode-renderer.tsx
+++ webapp/src/components/login/sendcode-renderer.tsx
@@ -1,16 +1,20 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 import FormInput from '../ui/FormInput';
 import Button from '../ui/button';
+import {useRouter} from "next/router";
+import environments from "@app/configs/environments";
+import {Google} from "@app/components/icons/brands/google";
 
-export default function SendCode({ updateEmail, isLoading, postSendOtp }: any) {
+export default function SendCode({updateEmail, isLoading, postSendOtp}: any) {
     const [emailInput, setEmailInput] = useState('');
 
     const [emailValid, setEmailValid] = useState(false);
 
+    const router = useRouter()
     const handleClick = (e: any) => {
         e.preventDefault();
-        const email = { receiver_email: emailInput };
+        const email = {receiver_email: emailInput};
         updateEmail(emailInput);
         postSendOtp(email);
     };
@@ -23,15 +27,31 @@ export default function SendCode({ updateEmail, isLoading, postSendOtp }: any) {
         setEmailInput(e.target.value);
     };
 
+    const handleGoogleOauthLogin = (e: any) => {
+        router.push(`${environments.API_ENDPOINT_HOST}/auth/google/basicAuth`)
+    }
     return (
         <div className="flex flex-col justify-center">
             <form onSubmit={handleClick}>
                 <h2 className="text-lg font-bold text-center">Enter your Gmail</h2>
                 <p className="text-gray-400 text-sm mb-4">An OTP code will be sent to your gmail account</p>
-                <FormInput inputFieldType="email" value={emailInput} placeholder={'Enter your email'} onChange={handleChangeOnInput} handleValidation={handleValidation} />
-                <Button type="submit" disabled={!emailValid} isLoading={isLoading} variant="solid" className={`mt-2 w-full mb-0`} onClick={handleClick}>
+                <FormInput inputFieldType="email" value={emailInput} placeholder={'Enter your email'}
+                           onChange={handleChangeOnInput} handleValidation={handleValidation}/>
+                <Button type="submit" disabled={!emailValid} isLoading={isLoading} variant="solid"
+                        className={`mt-2 w-full mb-0`} onClick={handleClick}>
                     Get in
                 </Button>
+                <div className="flex py-5 items-center justify-center">
+                    <div className="border-t w-5 border-gray-400"></div>
+                    <span className="flex-shrink mx-4 text-gray-400">or</span>
+                    <div className="border-t w-5 border-gray-400"></div>
+                </div>
+                <button type="submit" className="bg-[#1a73e8]" onClick={handleGoogleOauthLogin}>
+                    <div className="rounded-full bg-white">
+                        <Google/>
+                    </div>
+
+                </button>
             </form>
         </div>
     );
