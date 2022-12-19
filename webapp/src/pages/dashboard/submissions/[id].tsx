0befe8cbb6ceaa75afffbe3d30d3782efb78e738
rupan.chaulagain <rupanchaulagain.rc@gmail.com>
2022-12-19 19:30:35 +0545

    fixed path for breadcrumbs on dashboard submissions

--- webapp/src/pages/dashboard/submissions/[id].tsx
+++ webapp/src/pages/dashboard/submissions/[id].tsx
@@ -252,8 +252,7 @@ export default function Submission({ form, workspace, submissionId, ...props }: 
         router
             .push(
                 {
-                    pathname: '/',
-                    query: { view: 'mySubmissions' }
+                    pathname: '/dashboard/submissions'
                 },
                 undefined,
                 { scroll: true, shallow: true }
@@ -268,7 +267,7 @@ export default function Submission({ form, workspace, submissionId, ...props }: 
                 <nav className="flex mt-3 px-6 md:px-0" aria-label="Breadcrumb">
                     <ol className="inline-flex items-center space-x-1 md:space-x-3">
                         <li className="inline-flex items-center">
-                            <span aria-hidden onClick={() => router.push('/')} className="cursor-pointer inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                            <span aria-hidden onClick={() => router.push('/dashboard')} className="cursor-pointer inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
                                 <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
                                 </svg>
