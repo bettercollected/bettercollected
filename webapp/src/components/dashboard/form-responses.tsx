3b34171e90b69a7fe04c7297e19985da5eba20a6
rupan.chaulagain <rupanchaulagain.rc@gmail.com>
2022-12-26 10:17:36 +0545

    fixes to sidebar forms not highlighting when switching on topbar inside forms tab

--- webapp/src/components/dashboard/form-responses.tsx
+++ webapp/src/components/dashboard/form-responses.tsx
@@ -41,8 +41,6 @@ function createData(name: string, calories: number, fat: number, carbs: number, 
     return { name, calories, fat, carbs, protein };
 }
 
-const rows = [createData('Frozen yoghurt', 159, 6.0, 24, 4.0), createData('Ice cream sandwich', 237, 9.0, 37, 4.3), createData('Eclair', 262, 16.0, 24, 6.0), createData('Cupcake', 305, 3.7, 67, 4.3), createData('Gingerbread', 356, 16.0, 49, 3.9)];
-
 export default function FormSubmissionsTab({ workspaceId, formId }: any) {
     const [page, setPage] = useState(1);
     const handleChangePage = (event: unknown, newPage: number) => {
