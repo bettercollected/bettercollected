2f6ebac47ec812369cc67b2af487d70a0f38e3cd
rupan.chaulagain <rupanchaulagain.rc@gmail.com>
2023-01-04 20:43:59 +0545

    removed useless form component

--- webapp/src/components/form/components/description.tsx
+++
@@ -1,3 +0,0 @@
-export default function Description({ children }: any) {
-    return <p className="text-[#0000009C] w-full text-lg mb-4">{children}</p>;
-}
--- webapp/src/components/form/components/heading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Heading({ children }: any) {
-    return <h1 className="text-3xl w-full font-extrabold">{children}</h1>;
-}
