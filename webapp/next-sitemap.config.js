63acf2c8e550258995c8702f81c94d014770426c
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-01-09 14:51:55 +0545

    Disable indexing in /dashbaord pages

--- webapp/next-sitemap.config.js
+++ webapp/next-sitemap.config.js
@@ -20,6 +20,19 @@ module.exports = {
     robotsTxtOptions: {
         policies: [policy]
     },
+    transform: (config, path) => {
+        if (path.match(`\\/.*\\/(dashboard)\\.*`)) {
+            return null;
+        }
+        // Use default transformation for all other cases
+        return {
+            loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+            changefreq: config.changefreq,
+            priority: config.priority,
+            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+            alternateRefs: config.alternateRefs ?? []
+        };
+    },
     sourceDir: process.env.NODE_ENV === 'development' ? '.next-dev' : '.next',
     outDir: 'public'
 };
