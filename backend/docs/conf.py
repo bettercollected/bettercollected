2cff147716fae84e393a19ebbea209e0068f455c
Bibishan Pandey <bibishan@sireto.io>
2023-02-05 21:12:52 +0545

    Change sphinx theme and license rst

--- backend/docs/conf.py
+++ backend/docs/conf.py
@@ -8,8 +8,8 @@ from pallets_sphinx_themes import ProjectLink
 
 # Project --------------------------------------------------------------
 
-project = "bettercollected-backend-server"
-copyright = "2022, Bibishan Pandey"
+project = "Backend Server"
+copyright = "2023, Sireto Technology"
 author = "Bibishan Pandey"
 release = __version__
 
@@ -21,6 +21,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "pallets_sphinx_themes",
     "myst_parser",
+    "sphinx_material",
 ]
 
 autodoc_typehints = "description"
@@ -40,18 +41,41 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # HTML -----------------------------------------------------------------
 
-html_theme = "click"
+html_theme = "sphinx_material"
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the navigation.
+    "nav_title": "Better Collected",
+    # Set you GA account ID to enable tracking
+    # "google_analytics_account": "UA-XXXXX",
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    # "base_url": "https://project.github.io/project",
+    "version_dropdown": True,
+    "theme_color": "#3B82F6",
+    "color_primary": "white",
+    "color_accent": "#f43f5e",
+    "html_minify": True,
+    "css_minify": True,
+    "logo_icon": "&#xe869",
+    "repo_name": "Backend Server",
+    # "repo_url": "https://github.com/project/project/",
+    # Visible levels of the global TOC; -1 means unlimited
+    "globaltoc_depth": 4,
+    # If False, expand all TOC entries
+    "globaltoc_collapse": True,
+    # If True, show hidden TOC entries
+    "globaltoc_includehidden": True,
+}
 html_context = {
     "project_links": [
         ProjectLink("Source Code", ""),
     ]
 }
 html_sidebars = {
-    "index": ["project.html", "localtoc.html", "searchbox.html"],
-    "**": ["localtoc.html", "relations.html", "searchbox.html"],
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
 }
 singlehtml_sidebars = {"index": ["project.html", "localtoc.html"]}
-html_logo = "_static/logo.png"
-html_title = f"bettercollected-backend-server Documentation ({__version__})"
+html_title = f"Backend Server: Documentation ({__version__})"
 html_show_sourcelink = False
 html_static_path = ["_static"]
