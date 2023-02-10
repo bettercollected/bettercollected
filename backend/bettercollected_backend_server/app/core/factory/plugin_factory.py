f27c1367e3e6620313ab944b512b9c5ee1b45a03
Bibishan Pandey <bibishan@sireto.io>
2023-02-10 11:05:55 +0545

    [WIP] Plugin architecture host app configuration

---
+++ backend/bettercollected_backend_server/app/core/factory/plugin_factory.py
@@ -0,0 +1,29 @@
+"""Core implementation - plugin factory."""
+from typing import Any, Callable, Dict
+
+from bettercollected_backend_server.app.core.base.plugin_base import BasePlugin
+from common.enums.form_provider import FormProvider
+
+_plugin_creator_funcs: Dict[str, Callable[..., BasePlugin]] = {}
+
+
+def register_plugin(
+    provider: str | FormProvider, creation_func: Callable[..., BasePlugin]
+):
+    """Register a new form provider plugin."""
+    _plugin_creator_funcs[provider] = creation_func
+
+
+def unregister_plugin(provider: str | FormProvider):
+    """Unregister a loaded plugin."""
+    _plugin_creator_funcs.pop(provider, None)
+
+
+def create(args: Dict[str, Any]) -> BasePlugin:
+    """Create a plugin of a specific type, give a dictionary or args."""
+    provider = args.get("provider")
+    try:
+        creator_funcs = _plugin_creator_funcs[provider]
+        return creator_funcs(**args)
+    except KeyError:
+        raise ValueError(f"Provider: {provider} is not supported.")
---
+++ backend/bettercollected_backend_server/app/core/plugins/typeform.py
@@ -0,0 +1,17 @@
+from bettercollected_backend_server.app.core.factory import plugin_factory
+from bettercollected_backend_server.app.core.base.plugin_base import BasePlugin
+from common.enums.form_provider import FormProvider
+
+
+class TypeformPlugin(BasePlugin):
+    def __init__(self, provider: str | FormProvider):
+        super().__init__(provider)
+        self.provider = provider
+
+    def connect(self):
+        return "Connected to Typeform"
+
+
+def initialize() -> None:
+    """Register the Typeform Plugin."""
+    plugin_factory.register_plugin(FormProvider.TYPEFORM, TypeformPlugin)
