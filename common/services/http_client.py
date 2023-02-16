3c161d52ca4c2a1f52414caee762bb062bd8f6a5
nabin6246 <nabin62246@gmail.com>
2023-02-16 20:44:25 +0545

    Adds http client common services

---
+++ common/services/http_client.py
@@ -0,0 +1,20 @@
+import logging
+import typing
+
+from httpx import AsyncClient
+
+from common.exceptions.http import HTTPException
+from common.constants import messages
+
+log = logging.getLogger(__name__)
+
+
+class HttpClient(AsyncClient):
+
+    async def get(self, *args, **kwargs) -> typing.Any:
+        response = await super().get(*args, **kwargs)
+        if response.status_code != 200:
+            raise HTTPException(500, messages.connection_failed.format(
+                url=args[0]
+            ))
+        return response.json()
