d15c2b63b3836203bcfb34189b8a71b38c409a17
nabin6246 <nabin62246@gmail.com>
2023-02-24 01:47:26 +0545

    Fix redirection issue on auth

--- backend/backend/app/controllers/auth_router.py
+++ backend/backend/app/controllers/auth_router.py
@@ -1,5 +1,6 @@
 """Auth controller implementation."""
 import logging
+from typing import Optional
 
 from classy_fastapi import Routable, get
 from fastapi import Depends
@@ -7,6 +8,7 @@ from starlette.requests import Request
 from starlette.responses import RedirectResponse, Response
 
 from backend.app.container import container
+from backend.app.models.generic_models import GenericResponseModel, generate_generic_pageable_response
 from backend.app.router import router
 from backend.app.services.auth_cookie_service import set_tokens_to_response
 from backend.app.services.user_service import get_logged_user
@@ -25,13 +27,14 @@ class AuthRoutes(Routable):
 
     @get("/status")
     async def status(
-        self, user: User = Depends(get_logged_user)
-    ) -> AuthenticationStatus:
-        return AuthenticationStatus(user=user)
+            self, user: User = Depends(get_logged_user)
+    ):
+        return GenericResponseModel[AuthenticationStatus](
+            payload=generate_generic_pageable_response(data=AuthenticationStatus(user=user)))
 
     # TODO : Merge with plugin proxy currently it is handled for typeform only
     @get("/{provider_name}/oauth")
-    async def _oauth_provider(self, provider_name: str, request: Request):
+    async def _oauth_provider(self, provider_name: str, request: Request, creator: Optional[str] = True):
         client_referer_url = request.headers.get("referer")
         oauth_url = await self.auth_service.get_oauth_url(
             provider_name, client_referer_url
@@ -40,14 +43,15 @@ class AuthRoutes(Routable):
 
     @get("/{provider_name}/oauth/callback")
     async def _auth_callback(
-        self, provider_name: str, state: str, request: Request, response: Response
+            self, provider_name: str, state: str, request: Request
     ):
         user, state_data = await self.auth_service.handle_backend_auth_callback(
             provider_name=provider_name, state=state, request=request
         )
+        response = RedirectResponse(state_data.client_referer_uri)
         set_tokens_to_response(user, response)
         if state_data.client_referer_uri:
-            return RedirectResponse(state_data.client_referer_uri)
+            return response
         return {"message": "Token saved successfully."}
 
     # @get("/{provider}/basic")
