c4084ddd770691296a34e167ba7f930dff2cc98f
Bibishan Pandey <bibishan@sireto.io>
2023-03-13 13:00:55 +0545

    Add user service to get user credential and verify the authorization token

---
+++ integrations/google/googleform/app/services/user_service.py
@@ -0,0 +1,20 @@
+import jwt
+from starlette.requests import Request
+
+from common.models.user import User
+from googleform.app.exceptions import HTTPException
+from googleform.app.repositories.oauth_credential import OauthCredentialRepository
+from googleform.app.schemas.oauth_credential import Oauth2CredentialDocument
+from googleform.config import settings
+
+
+async def get_user_credential(request: Request) -> Oauth2CredentialDocument:
+    access_token = request.cookies.get("Authorization")
+    if not access_token:
+        raise HTTPException(401, "No Access Token provided.")
+    jwt_response = jwt.decode(
+        access_token, key=settings.AUTH_JWT_SECRET, algorithms=["HS256"]
+    )
+    user = User(**jwt_response)
+    credential = await OauthCredentialRepository().get(user.sub)
+    return credential
