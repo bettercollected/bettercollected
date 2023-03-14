6d00f94cdcfcffaa6e96c9adafc7a5c3f29f1fe0
nabin6246 <nabin62246@gmail.com>
2023-03-14 22:23:39 +0545

    Add jwt service to common

---
+++ common/services/jwt_service.py
@@ -0,0 +1,27 @@
+from typing import TypeVar, Type
+
+import jwt
+from pydantic import BaseModel
+
+from backend.config import settings
+
+T = TypeVar("T", bound=BaseModel)
+
+
+class JwtService:
+
+    def __init__(self, jwt_secret: str):
+        self.jwt_secret = jwt_secret
+
+    def encode(self, model: BaseModel) -> str:
+        return jwt.encode(
+            model.dict(exclude_none=True), key=self.jwt_secret
+        )
+
+    def decode(self, token: str, model: Type[T]) -> T:
+        decoded_data = jwt.decode(
+            token,
+            key=self.jwt_secret,
+            algorithms=["HS256"],
+        )
+        return model.parse_obj(decoded_data)
