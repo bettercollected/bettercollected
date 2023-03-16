4e9f2cb2b2fc5c98c5a23b7fa411fa2014bd645d
nabin6246 <nabin62246@gmail.com>
2023-03-16 11:41:39 +0545

    Remove console log statements

--- webapp/src/components/form/renderer/form-renderer.tsx
+++ webapp/src/components/form/renderer/form-renderer.tsx
@@ -170,7 +170,6 @@ export default function FormRenderer({ form, response }: FormRendererProps) {
                     </StyledTextField>
                 );
             case QUESTION_TYPE.MULTIPLE_CHOICE:
-                console.log('multiplechoice');
                 const choiceAnswer = ans?.choice?.value ?? ans?.choices?.values;
                 return (
                     <StyledTextField>
