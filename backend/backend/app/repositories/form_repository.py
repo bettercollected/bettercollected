5b03c310dc86164e9bf1a8c4a53f75af06490685
nabin6246 <nabin62246@gmail.com>
2023-03-21 00:20:23 +0545

    Add fastapi pagination for main branch

--- backend/backend/app/repositories/form_repository.py
+++ backend/backend/app/repositories/form_repository.py
@@ -1,36 +1,38 @@
 from typing import List
 
+import fastapi_pagination.ext.beanie
 from beanie import PydanticObjectId
+from beanie.odm.queries.aggregation import AggregationQuery
+from fastapi_pagination import Page
 
 from backend.app.schemas.standard_form import FormDocument
 
 
 class FormRepository:
-    async def get_forms_in_workspace(
-        self, workspace_id: PydanticObjectId, form_id_list: List[str]
-    ):
-        return (
-            await FormDocument.find({"form_id": {"$in": form_id_list}})
+    @staticmethod
+    def get_forms_in_workspace_query(
+            workspace_id: PydanticObjectId, form_id_list: List[str]
+    ) -> AggregationQuery:
+        forms = FormDocument.find({"form_id": {"$in": form_id_list}}) \
             .aggregate(
-                [
-                    {
-                        "$lookup": {
-                            "from": "workspace_forms",
-                            "localField": "form_id",
-                            "foreignField": "form_id",
-                            "as": "workspace_form",
-                        }
-                    },
-                    {"$unwind": "$workspace_form"},
-                    {"$match": {"workspace_form.workspace_id": workspace_id}},
-                    {"$set": {"settings": "$workspace_form.settings"}},
-                ]
-            )
-            .to_list()
+            [
+                {
+                    "$lookup": {
+                        "from": "workspace_forms",
+                        "localField": "form_id",
+                        "foreignField": "form_id",
+                        "as": "workspace_form",
+                    }
+                },
+                {"$unwind": "$workspace_form"},
+                {"$match": {"workspace_form.workspace_id": workspace_id}},
+                {"$set": {"settings": "$workspace_form.settings"}},
+            ]
         )
+        return forms
 
     async def search_form_in_workspace(
-        self, workspace_id: PydanticObjectId, form_ids: List[str], query: str
+            self, workspace_id: PydanticObjectId, form_ids: List[str], query: str
     ):
         return (
             await FormDocument.find(
@@ -42,7 +44,7 @@ class FormRepository:
                     ],
                 }
             )
-            .aggregate(
+                .aggregate(
                 [
                     {
                         "$lookup": {
@@ -57,7 +59,7 @@ class FormRepository:
                     {"$set": {"settings": "$workspace_form.settings"}},
                 ]
             )
-            .to_list()
+                .to_list()
         )
 
     async def save_form(self, form: FormDocument):
