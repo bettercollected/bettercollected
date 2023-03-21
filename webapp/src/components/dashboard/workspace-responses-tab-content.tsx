50bd76a6c851c5b1a2474a06045a9fd149b47d08
Bibishan Pandey <bibishan@sireto.io>
2023-03-21 16:14:03 +0545

    Fix margin bottom for submissions and deletion request

--- webapp/src/components/dashboard/workspace-responses-tab-content.tsx
+++ webapp/src/components/dashboard/workspace-responses-tab-content.tsx
@@ -68,7 +68,7 @@ export default function WorkspaceResponsesTabContent({ workspace, deletionReques
                     <p className="mt-4 p-0">0 forms</p>
                 </div>
             )}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-6">
                 {submissions?.length !== 0 &&
                     submissions?.map((submission: StandardFormResponseDto) => {
                         const slug = submission.responseId;
