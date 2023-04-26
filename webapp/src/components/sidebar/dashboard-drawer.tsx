4247a4a5c2a4eefc52b03a7fd9c5b8bcb0d905d4
Bibishan Pandey <bibishan@sireto.io>
2023-04-26 14:56:09 +0545

    Add workspace menu dropdown in dashboard drawer

--- webapp/src/components/sidebar/dashboard-drawer.tsx
+++ webapp/src/components/sidebar/dashboard-drawer.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
-import { ExpandMore, SettingsOutlined } from '@mui/icons-material';
-import { Accordion, AccordionDetails, AccordionSummary, Box, Divider, List, ListItem, Toolbar } from '@mui/material';
+import { Box, Divider, List, ListItem, Toolbar } from '@mui/material';
 
-import AuthAccountProfileImage from '@app/components/auth/account-profile-image';
 import { DashboardIcon } from '@app/components/icons/dashboard-icon';
 import { FormIcon } from '@app/components/icons/form-icon';
+import { SettingIcon } from '@app/components/icons/setting-icon';
 import MuiDrawer from '@app/components/sidebar/mui-drawer';
 import NavigationList from '@app/components/sidebar/navigation-list';
+import WorkspaceMenuDropdown from '@app/components/workspace/workspace-menu-dropdown';
 import { IDrawerProps, INavbarItem } from '@app/models/props/navbar';
 import { selectIsAdmin } from '@app/store/auth/slice';
 import { useAppSelector } from '@app/store/hooks';
-import { useGetAllMineWorkspacesQuery } from '@app/store/workspaces/api';
 import { selectWorkspace } from '@app/store/workspaces/slice';
-import { toEndDottedStr } from '@app/utils/stringUtils';
 
 DashboardDrawer.defaultProps = {
     drawerWidth: 289,
@@ -22,7 +20,6 @@ DashboardDrawer.defaultProps = {
 
 export default function DashboardDrawer({ drawerWidth, mobileOpen, handleDrawerToggle }: IDrawerProps) {
     const workspace = useAppSelector(selectWorkspace);
-    const { data, isLoading } = useGetAllMineWorkspacesQuery();
     const isAdmin = useAppSelector(selectIsAdmin);
     const commonWorkspaceUrl = `/${workspace?.workspaceName}/dashboard`;
 
@@ -42,10 +39,10 @@ export default function DashboardDrawer({ drawerWidth, mobileOpen, handleDrawerT
     ];
     const bottomNavList: Array<INavbarItem> = [
         {
-            key: 'workspace-settings',
-            name: 'Workspace Settings',
+            key: 'manage-workspace',
+            name: 'Manage workspace',
             url: `/${workspace?.workspaceName}/manage`,
-            icon: SettingsOutlined
+            icon: SettingIcon
         }
     ];
 
@@ -54,20 +51,8 @@ export default function DashboardDrawer({ drawerWidth, mobileOpen, handleDrawerT
             <Toolbar />
             <Box sx={{ overflow: 'auto', height: '100%' }}>
                 <List>
-                    <ListItem disablePadding>
-                        <Accordion disabled={isLoading} sx={{ paddingY: '16px', paddingX: '4px', width: '100%' }} elevation={0} className="hover:bg-zinc-100">
-                            <AccordionSummary expandIcon={<ExpandMore className="h-7 w-7 text-black-900 transition-all duration-300" />}>
-                                <AuthAccountProfileImage image={workspace?.profileImage} name={workspace?.title} />
-                                <p className="ml-3 p-0 !body1 flex items-center">{toEndDottedStr(workspace?.title, 30)}</p>
-                            </AccordionSummary>
-                            <AccordionDetails className="w-full flex flex-col gap-3">
-                                {data && Array.isArray(data) && data.length > 1 ? (
-                                    data.map((w) => <p key={w?.id}>{w?.title}</p>)
-                                ) : (
-                                    <p className="text-black-600">Currently, you only have a single workspace. Creation of new workspaces and collaboration coming soon.</p>
-                                )}
-                            </AccordionDetails>
-                        </Accordion>
+                    <ListItem disablePadding sx={{ paddingY: '20px', paddingX: '20px', width: '100%' }}>
+                        <WorkspaceMenuDropdown fullWidth />
                     </ListItem>
                 </List>
                 <Divider />
