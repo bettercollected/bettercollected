e802119911f72d7549df0e0ec9fd8f32781fa374
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-04-27 19:02:33 +0545

    Fix type issue for settings

--- backend/backend/config/https_certificate.py
+++ backend/backend/config/https_certificate.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings
 
 
 class HttpsCertificateApiSettings(BaseSettings):
-    host: ""
-    key: ""
+    host: str = ""
+    key: str = ""
 
     class Config:
         env_prefix = "HTTPS_CERT_API_"
