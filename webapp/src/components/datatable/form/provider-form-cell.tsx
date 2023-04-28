c7335dedbfb008f22ee39674966cf440b38a4fc4
Bibishan Pandey <bibishan@sireto.io>
2023-04-28 12:34:18 +0545

    Fix ellipsis text in datatable for forms list

--- webapp/src/components/datatable/form/provider-form-cell.tsx
+++ webapp/src/components/datatable/form/provider-form-cell.tsx
@@ -1,12 +1,10 @@
-import { Tooltip } from '@mui/material';
+import { Tooltip, Typography } from '@mui/material';
 
 import { TypeformIcon } from '@app/components/icons/brands/typeform';
 import { GoogleFormIcon } from '@app/components/icons/google-form-icon';
 import ActiveLink from '@app/components/ui/links/active-link';
-import { useBreakpoint } from '@app/lib/hooks/use-breakpoint';
 import { StandardFormDto } from '@app/models/dtos/form';
 import { WorkspaceDto } from '@app/models/dtos/workspaceDto';
-import { toEndDottedStr } from '@app/utils/stringUtils';
 
 interface IDataTableProviderFormCellProps {
     workspace: WorkspaceDto;
@@ -16,16 +14,16 @@ interface IDataTableProviderFormCellProps {
 export default function DataTableProviderFormCell({ workspace, form }: IDataTableProviderFormCellProps) {
     const Icon = form?.settings?.provider === 'google' ? GoogleFormIcon : TypeformIcon;
 
-    const breakpoint = useBreakpoint();
-
     return (
-        <div className="flex items-center body4 justify-start gap-3">
-            <Icon width={40} height={40} />{' '}
-            <ActiveLink href={`/${workspace.workspaceName}/dashboard/forms/${form.formId}`} className="hover:text-brand-600 hover:underline">
-                <Tooltip title={form?.title || 'Untitled'} arrow placement="top-start" enterDelay={300} enterTouchDelay={0}>
-                    <span>{['xs', '2xs', 'sm', 'md'].indexOf(breakpoint) !== -1 ? toEndDottedStr(form?.title || 'Untitled', 15) : toEndDottedStr(form?.title || 'Untitled', 20)}</span>
-                </Tooltip>
-            </ActiveLink>
-        </div>
+        <ActiveLink href={`/${workspace.workspaceName}/dashboard/forms/${form.formId}`} className="w-fit">
+            <Tooltip title={form?.title || 'Untitled'} arrow placement="top-start" enterDelay={300} enterTouchDelay={0}>
+                <Typography className="hover:!text-brand-600 hover:!underline flex justify-start items-center gap-3 !body3 !not-italic" noWrap>
+                    <span className="w-10 h-10">
+                        <Icon width={40} height={40} />
+                    </span>
+                    {form?.title || 'Untitled'}
+                </Typography>
+            </Tooltip>
+        </ActiveLink>
     );
 }
