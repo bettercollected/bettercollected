665d9f1e196f854a64fd2fbbe810be40c9ad4dd7
Bibishan Pandey <bibishan@sireto.io>
2023-05-03 20:01:21 +0545

    Add server side workspace dispatcher HOC

---
+++ webapp/src/Components/HOCs/ServerSideWorkspaceDispatcher.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+
+import { WorkspaceDto } from '@app/models/dtos/workspaceDto';
+import { useAppDispatch } from '@app/store/hooks';
+import { setWorkspace } from '@app/store/workspaces/slice';
+
+interface IServerSideWorkspaceDispatcherProps {
+    children: React.ReactNode | React.ReactNode[];
+    workspace: WorkspaceDto | null | undefined;
+}
+
+export default function ServerSideWorkspaceDispatcher({ children, workspace }: IServerSideWorkspaceDispatcherProps) {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (workspace) dispatch(setWorkspace(workspace));
+    }, []);
+
+    return <>{children}</>;
+}
