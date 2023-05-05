7f33b0405954fea1c499880db24df34f7242b2b0
Bibishan Pandey <bibishan@sireto.io>
2023-05-05 14:35:48 +0545

    Fix log out modal styles

--- webapp/src/components/logout/logout-view.tsx
+++ webapp/src/components/logout/logout-view.tsx
@@ -33,14 +33,14 @@ export default function LogoutView(props: any) {
         <div className="relative z-50 mx-auto max-w-full min-w-full md:max-w-[600px] lg:max-w-[600px]" {...props}>
             <div className="rounded-[4px] relative m-auto max-w-[500px] items-start justify-between bg-white">
                 <div className="relative flex flex-col items-center gap-8 justify-between p-10">
-                    <Logout className="text-6xl text-red-500" />
-                    <h4 className="sh1 text-center">Are you sure you want to logout?</h4>
+                    {/* <Logout className="text-6xl text-red-500" /> */}
+                    <h4 className="sh1">Are you sure you want to logout?</h4>
 
                     <div className="flex w-full gap-4 justify-end">
-                        <Button data-testid="logout-button" variant="solid" size="medium" color="danger" onClick={handleLogout}>
+                        <Button data-testid="logout-button" className="flex-1" variant="solid" size="medium" color="danger" onClick={handleLogout}>
                             Logout
                         </Button>
-                        <Button variant="solid" color="gray" size="medium" className="!bg-black-500" onClick={closeModal}>
+                        <Button variant="solid" color="gray" size="medium" className="flex-1 !bg-black-500" onClick={closeModal}>
                             Cancel
                         </Button>
                     </div>
