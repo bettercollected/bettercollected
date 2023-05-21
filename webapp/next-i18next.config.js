f3d858876845a95744038c7a1db848eb54f0d1ac
Niraj <duwalkneerose@gmail.com>
2023-05-21 08:50:50 +0545

    Move locales to public

--- webapp/next-i18next.config.js
+++ webapp/next-i18next.config.js
@@ -5,6 +5,5 @@ module.exports = {
         defaultLocale: 'en',
         locales: ['en', 'nl', 'np']
     },
-    reloadOnPrerender: process.env.NODE_ENV === 'development',
-    localePath: typeof window === 'undefined' ? require('path').resolve('./src/assets/locales') : '/src/assets/locales'
+    localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales'
 };
