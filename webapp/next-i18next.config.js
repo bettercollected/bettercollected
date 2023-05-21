0e879e517ff1d3ef922fe7fd7f459b565fefdbbf
Niraj <duwalkneerose@gmail.com>
2023-05-21 08:37:59 +0545

    change i18next localePath

--- webapp/next-i18next.config.js
+++ webapp/next-i18next.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 module.exports = {
-    debug: false,
+    debug: process.env.NODE_ENV === 'development',
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'nl', 'np']
     },
-    localePath: path.resolve('./src/assets/locales/')
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
+    localePath: typeof window === 'undefined' ? require('path').resolve('./src/assets/locales') : '/src/assets/locales'
 };
