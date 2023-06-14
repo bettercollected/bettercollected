3513673698f4a09fa9251e97111f52444f122290
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-06-14 11:19:27 +0545

    Update preview modal to be in center

--- webapp/src/components/modal-views/modals/workspace-preview-modal.tsx
+++ webapp/src/components/modal-views/modals/workspace-preview-modal.tsx
@@ -9,23 +9,23 @@ export default function WorkspacePreviewModal() {
 
     return (
         <div
-            className="p-5 md:px-20 min-h-screen min-w-screen"
+            className="p-5 md:p-10 md:px-20 lg:px-30 xl:px-40 min-h-screen flex flex-col justify-center min-w-screen"
             onClick={(event: any) => {
-                if (event.target === event.currentTarget) {
-                    closeModal();
-                }
+                if (event.target === event.currentTarget) closeModal();
             }}
         >
-            <div className="bg-white relative overflow-hidden pointer-events-none h-full rounded-lg ">
-                <div
-                    className="absolute top-5 right-5 cursor-pointer z-20 !pointer-events-auto"
-                    onClick={() => {
-                        closeModal();
-                    }}
-                >
-                    <Close />
+            <div>
+                <div className="bg-white relative overflow-hidden pointer-events-none h-full rounded-lg ">
+                    <div
+                        className="absolute top-5 rounded-full bg-brand-100 p-2 right-5 cursor-pointer z-20 !pointer-events-auto"
+                        onClick={() => {
+                            closeModal();
+                        }}
+                    >
+                        <Close />
+                    </div>
+                    <WorkspaceHomeContainer isCustomDomain={false} />
                 </div>
-                <WorkspaceHomeContainer isCustomDomain={false} />
             </div>
         </div>
     );
