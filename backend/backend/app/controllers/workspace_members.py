312a7aeb381dc4f4696eccd8c85ce195e76fd5cb
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-06-15 12:51:34 +0545

    Uppdate workspace invitation dto

--- backend/backend/app/controllers/workspace_members.py
+++ backend/backend/app/controllers/workspace_members.py
@@ -6,7 +6,10 @@ from fastapi import Depends
 from fastapi_pagination import Page
 
 from backend.app.container import container
-from backend.app.models.dtos.workspace_member_dto import WorkspaceMemberDto
+from backend.app.models.dtos.workspace_member_dto import (
+    WorkspaceMemberDto,
+    WorkspaceInvitationDto,
+)
 from backend.app.models.enum.invitation_response import InvitationResponse
 from backend.app.models.invitation_request import InvitationRequest
 from backend.app.router import router
@@ -48,7 +51,7 @@ class WorkspaceMembersRouter(Routable):
             workspace_id=workspace_id, user_id=user_id, user=user
         )
 
-    @get("/invitations", response_model=Page[Any])
+    @get("/invitations", response_model=Page[WorkspaceInvitationDto])
     async def get_workspace_invitations(
         self,
         workspace_id: PydanticObjectId,
@@ -69,7 +72,7 @@ class WorkspaceMembersRouter(Routable):
             workspace_id=workspace_id, invitation=invitation, user=user
         )
 
-    @get("/invitations/{invitation_token}")
+    @get("/invitations/{invitation_token}", response_model=WorkspaceInvitationDto)
     async def get_invitation_by_token(
         self,
         workspace_id: PydanticObjectId,
--- backend/backend/app/models/dtos/workspace_member_dto.py
+++ backend/backend/app/models/dtos/workspace_member_dto.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 from typing import List, Optional
 
+from beanie import PydanticObjectId
 from fastapi_camelcase import CamelModel
+from pydantic import EmailStr
 
 from backend.app.models.enum.workspace_roles import WorkspaceRoles
+from common.enums.workspace_invitation_status import InvitationStatus
 
 
 class WorkspaceMemberDto(CamelModel):
@@ -22,3 +25,15 @@ class FormImporterDetails(CamelModel):
     last_name: Optional[str]
     email: Optional[str]
     profile_image: Optional[str]
+
+
+class WorkspaceInvitationDto(CamelModel):
+    created_at: Optional[datetime]
+    email: Optional[EmailStr]
+    expiry: Optional[int]
+    invitation_status: Optional[InvitationStatus]
+    invitation_token: Optional[str]
+    role: Optional[WorkspaceRoles]
+    updated_at: Optional[datetime]
+    workspace_id: Optional[PydanticObjectId]
+    id: Optional[PydanticObjectId]
