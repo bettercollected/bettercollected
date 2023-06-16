35350d809175715f80d9125372a6910175b9fb9b
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-06-16 13:40:01 +0545

    Add api_key header for requests

--- temporal/worker/activities/delete_user.py
+++ temporal/worker/activities/delete_user.py
@@ -10,7 +10,8 @@ with workflow.unsafe.imports_passed_through():
 @activity.defn(name="delete_user")
 async def delete_user(user_token: UserTokens):
     cookies = {"Authorization": user_token.access_token, "RefreshToken": user_token.refresh_token}
-    response = requests.delete(url=settings.server_url + "/auth/user", cookies=cookies)
+    headers = {"api_key": settings.api_key}
+    response = requests.delete(url=settings.server_url + "/auth/user", cookies=cookies, headers=headers)
     if response.status_code != 200:
         raise RuntimeError("Could not delete user")
     return "User Deleted Successfully"
--- temporal/worker/settings/application.py
+++ temporal/worker/settings/application.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 class ApplicationSettings(BaseSettings):
     server_url: str = "http://backend:8000/api/v1"
+    api_key: str = "random_api_key"
 
 
 settings = ApplicationSettings()
