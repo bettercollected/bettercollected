079533378ff367ebc3de93569426f953c294d659
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-06-19 17:09:27 +0545

    Add docker file and update environment

--- temporal/worker/main.py
+++ temporal/worker/main.py
@@ -4,12 +4,12 @@ from temporalio.client import Client
 from temporalio.worker import Worker
 
 from activities.delete_user import delete_user
+from settings.application import settings
 from workflows.user_deletion_workflow import DeleteUserWorkflow
 
 
 async def main():
-    client = await Client.connect("localhost:7233")
-
+    client = await Client.connect(settings.temporal_server_url, namespace=settings.namespace)
     worker = Worker(
         client,
         task_queue="delete_user",
@@ -17,6 +17,7 @@ async def main():
         activities=[delete_user],
     )
     await worker.run()
+    exit()
 
 
 if __name__ == "__main__":
--- temporal/worker/settings/application.py
+++ temporal/worker/settings/application.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 
 class ApplicationSettings(BaseSettings):
+    temporal_server_url: str = "localhost:7233"
+    namespace: str = "default"
     server_url: str = "http://backend:8000/api/v1"
     api_key: str = "random_api_key"
     aes_hex_key: str = ""
