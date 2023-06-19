57b0b78f95c3d3b254091a2ff4ae2cef057cba1c
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-06-19 16:23:37 +0545

    Add decryption for user credentials

--- temporal/worker/activities/delete_user.py
+++ temporal/worker/activities/delete_user.py
@@ -1,14 +1,19 @@
+import json
+
 from temporalio import activity, workflow
 
-from models.user_tokens import UserTokens
 from settings.application import settings
 
 with workflow.unsafe.imports_passed_through():
     import requests
+    from configs.crypto import crypto
+    from models.user_tokens import UserTokens
 
 
 @activity.defn(name="delete_user")
-async def delete_user(user_token: UserTokens):
+async def delete_user(token: str):
+    decrypted_token = crypto.decrypt(token)
+    user_token = UserTokens(**json.loads(decrypted_token))
     cookies = {"Authorization": user_token.access_token, "RefreshToken": user_token.refresh_token}
     headers = {"api_key": settings.api_key}
     response = requests.delete(url=settings.server_url + "/auth/user", cookies=cookies, headers=headers)
