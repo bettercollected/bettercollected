4a97eeb8081f508dd1210e101dee9da5c522fdff
KNabin <nabin.kawan.9@gmail.com>
2023-07-27 13:34:11 +0545

    Change type of BuilderMenuState

--- webapp/src/store/form-builder/types.ts
+++ webapp/src/store/form-builder/types.ts
@@ -6,7 +6,7 @@ import { AppDispatch } from '../store';
 
 export interface IBuilderMenuState {
     spotlightField?: { isOpen: boolean; afterFieldUuid: string };
-    commands?: { isOpen: boolean; atFieldUuid: string };
+    commands?: { isOpen: boolean; atFieldUuid: string; position: 'up' | 'down' };
     fieldSettings?: { isOpen: boolean; atFieldUuid: string };
     pipingFields?: { isOpen: boolean; atFieldUuid: string };
     pipingFieldSettings?: { isOpen: boolean; uuid: string };
