d5d1178859805fd6cf24ca3e0a6d5cc6d54a91dc
rupan.chaulagain <rupanchaulagain.rc@gmail.com>
2023-07-27 11:21:02 +0545

    Prevent using invalid characters in workspace handle, add validation messages and translation to it

--- webapp/src/constants/locales/validation-message.ts
+++ webapp/src/constants/locales/validation-message.ts
@@ -1,5 +1,6 @@
 export const validationMessage = {
     workspaceTitle: 'WORKSPACE.TITLE_VALIDATION_MESSAGE',
     workspaceName: 'WORKSPACE.WORKSPACE_NAME_VALIDATION_MESSAGE',
+    workspaceNameInvalidCharacter: 'WORKSPACE.WORKSPACE_NAME_INVALID_CHARACTER',
     slug: 'SLUG.VALIDATION_MESSAGE'
 };
