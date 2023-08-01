a73491e1c991682c0a3bd2d1d54089eeaac77616
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-08-01 16:06:00 +0545

    Add workflow to import form

--- temporal/worker/main.py
+++ temporal/worker/main.py
@@ -4,7 +4,9 @@ from temporalio.client import Client
 from temporalio.worker import Worker
 
 from activities.delete_user import delete_user
+from activities.import_form import import_form
 from settings.application import settings
+from workflows.form_import_workflow import ImportFormWorkflow
 from workflows.user_deletion_workflow import DeleteUserWorkflow
 
 
@@ -12,9 +14,9 @@ async def main():
     client = await Client.connect(settings.temporal_server_url, namespace=settings.namespace)
     worker = Worker(
         client,
-        task_queue="delete_user",
-        workflows=[DeleteUserWorkflow],
-        activities=[delete_user],
+        task_queue=settings.worker_queue,
+        workflows=[DeleteUserWorkflow, ImportFormWorkflow],
+        activities=[delete_user, import_form],
     )
     await worker.run()
     exit()
--- temporal/worker/settings/application.py
+++ temporal/worker/settings/application.py
@@ -10,6 +10,7 @@ class ApplicationSettings(BaseSettings):
     server_url: str = "http://backend:8000/api/v1"
     api_key: str = "random_api_key"
     aes_hex_key: str = ""
+    worker_queue: str = "default"
 
 
 settings = ApplicationSettings()
