75d9960036ce52532ae806c9d05262274d2efd7d
Sital Nagarkoti <sitalnagarkoti123@gmail.com>
2023-08-01 14:20:39 +0545

    Refactor "test_workspace_responders.py" by removing code smells.

--- backend/tests/app/controllers/test_workspace_responders.py
+++ backend/tests/app/controllers/test_workspace_responders.py
@@ -1,17 +1,24 @@
 import pytest
 
 from backend.app.container import container
+from common.constants import MESSAGE_FORBIDDEN
 from tests.app.controllers.data import testUser, workspace_tag
 
 
 @pytest.fixture()
-def client(client_test):
-    yield client_test
+def workspace_responders_api(workspace):
+    return "api/v1/workspaces/" + str(workspace.id) + "/responders"
 
 
 @pytest.fixture()
-def workspace_responders_api(workspace):
-    yield "api/v1/workspaces/" + str(workspace.id) + "/responders"
+def workspace_tag_url(workspace_responders_api):
+    return f"{workspace_responders_api}/tags"
+
+
+async def create_tag_for_responder(workspace, tag_title):
+    return await container.workspace_responders_service().create_workspace_tag(
+        workspace.id, tag_title, testUser
+    )
 
 
 @pytest.mark.asyncio
@@ -29,18 +36,21 @@ class TestWorkspaceResponders:
         workspace_responders = client.get(
             workspace_responders_api, cookies=test_user_cookies
         )
-        emails = [
+
+        expected_responders_emails = [
             workspace_form_response_2["dataOwnerIdentifier"],
             workspace_form_response_1["dataOwnerIdentifier"],
             workspace_form_response["dataOwnerIdentifier"],
         ]
-        emails.sort()
-        assert workspace_responders.status_code == 200
-        assert workspace_responders.json()["total"] == 3
-        workspace_responders_emails = [
+        expected_responders_emails.sort()
+        actual_responders_emails = [
             item["email"] for item in workspace_responders.json()["items"]
         ]
-        assert workspace_responders_emails == emails
+        expected_responders_number = 3
+        actual_responders_number = workspace_responders.json()["total"]
+        assert workspace_responders.status_code == 200
+        assert actual_responders_number == expected_responders_number
+        assert actual_responders_emails == expected_responders_emails
 
     def test_unauthorized_get_workspace_responders(
         self,
@@ -52,69 +62,60 @@ class TestWorkspaceResponders:
         unauthorized_client = client.get(
             workspace_responders_api, cookies=test_user_cookies_1
         )
+
+        expected_response_message = MESSAGE_FORBIDDEN
+        actual_response_message = unauthorized_client.json()
         assert unauthorized_client.status_code == 403
-        assert (
-            unauthorized_client.json()
-            == "403 - Forbidden: You don't have permission to access this resource."
-        )
+        assert actual_response_message == expected_response_message
 
     async def test_get_workspace_tags(
         self,
         client,
-        workspace_responders_api,
+        workspace_tag_url,
         workspace_group,
         test_user_cookies,
         workspace,
     ):
-        tag1 = await container.workspace_responders_service().create_workspace_tag(
-            workspace.id, "Admin", testUser
-        )
-        tag2 = await container.workspace_responders_service().create_workspace_tag(
-            workspace.id, "Leader", testUser
-        )
-        workspace_tags = client.get(
-            workspace_responders_api + str("/tags"), cookies=test_user_cookies
-        )
-        workspace_tags_title = [item.get("title") for item in workspace_tags.json()]
-        tags = [tag1.title, tag2.title]
-        assert workspace_tags_title == tags
+        tag1 = await create_tag_for_responder(workspace, "Admin")
+        tag2 = await create_tag_for_responder(workspace, "Leader")
+
+        workspace_tags = client.get(workspace_tag_url, cookies=test_user_cookies)
+
+        actual_tags = [item.get("title") for item in workspace_tags.json()]
+        expected_tags = [tag1.title, tag2.title]
+        assert actual_tags == expected_tags
 
     def test_unauthorized_get_workspace_tags(
-        self, client, workspace_responders_api, test_user_cookies_1
+        self, client, workspace_tag_url, test_user_cookies_1
     ):
-        workspace_tags = client.get(
-            workspace_responders_api + "/tags", cookies=test_user_cookies_1
-        )
-        assert workspace_tags.status_code == 403
-        assert (
-            workspace_tags.json()
-            == "403 - Forbidden: You don't have permission to access this resource."
+        unauthorized_client = client.get(workspace_tag_url, cookies=test_user_cookies_1)
+
+        expected_response_message = MESSAGE_FORBIDDEN
+        actual_response_message = unauthorized_client.json()
+        assert unauthorized_client.status_code == 403
+        assert actual_response_message == expected_response_message
+
+    def test_create_workspace_tag(self, client, workspace_tag_url, test_user_cookies):
+        tag = client.post(
+            workspace_tag_url, cookies=test_user_cookies, json=workspace_tag
         )
 
-    def test_create_workspace_tag(
-        self, client, workspace_responders_api, test_user_cookies
-    ):
-        tag = client.post(
-            workspace_responders_api + "/tags",
-            cookies=test_user_cookies,
-            json=workspace_tag,
-        )
+        expected_tag = workspace_tag.get("title")
+        actual_tag = tag.json().get("title")
         assert tag.status_code == 200
-        assert tag.json().get("title") == workspace_tag.get("title")
+        assert actual_tag == expected_tag
 
     def test_unauthorized_create_workspace_tag(
-        self, client, workspace_responders_api, test_user_cookies_1
+        self, client, workspace_tag_url, test_user_cookies_1
     ):
-        workspace_tags = client.post(
-            workspace_responders_api + "/tags",
-            cookies=test_user_cookies_1,
-            json=workspace_tag,
+        unauthorized_client = client.post(
+            workspace_tag_url, cookies=test_user_cookies_1, json=workspace_tag
         )
-        assert workspace_tags.status_code == 403
-        assert (
-            workspace_tags.json()
-            == "403 - Forbidden: You don't have permission to access this resource."
-        )
+
+        expected_response_message = MESSAGE_FORBIDDEN
+        actual_response_message = unauthorized_client.json()
+        assert unauthorized_client.status_code == 403
+        assert actual_response_message == expected_response_message
 
     async def test_patch_workspace_responder_with_email(
         self, client, test_user_cookies, workspace, workspace_responders_api
@@ -122,13 +123,20 @@ class TestWorkspaceResponders:
         tag = await container.workspace_responders_service().create_workspace_tag(
             workspace.id, "GrandMaster", testUser
         )
+        patch_workspace_responder_url = (
+            f"{workspace_responders_api}?email=bettercollected@gmail.com"
+        )
+
         workspace_responder = client.patch(
-            workspace_responders_api + "?email=bettercollected@gmail.com",
+            patch_workspace_responder_url,
             cookies=test_user_cookies,
             json={"tags": [str(tag.id)]},
         )
+
+        expected_tag_id = str(tag.id)
+        actual_tag_id = workspace_responder.json().get("tags")[0]
         assert workspace_responder.status_code == 200
-        assert workspace_responder.json().get("tags")[0] == str(tag.id)
+        assert actual_tag_id == expected_tag_id
 
     async def test_unauthorized_patch_workspace_responder_with_email(
         self, client, test_user_cookies_1, workspace_responders_api, workspace
@@ -136,13 +144,17 @@ class TestWorkspaceResponders:
         tag = await container.workspace_responders_service().create_workspace_tag(
             workspace.id, "GrandMaster", testUser
         )
-        workspace_responder = client.patch(
-            workspace_responders_api + "?email=bettercollected@gmail.com",
+        patch_workspace_responder_url = (
+            f"{workspace_responders_api}?email=bettercollected@gmail.com"
+        )
+
+        unauthorized_client = client.patch(
+            patch_workspace_responder_url,
             cookies=test_user_cookies_1,
             json={"tags": [str(tag.id)]},
         )
-        assert workspace_responder.status_code == 403
-        assert (
-            workspace_responder.json()
-            == "403 - Forbidden: You don't have permission to access this resource."
-        )
+
+        expected_response_message = MESSAGE_FORBIDDEN
+        actual_response_message = unauthorized_client.json()
+        assert unauthorized_client.status_code == 403
+        assert actual_response_message == expected_response_message
