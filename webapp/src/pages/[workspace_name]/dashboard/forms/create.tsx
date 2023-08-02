1d3607c41a866f99ed948babfbd7818893d174c8
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-08-02 14:42:56 +0545

    Listen keydown events of form builder using wrapper

--- webapp/src/pages/[workspace_name]/dashboard/forms/create.tsx
+++ webapp/src/pages/[workspace_name]/dashboard/forms/create.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 
 import FormBuilderContainerWrapper from '@Components/HOCs/FormBuilderContainerWrapper';
+import FormBuilderKeyDownListerner from '@Components/HOCs/FormBuilderKeyDownListerner';
 
 import environments from '@app/configs/environments';
 import FormBuilder from '@app/containers/form-builder/FormBuilder';
@@ -24,9 +25,11 @@ export default function CreateFormPage({ workspace, _nextI18Next }: ICreateFormP
 
     return environments.ENABLE_FORM_BUILDER ? (
         <FormBuilderContainerWrapper>
-            <Layout isCustomDomain={false} isClientDomain={false} showNavbar={true} hideMenu={false} showAuthAccount={true} className="!p-0 !bg-white flex flex-col !min-h-calc-68">
-                <FormBuilder workspace={workspace} _nextI18Next={_nextI18Next} />
-            </Layout>
+            <FormBuilderKeyDownListerner>
+                <Layout isCustomDomain={false} isClientDomain={false} showNavbar={true} hideMenu={false} showAuthAccount={true} className="!p-0 !bg-white flex flex-col !min-h-calc-68">
+                    <FormBuilder workspace={workspace} _nextI18Next={_nextI18Next} />
+                </Layout>
+            </FormBuilderKeyDownListerner>
         </FormBuilderContainerWrapper>
     ) : (
         <></>
