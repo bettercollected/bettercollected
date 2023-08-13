7e42f07e853bf1588cfd072b182f99b4ec341a21
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-08-13 22:18:10 +0545

    Fix background in form page

--- webapp/src/layouts/_layout.tsx
+++ webapp/src/layouts/_layout.tsx
@@ -31,9 +31,9 @@ export default function Layout({
     showAuthAccount
 }: React.PropsWithChildren<LayoutProps>) {
     return (
-        <div className="!min-h-screen !min-w-full bg-brand-100 dark:bg-dark z-20">
+        <div className="!min-h-screen !min-w-full !bg-brand-100 dark:bg-dark z-20">
             {showNavbar && <AuthNavbar isFooter={isFooter} isCustomDomain={isCustomDomain} isClientDomain={isClientDomain} showHamburgerIcon={showHamburgerIcon} hideMenu={hideMenu} showPlans={false} showAuthAccount={showAuthAccount} />}
-            <Box className={`float-none lg:float-right ${showNavbar ? 'min-h-calc-68 mt-[68px] ' : 'min-h-screen'} w-full px-5  !bg-brand-100 lg:px-10 ${className}`} component="main" sx={{ display: 'flex', width: '100%' }}>
+            <Box className={`float-none lg:float-right ${showNavbar ? '!min-h-calc-68 !mt-[68px] ' : 'min-h-screen'} w-full px-5  !bg-brand-100 lg:px-10 ${className}`} component="main" sx={{ display: 'flex', width: '100%' }}>
                 <div className={cn(`w-full h-full ${childClassName}`)}>{children}</div>
             </Box>
         </div>
