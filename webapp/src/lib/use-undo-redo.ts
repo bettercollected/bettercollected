3e6ef94d9f5ef42da64a4ef545fd3216c3803995
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-08-17 21:07:04 +0545

    Add undo-redo hook

---
+++ webapp/src/lib/use-undo-redo.ts
@@ -0,0 +1,29 @@
+import { useLayoutEffect, useState } from 'react';
+
+import EventBusEventType from '@app/models/enums/eventBusEnum';
+
+import eventBus from './event-bus';
+
+export default function useUndoRedo() {
+    const [isUndoRedoInProgress, setIsUndoRedoInProgress] = useState(false);
+
+    const startUndoRedoAction = () => {
+        setIsUndoRedoInProgress(true);
+    };
+
+    const completeUndoRedoAction = () => {
+        setIsUndoRedoInProgress(false);
+    };
+
+    useLayoutEffect(() => {
+        eventBus.on(EventBusEventType.History.UndoRedoStart, startUndoRedoAction);
+        eventBus.on(EventBusEventType.History.UndoRedoCompleted, completeUndoRedoAction);
+
+        return () => {
+            eventBus.removeListener(EventBusEventType.History.UndoRedoStart, startUndoRedoAction);
+            eventBus.removeListener(EventBusEventType.History.UndoRedoCompleted, completeUndoRedoAction);
+        };
+    }, []);
+
+    return { isUndoRedoInProgress };
+}
