85e6605235b4f9fdd060193347e65e05e796a495
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-08-22 22:51:40 +0545

    Fix description is not shown in workspace

--- webapp/src/components/workspace/public-workspace-title-description.tsx
+++ webapp/src/components/workspace/public-workspace-title-description.tsx
@@ -1,38 +1,42 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import { useTranslation } from 'next-i18next';
+import {useTranslation} from 'next-i18next';
 
 import Tooltip from '@Components/Common/DataDisplay/Tooltip';
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
 
 import ReactContentEditable from '@app/components/inline-editable';
-import { toastMessage } from '@app/constants/locales/toast-message';
-import { ToastId } from '@app/constants/toastId';
-import { useAppDispatch, useAppSelector } from '@app/store/hooks';
-import { usePatchExistingWorkspaceMutation } from '@app/store/workspaces/api';
-import { setWorkspace } from '@app/store/workspaces/slice';
-import { toEndDottedStr, trimTooltipTitle } from '@app/utils/stringUtils';
+import {toastMessage} from '@app/constants/locales/toast-message';
+import {ToastId} from '@app/constants/toastId';
+import {useAppDispatch, useAppSelector} from '@app/store/hooks';
+import {usePatchExistingWorkspaceMutation} from '@app/store/workspaces/api';
+import {setWorkspace} from '@app/store/workspaces/slice';
+import {toEndDottedStr, trimTooltipTitle} from '@app/utils/stringUtils';
+import MarkdownText from "@Components/Common/Markdown";
 
 interface IPublicWorkspaceTitleAndDescriptionProps {
     isFormCreator: boolean;
     className?: string;
 }
 
-export default function PublicWorkspaceTitleAndDescription({ isFormCreator, className = '' }: IPublicWorkspaceTitleAndDescriptionProps) {
+export default function PublicWorkspaceTitleAndDescription({
+                                                               isFormCreator,
+                                                               className = ''
+                                                           }: IPublicWorkspaceTitleAndDescriptionProps) {
     const [isMarkdownEditable, setIsMarkdownEditable] = useState(false);
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     const dispatch = useAppDispatch();
-    const [patchExistingWorkspace, { isLoading }] = usePatchExistingWorkspaceMutation();
+    const [patchExistingWorkspace, {isLoading}] = usePatchExistingWorkspaceMutation();
     const workspace = useAppSelector((state) => state.workspace);
 
     const patchWorkspaceInformation = async (formData: any) => {
-        const response: any = await patchExistingWorkspace({ workspace_id: workspace.id, body: formData });
+        const response: any = await patchExistingWorkspace({workspace_id: workspace.id, body: formData});
         if (response.error) {
-            toast(response.error.data || t(toastMessage.somethingWentWrong).toString(), { toastId: ToastId.ERROR_TOAST });
+            toast(response.error.data || t(toastMessage.somethingWentWrong).toString(), {toastId: ToastId.ERROR_TOAST});
         }
         if (response.data) {
             dispatch(setWorkspace(response.data));
-            toast(t(toastMessage.workspaceUpdate).toString(), { type: 'default', toastId: ToastId.SUCCESS_TOAST });
+            toast(t(toastMessage.workspaceUpdate).toString(), {type: 'default', toastId: ToastId.SUCCESS_TOAST});
         }
     };
 
@@ -61,15 +65,17 @@ export default function PublicWorkspaceTitleAndDescription({ isFormCreator, clas
                     <Tooltip title={trimTooltipTitle(fullWorkspaceName)}>
                         <h4 className="h4 w-fit">{strippedWorkspaceTitle}</h4>
                     </Tooltip>
-                    {/* <MarkdownText scrollTitle={fullWorkspaceName} description={workspace.description} contentStripLength={280} markdownClassName="text-black-700 " textClassName="text-black-700 body3" /> */}
+                    <MarkdownText className="max-w-[700px] overflow-hidden" text={workspace.description}/>
                 </div>
             </div>
         );
     return (
         <div className={`h-full w-full ${className}`}>
             <div className="w-full flex flex-col gap-4">
-                <ReactContentEditable callback={handleTitleChange} type="h4" content={fullWorkspaceName} className="h4" />
-                <ReactContentEditable callback={handleDescriptionChange} type="p" content={workspace?.description} className="text-black-700 body3" />
+                <ReactContentEditable callback={handleTitleChange} type="h4" content={fullWorkspaceName}
+                                      className="h4"/>
+                <ReactContentEditable callback={handleDescriptionChange} type="p" content={workspace?.description}
+                                      className="text-black-700 body3"/>
             </div>
         </div>
     );
