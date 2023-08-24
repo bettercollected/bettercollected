43a459837f499c3a714f0aaa1407db1c7cd0525a
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-08-24 15:34:49 +0545

    Enforce single file attachment per field

--- webapp/src/Components/Form/atom.ts
+++ webapp/src/Components/Form/atom.ts
@@ -1,21 +1,29 @@
 import { atom, useAtom } from 'jotai';
 
 interface FileAtom {
-    fileId: string;
     fieldId: string;
+    fileId: string;
     fileName: string;
     file: File;
 }
+
 const filesAtom = atom<FileAtom[]>([]);
 
 export default function useFormAtom() {
     const [files, setFiles] = useAtom(filesAtom);
 
     const addFile = (fieldId: string, fileId: string, fileName: string, file: File) => {
-        setFiles([...files, { fieldId, fileId, fileName, file }]);
+        // remove existing file
+        const updatedFiles = files.filter((item) => item.fieldId !== fieldId);
+        // add new file
+        updatedFiles.push({ fieldId, fileId, fileName, file });
+
+        setFiles(updatedFiles);
     };
+
     const resetFormFiles = () => {
         setFiles([]);
     };
+
     return { files, addFile, resetFormFiles };
 }
