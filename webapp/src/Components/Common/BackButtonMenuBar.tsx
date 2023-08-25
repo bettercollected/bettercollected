76f0f7e2782f1586fb3ac04ba61f99e725a8a537
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-08-25 10:10:40 +0545

    Add BackButtonMenuBar

---
+++ webapp/src/Components/Common/BackButtonMenuBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { ArrowBack } from '@app/components/icons/arrow-back';
+
+interface BackButtonMenuBarProps {
+    text: string;
+}
+export default function BackButtonMenuBar({ text }: BackButtonMenuBarProps) {
+    return (
+        <div className="flex py-2 px-5 items-center w-full fixed">
+            <div className="flex items-center space-x-1">
+                <ArrowBack />
+                <p className="p2">{text}</p>
+            </div>
+        </div>
+    );
+}
