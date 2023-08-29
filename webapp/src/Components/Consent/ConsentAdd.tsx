97d48acbbd09db8d3226556d237d9fed42dc57d8
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-08-29 10:33:23 +0545

    Add purpose modal

--- webapp/src/Components/Consent/ConsentAdd.tsx
+++ webapp/src/Components/Consent/ConsentAdd.tsx
@@ -4,6 +4,7 @@ import { Autocomplete } from '@mui/material';
 import cn from 'classnames';
 
 import { Hint } from '@app/components/icons/hint';
+import { useModal } from '@app/components/modal-views/context';
 import { OnlyClassNameInterface } from '@app/models/interfaces';
 
 import ConsentInput from '../Common/Input/AutoCompleteInput';
@@ -17,6 +18,7 @@ interface ConsentAddInputProps extends OnlyClassNameInterface {
 }
 
 export default function ConsentAddInput({ className, title, placeholder = '', hint, options }: ConsentAddInputProps) {
+    const { openModal } = useModal();
     return (
         <div className={cn('rounded-lg p-5 bg-new-black-200', className)}>
             {hint && (
@@ -25,7 +27,16 @@ export default function ConsentAddInput({ className, title, placeholder = '', hi
                     <div className="ml-10 p2 !text-new-black-800">{hint}</div>
                 </div>
             )}
-            <AutoCompleteInput dropdownTitle="Purpose Of The Form" title={title} placeholder={placeholder} className="!cursor-pointer" options={options} />
+            <AutoCompleteInput
+                dropdownTitle="Purpose Of The Form"
+                title={title}
+                placeholder={placeholder}
+                className="!cursor-pointer"
+                options={options}
+                onSelect={() => {
+                    openModal('CONSENT_PURPOSE_MODAL_VIEW');
+                }}
+            />
         </div>
     );
 }
