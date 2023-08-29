385b3a1ff6092b08b9c07a9e9457ab07a821e431
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-08-29 20:43:49 +0545

    Add customized mui-checkbox

---
+++ webapp/src/Components/Common/Input/CheckBox.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Checkbox, CheckboxProps } from '@mui/material';
+
+export default function CheckBox({ ...props }: CheckboxProps) {
+    return (
+        <Checkbox
+            className="!p-0 !border !border-new-black-300"
+            sx={{
+                color: '#DBDBDB',
+                width: '24px',
+                height: '24px',
+                border: '1px',
+                padding: '0px',
+                '&.Mui-disabled': {
+                    border: '1px',
+                    padding: '0px',
+                    color: '#DBDBDB'
+                }
+            }}
+            {...props}
+        />
+    );
+}
