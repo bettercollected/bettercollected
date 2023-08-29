20e43570a446d4abcbd8d145f15ea24e252e98d9
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-08-29 10:49:55 +0545

    Add attention text

--- webapp/src/pages/[workspace_name]/dashboard/forms/consent/create.tsx
+++ webapp/src/pages/[workspace_name]/dashboard/forms/consent/create.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import BackButtonMenuBar from '@Components/Common/BackButtonMenuBar';
+import AttentionText from '@Components/Consent/AttentionText';
 import ConsentInformationPanel from '@Components/Consent/ConsentInformationPanel';
 import CreateConsentForm from '@Components/Consent/Form/CreateConsentForm';
 
@@ -10,9 +11,12 @@ export default function CreateConsent() {
     return (
         <Layout isCustomDomain={false} isClientDomain={false} showNavbar={true} hideMenu={false} showAuthAccount={true} className="!p-0 !bg-white flex flex-col !min-h-calc-68">
             <div className="flex min-w-screen">
-                <div>
-                    <BackButtonMenuBar text="Back to Form" />
-                    <CreateConsentForm className="mt-20 pb-20" />
+                <BackButtonMenuBar text="Back to Form" />
+                <div className="mt-12">
+                    <div className="ml-[267px] w-[508px] mt-6">
+                        <AttentionText className="mt-12" text={`Design your form responder's consent page`} />
+                        <CreateConsentForm className="mt-10 pb-20" />
+                    </div>
                 </div>
                 <ConsentInformationPanel />
             </div>
