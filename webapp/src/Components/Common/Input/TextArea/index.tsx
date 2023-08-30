9f5c77ddd28a2eca37f3ca775f23d6077de1f48a
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-08-30 13:34:47 +0545

    Make consent page responsive

--- webapp/src/Components/Common/Input/TextArea/index.tsx
+++ webapp/src/Components/Common/Input/TextArea/index.tsx
@@ -4,7 +4,7 @@ import TextareaAutosize from '@mui/base/TextareaAutosize';
 import { TextareaAutosizeProps } from '@mui/material';
 
 const TextArea = React.forwardRef<HTMLTextAreaElement, TextareaAutosizeProps>(({ value, onChange, ...inputProps }, ref) => {
-    return <TextareaAutosize className="w-full rounded-md border-black-300 p-3  !text-black-800 placeholder:text-black-400 " disabled={false} ref={ref} aria-label="text-area" minRows={3} value={value} onChange={onChange} {...inputProps} />;
+    return <TextareaAutosize className="w-full rounded-md border-black-300 p-3  !text-black-800 placeholder:text-black-400 xs:text-xs" disabled={false} ref={ref} aria-label="text-area" minRows={3} value={value} onChange={onChange} {...inputProps} />;
 });
 
 TextArea.displayName = 'TextArea';
--- webapp/src/pages/[workspace_name]/dashboard/forms/consent/create.tsx
+++ webapp/src/pages/[workspace_name]/dashboard/forms/consent/create.tsx
@@ -9,17 +9,15 @@ import Layout from '@app/layouts/_layout';
 
 export default function CreateConsent() {
     return (
-        <Layout isCustomDomain={false} isClientDomain={false} showNavbar={true} hideMenu={false} showAuthAccount={true} className="!p-0 !bg-white flex flex-col !min-h-calc-68">
-            <div className="flex min-w-screen">
-                <BackButtonMenuBar text="Back to Form" />
-                <div className="mt-12">
-                    <div className="ml-[267px] w-[508px] mt-6">
-                        <AttentionText className="mt-12" text={`Design your form responder's consent page`} />
-                        <ConsentBuilder className="mt-10 pb-20" />
-                    </div>
+        <div className="flex min-w-screen bg-white">
+            <BackButtonMenuBar text="Back to Form" />
+            <div className="mt-12">
+                <div className="mx-[15px] sm:mx-[40px] md:ml-[120px] xl:ml-[267px] w-fit md:w-[508px] mt-6">
+                    <AttentionText className="mt-12" text={`Design your form responder's consent page`} />
+                    <ConsentBuilder className="mt-10 pb-20" />
                 </div>
-                <ConsentInformationPanel />
             </div>
-        </Layout>
+            <ConsentInformationPanel />
+        </div>
     );
 }
