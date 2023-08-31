746adb181782f8ceda44c2568a72c8a1736479c8
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-08-31 07:48:08 +0545

    Add data access details in consent page

--- webapp/src/Components/Consent/Form/ConsentForm.tsx
+++ webapp/src/Components/Consent/Form/ConsentForm.tsx
@@ -6,11 +6,13 @@ import cn from 'classnames';
 
 import { useFullScreenModal } from '@app/components/modal-views/full-screen-modal-context';
 import { formPurpose } from '@app/data/consent';
+import { StandardFormDto } from '@app/models/dtos/form';
 import { ConsentCategoryType, ConsentType } from '@app/models/enums/consentEnum';
 import { OnlyClassNameInterface } from '@app/models/interfaces';
 import { IConsentOption } from '@app/models/types/consentTypes';
 import { setAddConsent, setPrivacyPoilicy, setResponderRights } from '@app/store/consent/actions';
 import { useGetAllWorkspaceConsentsQuery } from '@app/store/consent/api';
+import { consent } from '@app/store/consent/consentSlice';
 import { selectConsentState } from '@app/store/consent/selectors';
 import { selectForm } from '@app/store/forms/slice';
 import { useAppDispatch, useAppSelector } from '@app/store/hooks';
@@ -20,35 +22,56 @@ import ConsentField from './ConsentField';
 
 interface ConsentBuilderProps extends OnlyClassNameInterface {
     onFormSubmit: any;
+    form: StandardFormDto;
 }
 
-export default function ConsentForm({ className, onFormSubmit }: ConsentBuilderProps) {
-    const formState = useAppSelector(selectForm);
+export default function ConsentForm({ className, onFormSubmit, form }: ConsentBuilderProps) {
     const dispatch = useAppDispatch();
     const { closeModal } = useFullScreenModal();
+    const workspace = useAppSelector(selectWorkspace);
 
     const getFilteredConsents = (category: ConsentCategoryType) => {
-        debugger;
-        return formState.consent.map((consent, idx) => consent?.category === category && <ConsentField key={consent.consentId} className={`${idx === 0 && 'border-y'}`} consent={consent} />);
+        return form.consent.map((consent, idx) => consent?.category === category && <ConsentField key={consent.consentId} className={`${idx === 0 && 'border-y'}`} consent={consent} />);
     };
-    const formPurposeDetails = (
-        <>
-            <div>
-                <div className="h4-new pb-5 xs:pb-[17px]">Purpose of this form:</div>
-                {getFilteredConsents(formPurpose.category)}
-            </div>
-        </>
-    );
 
-    const responderRightDetails = (
+    const dataAccessDetails = (
         <>
             <div className="space-y-5">
-                <div className="h4-new">{`YourRights`}</div>
-                <div className="h6-new">You can request for deletion of your data at any time</div>
+                <div className="h4-new">{`Who can access your data?`}</div>
+                <div className="border-y border-new-black-300 py-5 space-y-2">
+                    <div className="h6-new">{workspace.workspaceName} with 10 members</div>
+                    <ul className="space-y-3 list-disc !px-5">
+                        <li className="p2">We will only collect and use your data as described in privacy policy.</li>
+                        <li className="p2">We will not sell or share your data with any other third parties without your consent.</li>
+                        <li className="p2">We will take all reasonable steps to protect your data from unauthorized access, use, or disclosure.</li>
+                    </ul>
+                </div>
             </div>
         </>
     );
+    const renderResponderRights = () => {
+        const responderRight = form.consent.filter((consent) => consent.category === ConsentCategoryType.RespondersRights).length !== 0;
+        if (responderRight) {
+            return (
+                <div className="space-y-5">
+                    <div className="h4-new">{`Your Rights`}</div>
+                    <div className="h6-new border-y border-new-black-300 py-5">You can request for deletion of your data at any time</div>
+                </div>
+            );
+        }
+    };
 
+    const renderFormPurposes = () => {
+        const formPurposes = form.consent.filter((consent) => consent.category === ConsentCategoryType.PurposeOfTheForm).length !== 0;
+        if (formPurposes) {
+            return (
+                <div>
+                    <div className="h4-new pb-5">Purpose of this form:</div>
+                    {getFilteredConsents(formPurpose.category)}
+                </div>
+            );
+        }
+    };
     const onSubmit = async (event: any) => {
         event.preventDefault();
         try {
@@ -62,14 +85,15 @@ export default function ConsentForm({ className, onFormSubmit }: ConsentBuilderP
             <div className="space-y-20 xs:space-y-[70px]">
                 <div className="space-y-4">
                     <div className="h4">Form Purpose and Data Usage</div>
-                    <div className="p2 !text-new-black-800 xs:!text-xs">
+                    <div className="p2 !text-new-black-800 ">
                         {`We want to make sure you're fully informed about how your data will be used
 before you proceed with our form. Our commitment to transparency means
 that we've included a consent page to provide you with important details.`}
                     </div>
                 </div>
-                {formPurposeDetails}
-                {responderRightDetails}
+                {renderFormPurposes()}
+                {renderResponderRights()}
+                {dataAccessDetails}
             </div>
             <FormButton className="w-[192px] mt-[60px]">Done</FormButton>
         </form>
