a5435035f066a6103deaf606e169801ff7b90bb7
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-08-31 09:37:28 +0545

    Customize HintBox acc. to type

--- webapp/src/Components/Consent/Form/HintBox.tsx
+++ webapp/src/Components/Consent/Form/HintBox.tsx
@@ -7,17 +7,22 @@ import cn from 'classnames';
 import { Hint } from '@app/components/icons/hint';
 import { OnlyClassNameInterface } from '@app/models/interfaces';
 
+type HintBoxSize = 'small' | 'normal';
+
 interface HintBoxProps extends OnlyClassNameInterface {
     title: string;
     description?: string;
+    size?: HintBoxSize;
+    iconColor?: string;
 }
-export default function HintBox({ title, description, className }: HintBoxProps) {
+export default function HintBox({ title, description, className, size = 'normal', iconColor = '#FFA716' }: HintBoxProps) {
+    const titleClassName = size === 'small' && 'leading-6 font-semibold';
     return (
         <div className={cn('rounded-lg p-5 bg-new-black-200', className)}>
             <div className="relative">
-                <Hint className="absolute" fillColor="#FFA716" />
+                <Hint className="absolute" fillColor={iconColor} />
                 <div className="ml-10 space-y-3">
-                    <div className="h5-new">{title}</div>
+                    <div className={cn('h5-new', titleClassName)}>{title}</div>
                     <div className="p2 !text-new-black-800">{description}</div>
                 </div>
             </div>
