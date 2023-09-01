82689ae5386a3903dcc50551e16ad1349b5efda1
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-09-01 17:05:05 +0545

    Move actions to popover in form builder

--- webapp/src/Components/FormBuilder/MenuBar/index.tsx
+++ webapp/src/Components/FormBuilder/MenuBar/index.tsx
@@ -11,14 +11,14 @@ import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
 import Toolbar from '@mui/material/Toolbar';
-import { alpha, styled } from '@mui/material/styles';
-
-import { useFullScreenModal } from '@app/components/modal-views/full-screen-modal-context';
+import {alpha, styled} from '@mui/material/styles';
 import useBuilderTranslation from '@app/lib/hooks/use-builder-translation';
+import {useBreakpoint} from "@app/lib/hooks/use-breakpoint";
+import CustomPopover from "@Components/Common/CustomPopover";
 import SettingsIcon from '@Components/Common/Icons/Settings';
 import InfoIcon from '@Components/Common/Icons/FormBuilder/infoIcon';
 
-const Search = styled('div')(({ theme }) => ({
+const Search = styled('div')(({theme}) => ({
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
@@ -34,7 +34,7 @@ const Search = styled('div')(({ theme }) => ({
     }
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled('div')(({theme}) => ({
     padding: theme.spacing(0, 2),
     height: '100%',
     position: 'absolute',
@@ -44,7 +44,7 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
     justifyContent: 'center'
 }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
+const StyledInputBase = styled(InputBase)(({theme}) => ({
     color: 'inherit',
     '& .MuiInputBase-input': {
         padding: theme.spacing(1, 1, 1, 0),
@@ -70,15 +70,72 @@ interface IFormBuilderMenuBarProps {
     isUpdating?: boolean;
 }
 
-const optionButtonClassName = 'flex flex-col text-black-700 !p-2 !md:p-3 !px-3 !md:px-5 border-1 h-full hover-none border-solid border-gray-500 md:gap-2 md:flex-row rounded-none ';
+const optionButtonClassName = 'flex text-black-700  text-sm lg:text-normal !lg:p-2 !lg:p-3 lg:!px-3 !lg:px-5 border-1 h-full  lg:w-fit w-full hover-none border-solid border-gray-500 md:gap-2 rounded-none ';
 
-export default function FormBuilderMenuBar({ onInsert, onAddFormLogo, onAddFormCover, onPreview, onFormPublish, onClickSettings, onClickTips, isUpdating }: IFormBuilderMenuBarProps) {
-    const { t } = useBuilderTranslation();
-    const { openModal } = useFullScreenModal();
+export default function FormBuilderMenuBar({
+                                               onInsert,
+                                               onAddFormLogo,
+                                               onAddFormCover,
+    onClickSettings, onClickTips,
+                                               onPreview,
+                                               onFormPublish,
+                                               isUpdating
+                                           }: IFormBuilderMenuBarProps) {
+    const {t} = useBuilderTranslation();
+
+    const breakpoint = useBreakpoint()
+
+
+    const Actions = () => (
+        <Toolbar className=" !px-0 lg:px-6 divide-y divide-black-200 lg:divide-y-0  flex flex-col lg:flex-row body4 w-fit justify-center">
+            <Divider orientation="vertical" className="hidden lg:flex" flexItem/>
+            <IconButton color="inherit" className={optionButtonClassName} onClick={onInsert}>
+                <PlusIcon/>
+                <span className=" text-black-700">{t('INSERT.DEFAULT')}</span>
+            </IconButton>
+            <Divider orientation="vertical" className="hidden lg:flex" flexItem/>
+            <IconButton color="inherit" className={optionButtonClassName} onClick={onAddFormLogo}>
+                <CircleOutlinedIcon/>
+                <span className="text-black-700 ">Logo</span>
+            </IconButton>
+            <Divider orientation="vertical" className="hidden lg:flex" flexItem/>
+            <IconButton color="inherit" className={optionButtonClassName} onClick={onAddFormCover}>
+                <CoverIcon/>
+                <span className="text-black-700">Cover</span>
+            </IconButton>
+            <Divider orientation="vertical" className="hidden lg:flex" flexItem/>
+            <div className="hidden lg:flex lg:w-20"/>
+
+            <IconButton size="small" color="inherit" className={optionButtonClassName} onClick={onClickTips}>
+                <InfoIcon />
+                <span className=" text-black-700 ">Tips</span>
+            </IconButton>
+            <Divider orientation="vertical" flexItem />
+            {/* <IconButton size="small" color="inherit" className={optionButtonClassName} onClick={onClickSettings}>
+                    <SettingsIcon />
+                    <span className=" text-black-700 ">Settings</span>
+                </IconButton> */}
+            <Divider orientation="vertical" flexItem />
+            <Divider orientation="vertical" className="hidden lg:flex" flexItem/>
+            <IconButton color="inherit" className={optionButtonClassName} onClick={onPreview}>
+                <VisibilityOutlinedIcon/>
+                <span className=" text-black-700 ">{t('PREVIEW.DEFAULT')}</span>
+            </IconButton>
+            {/*</Tooltip>*/}
+            <Divider orientation="vertical" className="hidden lg:flex" flexItem/>
+            {/*<Tooltip title={t('PUBLISH.DEFAULT')}>*/}
+            <IconButton color="inherit" className={optionButtonClassName} onClick={onFormPublish}>
+                {isUpdating ? <LoadingIcon/> : <PublishIcon/>}
+                <span className=" text-black-700">{t('PUBLISH.DEFAULT')}</span>
+            </IconButton>
+            {/*</Tooltip>*/}
+            <Divider orientation="vertical" className="hidden lg:flex" flexItem/>
+        </Toolbar>
+    )
     return (
         <AppBar
             position="static"
-            className="border-b-[1px] !min-h-[40px] bg-gradient-to-b from-white to-white/80 sticky top-[68px] z-[1000] shadow-inner backdrop-blur border-black-400"
+            className="border-b-[1px] !min-h-[40px] bg-gradient-to-b from-white to-white/80 sticky items-center justify-center flex top-[68px] z-[1000] shadow-inner backdrop-blur border-black-400"
             sx={{
                 borderRadius: 0,
                 background: 'inherit',
@@ -86,54 +143,11 @@ export default function FormBuilderMenuBar({ onInsert, onAddFormLogo, onAddFormC
                 boxShadow: 'inherit'
             }}
         >
-            <Toolbar sx={{ height: '40px !important' }} className="flex body4 w-full justify-center">
-                <Divider orientation="vertical" flexItem />
-                {/*<Tooltip title={t('INSERT.A_FIELD')}>*/}
-                <IconButton size="small" color="inherit" className={optionButtonClassName} onClick={onInsert}>
-                    <PlusIcon />
-                    <span className=" text-black-700">{t('INSERT.DEFAULT')}</span>
-                </IconButton>
-                {/*</Tooltip>*/}
-                <Divider orientation="vertical" flexItem />
-                <IconButton size="small" color="inherit" className={optionButtonClassName} onClick={onAddFormLogo}>
-                    <CircleOutlinedIcon />
-                    <span className="hidden text-black-700 lg:flex">Logo</span>
-                </IconButton>
-                <Divider orientation="vertical" flexItem />
-                <IconButton size="small" color="inherit" className={optionButtonClassName} onClick={onAddFormCover}>
-                    <CoverIcon />
-                    <span className="hidden text-black-700 lg:flex">Cover</span>
-                </IconButton>
-                <Divider orientation="vertical" flexItem />
-                <div className="hidden md:flex md:w-20" />
-                <Divider className="hidden sm:flex" orientation="vertical" flexItem />
-                <Divider orientation="vertical" flexItem />
-
-                {/*<Tooltip title={t('PREVIEW.DEFAULT')}>*/}
-                <IconButton size="small" color="inherit" className={optionButtonClassName} onClick={onClickTips}>
-                    <InfoIcon />
-                    <span className=" text-black-700 ">Tips</span>
-                </IconButton>
-                <Divider orientation="vertical" flexItem />
-                {/* <IconButton size="small" color="inherit" className={optionButtonClassName} onClick={onClickSettings}>
-                    <SettingsIcon />
-                    <span className=" text-black-700 ">Settings</span>
-                </IconButton> */}
-                <Divider orientation="vertical" flexItem />
-                <IconButton size="small" color="inherit" className={optionButtonClassName} onClick={onPreview}>
-                    <VisibilityOutlinedIcon />
-                    <span className=" text-black-700 ">{t('PREVIEW.DEFAULT')}</span>
-                </IconButton>
-                {/*</Tooltip>*/}
-                <Divider orientation="vertical" flexItem />
-                {/*<Tooltip title={t('PUBLISH.DEFAULT')}>*/}
-                <IconButton size="small" color="inherit" className={optionButtonClassName} onClick={onFormPublish}>
-                    {isUpdating ? <LoadingIcon /> : <PublishIcon />}
-                    <span className=" text-black-700">{t('PUBLISH.DEFAULT')}</span>
-                </IconButton>
-                {/*</Tooltip>*/}
-                <Divider orientation="vertical" flexItem />
-            </Toolbar>
+            {
+                ["2xs", "xs", "sm", "md"].indexOf(breakpoint) !== -1 ?
+                    <CustomPopover content={<Actions/>}>Actions</CustomPopover>
+                    : <Actions/>
+            }
         </AppBar>
     );
 }
