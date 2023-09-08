4fa066dfcf8926a42826c96c4381e3646667fb55
Sital999 <sitalnagarkoti123@gmail.com>
2023-09-08 16:11:30 +0545

    Update Icons svg

--- webapp/src/Components/Common/Icons/FormBuilderSlashIcon.tsx
+++ webapp/src/Components/Common/Icons/FormBuilderSlashIcon.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 export default function FormBuilderSlashIcon(props: React.SVGAttributes<{}>) {
     return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-            <rect width="24" height="24" rx="2" fill="#D8E8FF" />
-            <path d="M16 5L9 18" stroke="#0764EB" strokeLinecap="round" />
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 5L8 19" stroke="#4D4D4D" stroke-width="2" stroke-linecap="round" />
         </svg>
     );
 }
