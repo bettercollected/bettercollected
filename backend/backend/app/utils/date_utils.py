a5baf94e729df42be71770f17846f1d22a205378
Nabin Kawan <nabin.kawan.9@gmail.com>
2023-09-11 14:24:35 +0545

    Consider expiry time of form submission (+24 hours)

--- backend/backend/app/utils/date_utils.py
+++ backend/backend/app/utils/date_utils.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
 
-def get_formatted_date_from_str(dt_str, format_schema="%Y-%m-%d"):
+def get_formatted_date_from_str(dt_str, format_schema="%Y-%m-%d:%H:%M:%S"):
     return datetime.strptime(dt_str, format_schema)
