f68e1c9bb39f786a8d238cfa9fbdbb501b92a428
Sital999 <sitalnagarkoti123@gmail.com>
2023-09-26 16:29:27 +0545

    Update setting tab panel view

--- webapp/src/components/form/settings.tsx
+++ webapp/src/components/form/settings.tsx
@@ -10,8 +10,8 @@ export default function FormSettings() {
     const { t } = useTranslation();
     return (
         <div className="md:max-w-[740px]">
-            <p className="body1 !leading-none">{t(localesCommon.settings)}</p>
-            <p className="sm:max-w-[301px] body4 mt-4 mb-8 !text-black-700">{t(formConstant.settings.description)}</p>
+            <p className="sh1 !text-black-800 !leading-none">{t(localesCommon.settings)}</p>
+            <p className="w-full body4 mt-4 mb-12 !text-black-700">Effortlessly customize form links and manage their forms effectively.</p>
             <FormSettingsTab />
         </div>
     );
