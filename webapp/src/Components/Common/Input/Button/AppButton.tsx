2d8427418ac0ef06cf2e64fd0970214f1528455e
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-09-26 15:04:09 +0545

    Refactor App Button according to new design

--- webapp/src/Components/Common/Input/Button/AppButton.tsx
+++ webapp/src/Components/Common/Input/Button/AppButton.tsx
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 
-import { CircularProgress } from '@mui/material';
+import {ButtonSize, ButtonVariant} from '@Components/Common/Input/Button/AppButtonProps';
+import {CircularProgress} from '@mui/material';
 import cn from 'classnames';
 
 interface AppButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     isLoading?: boolean;
+    variant?: ButtonVariant;
+    size?: ButtonSize;
+    icon?: ReactNode
 }
-export default function AppButton({ children, className, disabled, isLoading, ...buttonProps }: AppButtonProps) {
+
+export default function AppButton({
+                                      children,
+                                      className,
+                                      disabled,
+                                      isLoading,
+                                      icon,
+                                      variant = ButtonVariant.Primary,
+                                      size = ButtonSize.Small,
+                                      ...buttonProps
+                                  }: AppButtonProps) {
+    const getClassNamesForVariant = () => {
+        switch (variant) {
+            case ButtonVariant.Primary:
+                return disabled ? 'bg-black-300 text-black-500' : 'bg-brand-500 text-white hover:bg-brand-600 active:bg-brand-800 focus:ring';
+            case ButtonVariant.Secondary:
+                return disabled ? 'bg-black-300 text-black-500' : 'bg-black-800 text-white hover:bg-black-900 focus:ring-blue-500 focus-ring active:bg-black-900';
+            case ButtonVariant.Tertiary:
+                return disabled ? 'bg-transparent border text-black-500 border-black-300' : 'text-blue-500 border border-brand-500 hover:bg-brand-500 hover:text-white focus:ring focus:ring-blue-500 active:bg-brand-600';
+            case ButtonVariant.Danger:
+                return disabled ? 'bg-black-300 text-black-500' : 'bg-red-400 text-white hover:bg-red-500 ';
+            case ButtonVariant.Ghost:
+                return disabled ? 'bg-transparent text-black-500' : 'text-brand-500 border border-transparent active: hover:bg-black-200 focus:border active:bg-black-300 focus:border-brand-500';
+        }
+    };
+
+    const getClassnamesForSize = () => {
+        switch (size) {
+            case ButtonSize.Medium:
+                return 'h-[48px] text-sm';
+            case ButtonSize.Big:
+                return 'h-[64px] text-normal';
+            case ButtonSize.Small:
+                return 'h-[36px] text-sm';
+            case ButtonSize.Tiny:
+                return 'h-[28px] text-sm';
+        }
+    };
+
     return (
-        <button disabled={isLoading || disabled} className={cn('py-2 px-4 text-white rounded w-fit !text-[14px] bg-black-900  !font-semibold ', disabled ? 'cursor-not-allowed' : 'cursor-pointer', className)} {...buttonProps}>
-            {isLoading ? <CircularProgress size={20} color="inherit" /> : children}
+        <button disabled={isLoading || disabled}
+                className={cn('rounded gap-2 !min-w-fit flex items-center px-5', disabled ? 'cursor-not-allowed' : 'cursor-pointer', getClassnamesForSize(), getClassNamesForVariant(), className)} {...buttonProps}>
+            {!isLoading && icon}
+            {!disabled && isLoading && <CircularProgress size={14} color="inherit"/>}
+            {
+                children
+            }
         </button>
     );
 }
