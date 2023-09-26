39c786e44f3b1e7f0ba1b63b7355df510ef0a8c1
Sital999 <sitalnagarkoti123@gmail.com>
2023-09-26 16:31:51 +0545

    Update paramtabs acc to new design

--- webapp/src/components/dashboard/form-tab-content.tsx
+++ webapp/src/components/dashboard/form-tab-content.tsx
@@ -4,7 +4,7 @@ import FormRenderer from '../form/renderer/form-renderer';
 
 export const FormTabContent = ({ form }: any) => {
     return (
-        <div className="w-full rounded bg-white max-w-[700px] flex items-center ">
+        <div className="w-full rounded bg-white flex items-center ">
             <FormRenderer form={form} enabled={false} isDisabled={true} />
         </div>
     );
