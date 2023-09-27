0a94431f2c1999837442d52557466d82c39dc181
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-09-27 11:41:18 +0545

    Refactor Workspace Form card for dashboard

--- webapp/src/Components/Common/CreateFormButton.tsx
+++ webapp/src/Components/Common/CreateFormButton.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
-import { useTranslation } from 'next-i18next';
-import { useRouter } from 'next/router';
-
-import Button from '@app/components/ui/button';
+import {useTranslation} from 'next-i18next';
+import {useRouter} from 'next/router';
 import environments from '@app/configs/environments';
-import { builderConstants } from '@app/constants/locales/form-builder';
-import { setBuilderState } from '@app/store/form-builder/actions';
-import { initBuilderState } from '@app/store/form-builder/builderSlice';
-import { useAppDispatch, useAppSelector } from '@app/store/hooks';
-import { useCreateFormMutation } from '@app/store/workspaces/api';
-import { selectWorkspace } from '@app/store/workspaces/slice';
+import {builderConstants} from '@app/constants/locales/form-builder';
+import {setBuilderState} from '@app/store/form-builder/actions';
+import {initBuilderState} from '@app/store/form-builder/builderSlice';
+import {useAppDispatch, useAppSelector} from '@app/store/hooks';
+import {useCreateFormMutation} from '@app/store/workspaces/api';
+import {selectWorkspace} from '@app/store/workspaces/slice';
+import AppButton from "@Components/Common/Input/Button/AppButton";
+import {ButtonVariant} from "@Components/Common/Input/Button/AppButtonProps";
 
 export default function CreateFormButton() {
     const router = useRouter();
     const workspace = useAppSelector(selectWorkspace);
-    const { t: builderTranslation } = useTranslation('builder');
-    const [postCreateForm, { isLoading: posting }] = useCreateFormMutation();
+    const {t: builderTranslation} = useTranslation('builder');
+    const [postCreateForm, {isLoading: posting}] = useCreateFormMutation();
     const dispatch = useAppDispatch();
     const onClickButton = async () => {
         const formData = new FormData();
@@ -28,21 +28,22 @@ export default function CreateFormButton() {
         postReq.consent = [];
         postReq.settings = {};
         formData.append('form_body', JSON.stringify(postReq));
-        const apiObj: any = { workspaceId: workspace.id, body: formData };
+        const apiObj: any = {workspaceId: workspace.id, body: formData};
         const response: any = await postCreateForm(apiObj);
 
         if (response?.data) {
             router.push(`/${workspace?.workspaceName}/dashboard/forms/${response?.data?.formId}/edit`);
-            dispatch(setBuilderState({ isFormDirty: false }));
+            dispatch(setBuilderState({isFormDirty: false}));
         }
     };
 
     return (
         <>
             {environments.ENABLE_FORM_BUILDER && (
-                <Button variant="solid" onClick={onClickButton} isLoading={posting}>
+                <AppButton className="min-w-[160px]" variant={ButtonVariant.Primary} onClick={onClickButton}
+                           isLoading={posting}>
                     {builderTranslation(builderConstants.createForm)}
-                </Button>
+                </AppButton>
             )}
         </>
     );
