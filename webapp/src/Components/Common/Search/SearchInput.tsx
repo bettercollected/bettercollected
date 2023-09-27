a7ed5ad90c034707fa21cad6cb3105e2bf75cf4d
Sital999 <sitalnagarkoti123@gmail.com>
2023-09-27 13:07:23 +0545

    Update bg of SearchInput

--- webapp/src/Components/Common/Search/SearchInput.tsx
+++ webapp/src/Components/Common/Search/SearchInput.tsx
@@ -1,24 +1,25 @@
-import {useMemo} from 'react';
+import { useMemo } from 'react';
 
-import {useTranslation} from 'next-i18next';
+import { useTranslation } from 'next-i18next';
 
-import {debounce} from 'lodash';
+import { debounce } from 'lodash';
 
-import {InputAdornment} from '@mui/material';
+import { InputAdornment } from '@mui/material';
 import TextField from '@mui/material/TextField';
+import cn from 'classnames';
 
-import {StyledTextField} from '@app/components/dashboard/workspace-forms-tab-content';
-import {SearchIcon} from '@app/components/icons/search';
-import {placeHolder} from '@app/constants/locales/placeholder';
-import cn from "classnames";
+import { StyledTextField } from '@app/components/dashboard/workspace-forms-tab-content';
+import { SearchIcon } from '@app/components/icons/search';
+import { placeHolder } from '@app/constants/locales/placeholder';
 
 interface ISearchInputProps {
     handleSearch: (event: any) => void;
-    className?: string
+    className?: string;
+    placeholder?: string;
 }
 
-export default function SearchInput({handleSearch, className}: ISearchInputProps) {
-    const {t} = useTranslation();
+export default function SearchInput({ handleSearch, className, placeholder }: ISearchInputProps) {
+    const { t } = useTranslation();
     const debouncedResults = useMemo(() => {
         return debounce(handleSearch, 500);
     }, []);
@@ -26,19 +27,20 @@ export default function SearchInput({handleSearch, className}: ISearchInputProps
     return (
         <StyledTextField>
             <TextField
-                sx={{height: '46px', padding: 0}}
+                sx={{ height: '40px', padding: 0 }}
                 size="small"
                 name="search-input"
-                placeholder={t(placeHolder.search)}
+                placeholder={placeholder ? placeholder : t(placeHolder.search)}
                 onChange={debouncedResults}
                 className={cn('w-full bg-white focus:bg-white active:bg-white', className)}
                 InputProps={{
                     sx: {
-                        paddingLeft: '16px'
+                        paddingLeft: '16px',
+                        backgroundColor: '#DBDBDB'
                     },
-                    endAdornment: (
-                        <InputAdornment sx={{padding: 0}} position="end">
-                            <SearchIcon/>
+                    startAdornment: (
+                        <InputAdornment sx={{ padding: 0 }} position="start">
+                            <SearchIcon className=' stroke-black stroke-2'/>
                         </InputAdornment>
                     )
                 }}
