b5829edf9482bf9f471ceaa29be47c7b0c53d980
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-09-27 16:16:04 +0545

    Fix table styles

--- webapp/src/Components/Common/Search/SearchInput.tsx
+++ webapp/src/Components/Common/Search/SearchInput.tsx
@@ -1,16 +1,16 @@
-import { useMemo } from 'react';
+import {useMemo} from 'react';
 
-import { useTranslation } from 'next-i18next';
+import {useTranslation} from 'next-i18next';
 
-import { debounce } from 'lodash';
+import {debounce} from 'lodash';
 
-import { InputAdornment } from '@mui/material';
+import {InputAdornment} from '@mui/material';
 import TextField from '@mui/material/TextField';
 import cn from 'classnames';
 
-import { StyledTextField } from '@app/components/dashboard/workspace-forms-tab-content';
-import { SearchIcon } from '@app/components/icons/search';
-import { placeHolder } from '@app/constants/locales/placeholder';
+import {StyledTextField} from '@app/components/dashboard/workspace-forms-tab-content';
+import {SearchIcon} from '@app/components/icons/search';
+import {placeHolder} from '@app/constants/locales/placeholder';
 
 interface ISearchInputProps {
     handleSearch: (event: any) => void;
@@ -18,8 +18,8 @@ interface ISearchInputProps {
     placeholder?: string;
 }
 
-export default function SearchInput({ handleSearch, className, placeholder }: ISearchInputProps) {
-    const { t } = useTranslation();
+export default function SearchInput({handleSearch, className, placeholder}: ISearchInputProps) {
+    const {t} = useTranslation();
     const debouncedResults = useMemo(() => {
         return debounce(handleSearch, 500);
     }, []);
@@ -27,7 +27,7 @@ export default function SearchInput({ handleSearch, className, placeholder }: IS
     return (
         <StyledTextField>
             <TextField
-                sx={{ height: '40px', padding: 0 }}
+                sx={{height: '40px', padding: 0}}
                 size="small"
                 name="search-input"
                 placeholder={placeholder ? placeholder : t(placeHolder.search)}
@@ -36,10 +36,10 @@ export default function SearchInput({ handleSearch, className, placeholder }: IS
                 InputProps={{
                     sx: {
                         paddingLeft: '16px',
-                        backgroundColor: '#DBDBDB'
+                        backgroundColor: '#EEEEEE'
                     },
                     startAdornment: (
-                        <InputAdornment sx={{ padding: 0 }} position="start">
+                        <InputAdornment sx={{padding: 0}} position="start">
                             <SearchIcon className=' stroke-black stroke-2'/>
                         </InputAdornment>
                     )
