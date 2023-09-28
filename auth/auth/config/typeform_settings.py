7c0e56ea5857fe95b71706f23d392f55a8c0ca05
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-09-28 12:19:42 +0545

    Make login options optional

--- auth/auth/config/typeform_settings.py
+++ auth/auth/config/typeform_settings.py
@@ -2,13 +2,13 @@ from pydantic import BaseSettings
 
 
 class TypeformSettings(BaseSettings):
-    auth_uri = "https://api.typeform.com/oauth/authorize?state={state}&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}"
-    token_uri = "https://api.typeform.com/oauth/token"
-    scope = "accounts:read"
-    client_id = ""
-    client_secret = ""
-    redirect_uri = ""
-    api_uri = "https://api.typeform.com"
+    auth_uri: str = "https://api.typeform.com/oauth/authorize?state={state}&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}"
+    token_uri: str = "https://api.typeform.com/oauth/token"
+    scope: str = "accounts:read"
+    client_id: str = ""
+    client_secret: str = ""
+    redirect_uri: str = ""
+    api_uri: str = "https://api.typeform.com"
 
     class Config:
         env_prefix = "TYPEFORM_"
