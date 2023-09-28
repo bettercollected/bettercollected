3c7369c0aeefcce01e365471d98aa2363c56fe91
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-09-28 12:19:42 +0545

    Make login options optional

--- auth/auth/config/stripe.py
+++ auth/auth/config/stripe.py
@@ -2,12 +2,12 @@ from pydantic import BaseSettings
 
 
 class StripeSettings(BaseSettings):
-    product_id: str
-    secret: str
-    webhook_secret: str
-    cancel_url: str
-    success_url: str
-    return_url: str
+    product_id: str = ""
+    secret: str = ""
+    webhook_secret: str = ""
+    cancel_url: str = ""
+    success_url: str = ""
+    return_url: str = ""
 
     class Config:
         env_prefix = "STRIPE_"
