acb11ac2f3b11ed57177f062a8b77797e5ce7435
Sital999 <sitalnagarkoti123@gmail.com>
2023-09-29 13:58:46 +0545

    Add isDisabled and onClick on AppTextField

--- webapp/src/Components/Common/Input/AppTextField.tsx
+++ webapp/src/Components/Common/Input/AppTextField.tsx
@@ -1,16 +1,46 @@
-import React, { forwardRef } from 'react';
+import React, {forwardRef} from 'react';
 
-import { TextField, TextFieldProps } from '@mui/material';
+import {TextField, TextFieldProps} from '@mui/material';
 import cn from 'classnames';
 
 import useBuilderTranslation from '@app/lib/hooks/use-builder-translation';
 
-type AppTextFieldProps = TextFieldProps & { title?: string; required?: boolean; showIcon?: boolean; icon?: React.ReactNode; iconPosition?: 'start' | 'end'; isError?: boolean };
+type AppTextFieldProps = TextFieldProps & {
+    title?: string;
+    required?: boolean;
+    showIcon?: boolean;
+    icon?: React.ReactNode;
+    iconPosition?: 'start' | 'end';
+    isError?: boolean,
+    isDisabled?: boolean,
+    onClick?: () => void
+};
 
-const Description: React.FC<React.PropsWithChildren> = ({ children }) => <p className="p2 pb-2 !text-new-black-800">{children}</p>;
-const AppTextField: React.FC<AppTextFieldProps> & { Description: React.FC<React.PropsWithChildren> } = (props: AppTextFieldProps) => {
-    const { id, title, type = 'text', required = false, placeholder, multiline, inputProps, inputMode, className, showIcon = true, icon, children, iconPosition = 'start', isError, ...otherProps } = props;
-    const { t } = useBuilderTranslation();
+const Description: React.FC<React.PropsWithChildren> = ({children}) => <p
+    className="p2 pb-2 !text-new-black-800">{children}</p>;
+const AppTextField: React.FC<AppTextFieldProps> & {
+    Description: React.FC<React.PropsWithChildren>
+} = (props: AppTextFieldProps) => {
+    const {
+        id,
+        title,
+        type = 'text',
+        required = false,
+        placeholder,
+        multiline,
+        inputProps,
+        inputMode,
+        className,
+        showIcon = true,
+        icon,
+        children,
+        iconPosition = 'start',
+        isError,
+        isDisabled,
+        onClick,
+        ...otherProps
+    } = props;
+    const {t} = useBuilderTranslation();
 
     const getIcon = (position: 'start' | 'end') => {
         if (position === iconPosition) {
@@ -18,7 +48,7 @@ const AppTextField: React.FC<AppTextFieldProps> & { Description: React.FC<React.
         }
     };
     return (
-        <div className={cn('', className)}>
+        <div className={cn('', className)} onClick={onClick}>
             {title && (
                 <div className="h4-new mb-2">
                     {title} {required && <span className="text-pink ml-2">*</span>}
@@ -36,6 +66,7 @@ const AppTextField: React.FC<AppTextFieldProps> & { Description: React.FC<React.
                 variant="outlined"
                 multiline={multiline}
                 inputMode={inputMode || 'text'}
+                disabled={isDisabled}
                 inputProps={
                     inputProps || {
                         style: {
@@ -50,9 +81,13 @@ const AppTextField: React.FC<AppTextFieldProps> & { Description: React.FC<React.
                         }
                     }
                 }
-                InputProps={{ sx: { ':before': { content: 'none' } }, startAdornment: getIcon('start'), endAdornment: getIcon('end') }}
+                InputProps={{
+                    sx: {':before': {content: 'none'}},
+                    startAdornment: getIcon('start'),
+                    endAdornment: getIcon('end')
+                }}
                 size="small"
-                className={cn('!mb-0 !bg-white w-full !text-black-300 hover:shadow-input ')}
+                className={cn('!mb-0 !bg-white w-full !text-black-300 ')}
                 {...otherProps}
                 placeholder={placeholder || t('COMPONENTS.INPUT.END_ADORNMENT_PLACEHOLDER')}
                 sx={{
@@ -61,10 +96,10 @@ const AppTextField: React.FC<AppTextFieldProps> & { Description: React.FC<React.
                             borderColor: isError ? '#EA400E' : '#DBDBDB'
                         },
                         '&:hover fieldset': {
-                            borderColor: '#A2C5F8'
+                            borderColor: isDisabled ? '' : '#343A40'
                         },
                         '&.Mui-focused fieldset': {
-                            borderColor: '#A2C5F8',
+                            borderColor: '#B8E8FF',
                             boxShadow: '0px 0px 12px 0px rgba(7, 100, 235, 0.45)'
                         }
                     }
