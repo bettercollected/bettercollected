8c1b4ff7e89801bba47aca3ec3f0447c25729133
Sital999 <sitalnagarkoti123@gmail.com>
2023-09-29 14:08:23 +0545

    Add new column `Deleted On` on `Deletion_Request` on paramtabsView

--- webapp/src/components/badge/status-badge.tsx
+++ webapp/src/components/badge/status-badge.tsx
@@ -8,11 +8,11 @@ import { statusProps } from '@app/utils/validationUtils';
 
 export default function StatusBadge({ status, className = '' }: { status: string; className?: string }) {
     const { t } = useTranslation();
-    const { currentStatus, cName } = statusProps(status, t);
+    const { currentStatus, cName, dotCName } = statusProps(status, t);
 
     return (
-        <span className={`text-[9px] flex items-center gap-1 !h-[22px] body4  p-[6px] rounded-[50px] ${cName} ${className}`}>
-            <div className={`rounded-full !bg-black-800 !h-[6px] !w-[6px] ${cName}`} />
+        <span className={`text-[9px] flex items-center gap-1 !h-[22px] body4  p-4 rounded-[50px] ${cName} ${className}`}>
+            <div className={`rounded-full bg-black-800 !h-[6px] !w-[6px] ${dotCName}`} />
             {_.startCase(currentStatus || '')}
         </span>
     );
