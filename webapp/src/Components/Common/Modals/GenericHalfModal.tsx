11e80d54f30bb7fd2c769f932babe029751895d6
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-10-05 20:26:21 +0545

    Make a generic half screen modal

---
+++ webapp/src/Components/Common/Modals/GenericHalfModal.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from 'react';
+
+import AppButton from '@Components/Common/Input/Button/AppButton';
+import { ButtonSize, ButtonVariant } from '@Components/Common/Input/Button/AppButtonProps';
+import Divider from '@mui/material/Divider';
+
+import { Close } from '@app/components/icons/close';
+import { useModal } from '@app/components/modal-views/context';
+
+interface IModalWrapperProps {
+    headerTitle?: string;
+    title?: string;
+    subTitle?: string;
+    children?: ReactNode;
+    type?: 'delete' | 'confirmation';
+    positiveText?: string;
+    negativeText?: string;
+    positiveAction?: () => void;
+}
+
+export default function GenericHalfModal({ headerTitle, title, subTitle, type, positiveAction, positiveText, negativeText = 'Cancel', children }: IModalWrapperProps) {
+    const { closeModal } = useModal();
+    return (
+        <div className="flex flex-col bg-white rounded-md w-full min-w-[350px] max-w-[500px]">
+            <div className="p-4 flex items-center justify-between">
+                <span className="text-black-800 text-sm p2-new">{headerTitle}</span>
+                <Close onClick={closeModal} />
+            </div>
+            <Divider />
+            <div className="flex flex-col p-10">
+                {title && <span className="text-black-800 h2-new font-semibold">{title}</span>}
+                {subTitle && <span className="p2-new text-black-700"></span>}
+                {children}
+                <div className="flex w-full gap-4 mt-6">
+                    <AppButton className="flex-1" size={ButtonSize.Medium} onClick={closeModal} variant={ButtonVariant.Secondary}>
+                        {negativeText}
+                    </AppButton>
+                    <AppButton className="flex-1" size={ButtonSize.Medium} variant={type === 'delete' ? ButtonVariant.Danger : ButtonVariant.Primary} onClick={positiveAction}>
+                        {positiveText}
+                    </AppButton>
+                </div>
+            </div>
+        </div>
+    );
+}
--- webapp/src/components/form/delete-form-modal.tsx
+++ webapp/src/components/form/delete-form-modal.tsx
@@ -1,17 +1,18 @@
-import {useTranslation} from 'next-i18next';
-import {useRouter} from 'next/router';
+import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
 
-import {toast} from 'react-toastify';
+import AppButton from '@Components/Common/Input/Button/AppButton';
+import { ButtonVariant } from '@Components/Common/Input/Button/AppButtonProps';
+import GenericHalfModal from '@Components/Common/Modals/GenericHalfModal';
+import { toast } from 'react-toastify';
 
-import {Close} from '@app/components/icons/close';
-import {useModal} from '@app/components/modal-views/context';
-import {buttonConstant} from '@app/constants/locales/button';
-import {localesCommon} from '@app/constants/locales/common';
-import {toastMessage} from '@app/constants/locales/toast-message';
-import {useAppSelector} from '@app/store/hooks';
-import {useDeleteFormMutation} from '@app/store/workspaces/api';
-import AppButton from "@Components/Common/Input/Button/AppButton";
-import {ButtonVariant} from "@Components/Common/Input/Button/AppButtonProps";
+import { Close } from '@app/components/icons/close';
+import { useModal } from '@app/components/modal-views/context';
+import { buttonConstant } from '@app/constants/locales/button';
+import { localesCommon } from '@app/constants/locales/common';
+import { toastMessage } from '@app/constants/locales/toast-message';
+import { useAppSelector } from '@app/store/hooks';
+import { useDeleteFormMutation } from '@app/store/workspaces/api';
 
 export default function DeleteFormModal(props: any) {
     const { closeModal } = useModal();
@@ -34,30 +35,5 @@ export default function DeleteFormModal(props: any) {
             toast(t(toastMessage.formDeletionFail).toString(), { type: 'error' });
         }
     };
-
-    return (
-        <div className="relative z-50 mx-auto max-w-full min-w-full md:max-w-[600px] lg:max-w-[600px]" {...props}>
-            <div className="rounded-[4px] relative m-auto max-w-[500px] items-start justify-between bg-white">
-                <div className="relative flex flex-col items-start justify-start p-10">
-                    <div>
-                        <h4 className="sh1 mb-6">
-                            {t(localesCommon.delete)} &quot;{props?.form?.title}&quot;?
-                        </h4>
-                        <p className="!text-black-600 mb-8 body4 leading-none">{t(localesCommon.deleteMessage)}</p>
-                    </div>
-                    <div className="flex w-full gap-4 justify-between">
-                        <AppButton  variant={ButtonVariant.Danger} data-testid="logout-button" onClick={handleDelete}>
-                            {t(buttonConstant.delete)}
-                        </AppButton>
-                        <AppButton variant={ButtonVariant.Secondary} onClick={() => closeModal()}>
-                            {t(buttonConstant.cancel)}
-                        </AppButton>
-                    </div>
-                </div>
-                <div className="cursor-pointer absolute top-3 right-3 text-gray-600 hover:text-black" onClick={() => closeModal()}>
-                    <Close className="h-auto w-3 text-gray-600 dark:text-white" />
-                </div>
-            </div>
-        </div>
-    );
+    return <GenericHalfModal type="delete" positiveText="Delete" positiveAction={handleDelete} headerTitle="DeleteForm" title={`Are you sure to delete the form "${props?.form?.title || 'Untitled Form'}"?`} />;
 }
