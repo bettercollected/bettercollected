f510758db1c77096a5c745e18269940e1ee31292
Sital999 <sitalnagarkoti123@gmail.com>
2023-10-10 14:34:37 +0545

    Make componenet for dashboard all forms and pinned forms

---
+++ webapp/src/components/workspace-dashboard/workspace-dashboard-form-cards.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import CreateFormButton from '@Components/Common/CreateFormButton';
+import { ButtonVariant } from '@Components/Common/Input/Button/AppButtonProps';
+import BeaconComponent from '@Components/Joyride/JoyrideBeacon';
+
+import ActiveLink from '@app/components/ui/links/active-link';
+import WorkspaceFormCard from '@app/components/workspace-dashboard/workspace-form-card';
+import { StandardFormDto } from '@app/models/dtos/form';
+import { WorkspaceDto } from '@app/models/dtos/workspaceDto';
+
+interface IWorkspaceDashboardFormsCardProps {
+    workspaceForms: any;
+    workspace: WorkspaceDto;
+    hasCustomDomain: boolean;
+    showPinned?: boolean;
+}
+
+export default function WorkspaceDashboardFormsCard({ workspaceForms, workspace, hasCustomDomain, showPinned = false }: IWorkspaceDashboardFormsCardProps) {
+    const forms = workspaceForms;
+    const ref = React.useRef<HTMLDivElement>(null);
+
+    // @ts-ignore
+    return (
+        <div className="w-full mb-4 flex flex-col gap-5 h-fit">
+            {forms?.length === 0 ? (
+                <div className="w-full h-full flex flex-col items-center justify-center rounded-lg py-[84px]">
+                    <p className="h3-new text-black-800 font-semibold">You haven&apos;t created or imported any forms.</p>
+                    <p className="p1-new text-black-700 mb-6 mt-2">Create your first privacy friendly form.</p>
+                    <div ref={ref} className="relative">
+                        <CreateFormButton variant={ButtonVariant.Tertiary} />
+                        <div className="absolute -bottom-1 -right-1">
+                            <BeaconComponent />
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className="flex flex-col gap-6">
+                    {forms?.length !== 0 &&
+                        forms?.map((form: StandardFormDto, index: number) => (
+                            <ActiveLink key={form.formId} href={`/${workspace.workspaceName}/dashboard/forms/${form.formId}`}>
+                                <WorkspaceFormCard index={index} showPinned={showPinned} form={form} workspace={workspace} hasCustomDomain={hasCustomDomain} />
+                            </ActiveLink>
+                        ))}
+                </div>
+            )}
+        </div>
+    );
+}
