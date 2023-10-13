3adacdbd30707e086b78cbeb78f33d63b0f0b7d6
Sital999 <sitalnagarkoti123@gmail.com>
2023-10-13 14:42:20 +0545

    Make Dto, enum and schema for template

---
+++ backend/backend/app/models/template.py
@@ -0,0 +1,34 @@
+from typing import Optional, List
+
+from beanie import PydanticObjectId
+from common.models.standard_form import StandardFormField
+from fastapi_camelcase import CamelModel
+from pydantic import BaseModel
+
+from backend.app.models.enum.template import TemplateCategory
+
+
+class StandardTemplateSetting(BaseModel):
+    is_public: Optional[bool] = False
+
+
+class StandardFormTemplate(BaseModel):
+    id: Optional[PydanticObjectId]
+    workspace_id: Optional[PydanticObjectId]
+    type: Optional[str]
+    title: Optional[str]
+    description: Optional[str]
+    logo: Optional[str]
+    cover_image: Optional[str]
+    category: Optional[TemplateCategory]
+    button_text: Optional[str]
+    fields: Optional[List[StandardFormField]]
+    settings: Optional[StandardTemplateSetting] = StandardTemplateSetting()
+    created_by: Optional[str]
+    imported_from: Optional[PydanticObjectId]
+
+
+class StandardFormTemplateCamelModel(CamelModel, StandardFormTemplate):
+    pass
+
+
