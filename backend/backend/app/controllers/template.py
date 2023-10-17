db85c7d65121e19ef035fb32a4cfab30d5aca641
Sital999 <sitalnagarkoti123@gmail.com>
2023-10-17 09:53:47 +0545

    Add remaining endpoints for template

--- backend/backend/app/controllers/template.py
+++ backend/backend/app/controllers/template.py
@@ -1,8 +1,9 @@
+import json
 from typing import List
 
 from beanie import PydanticObjectId
-from classy_fastapi import Routable, post, get
-from fastapi import Depends
+from classy_fastapi import Routable, post, get, patch, delete
+from fastapi import Depends, UploadFile, Form
 from gunicorn.config import User
 
 from backend.app.container import container
@@ -47,7 +48,7 @@ class FormTemplateRouter(Routable):
                                  user: User = Depends(get_logged_user)):
         response = await self.form_template_service.get_template_by_id(workspace_id=workspace_id, user=user,
                                                                        template_id=template_id)
-        return response
+        return StandardFormTemplate(**response.dict())
 
     @post("/workspaces/{workspace_id}/form/{form_id}/template")
     async def create_template_from_form(self, workspace_id: PydanticObjectId,
@@ -61,7 +62,19 @@ class FormTemplateRouter(Routable):
         )
         return StandardFormTemplateCamelModel(**response.dict())
 
-    @post("/workspaces/{workspace_id}/template")
+    @post('/workspaces/{workspace_id}/template', response_model=StandardFormTemplate)
+    async def create_new_template(self, workspace_id: PydanticObjectId, template_body: str = Form(),
+                                  logo: UploadFile = None,
+                                  cover_image: UploadFile = None,
+                                  user: User = Depends(get_logged_user)):
+        template = json.loads(template_body)
+        response = await self.form_template_service.create_new_template(workspace_id=workspace_id, user=user,
+                                                                        template_body=StandardFormTemplate(
+                                                                            **template), logo=logo,
+                                                                        cover_image=cover_image)
+        return response
+
+    @post("/workspaces/{workspace_id}/template/import")
     async def import_template_to_workspace(self, workspace_id: PydanticObjectId, template_id: PydanticObjectId,
                                            user: User = Depends(get_logged_user)):
         response = await self.form_template_service.import_form_to_workspace(workspace_id, user, template_id)
@@ -72,4 +85,25 @@ class FormTemplateRouter(Routable):
                                         user: User = Depends(get_logged_user)):
         response = await self.form_template_service.create_form_from_template(workspace_id=workspace_id,
                                                                               template_id=template_id, user=user)
+        return response
+
+    @patch('/workspaces/{workspace_id}/template/{template_id}', response_model=StandardFormTemplate)
+    async def update_template(self, workspace_id: PydanticObjectId, template_id: PydanticObjectId,
+                              logo: UploadFile = None,
+                              cover_image: UploadFile = None,
+                              template_body: str = Form(),
+                              user: User = Depends(get_logged_user)
+                              ):
+        template = json.loads(template_body)
+        response = await self.form_template_service.update_template(workspace_id=workspace_id, template_id=template_id,
+                                                                    user=user,
+                                                                    template_body=StandardFormTemplate(**template),
+                                                                    logo=logo, cover_image=cover_image)
+        return response
+
+    @delete('/workspaces/{workspace_id}/template/{template_id}')
+    async def delete_template(self, workspace_id: PydanticObjectId, template_id: PydanticObjectId,
+                              user: User = Depends(get_logged_user)):
+        response = await self.form_template_service.delete_template(workspace_id=workspace_id, template_id=template_id,
+                                                                    user=user)
         return response
--- backend/backend/app/repositories/template.py
+++ backend/backend/app/repositories/template.py
@@ -1,5 +1,10 @@
-from beanie import PydanticObjectId
+from http import HTTPStatus
 
+from beanie import PydanticObjectId
+from common.models.user import User
+
+from backend.app.exceptions import HTTPException
+from backend.app.models.template import StandardFormTemplate
 from backend.app.schemas.template import FormTemplateDocument
 
 
@@ -21,3 +26,28 @@ class FormTemplateRepository:
         imported_template.workspace_id = workspace_id
         imported_template = await imported_template.save()
         return imported_template
+
+    async def create_new_template(self, workspace_id: PydanticObjectId, template_body: StandardFormTemplate,
+                                  user: User):
+        template = FormTemplateDocument(**template_body.dict())
+        template.workspace_id = workspace_id
+        template.created_by = user.id
+        return await template.save()
+
+    async def update_template(self, template_id: PydanticObjectId, template_body: StandardFormTemplate):
+        template = await FormTemplateDocument.find_one({"_id": template_id})
+        template.fields = template_body.fields
+        template.settings = template_body.settings
+        template.title = template_body.title
+        template.description = template_body.description
+        template.button_text = template_body.button_text
+        template.logo = template_body.logo
+        template.cover_image = template_body.cover_image
+        return await template.save()
+
+    async def delete_template(self, template_id: PydanticObjectId):
+        template = await FormTemplateDocument.find_one({"_id": template_id})
+        if not template:
+            raise HTTPException(HTTPStatus.NOT_FOUND, "Template not found")
+        await template.delete()
+        return template_id
