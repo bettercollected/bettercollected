26448ba3cd45a93b639ed67d96785727d3c524d4
Sital999 <sitalnagarkoti123@gmail.com>
2023-10-17 13:16:43 +0545

    Convert template_id from query to pathname

--- backend/backend/app/controllers/template.py
+++ backend/backend/app/controllers/template.py
@@ -31,11 +31,11 @@ from backend.app.services.workspace_form_service import WorkspaceFormService
 )
 class FormTemplateRouter(Routable):
     def __init__(
-        self,
-        workspace_form_service: WorkspaceFormService = container.workspace_form_service(),
-        form_template_service: FormTemplateService = container.form_template_service(),
-        *args,
-        **kwargs
+            self,
+            workspace_form_service: WorkspaceFormService = container.workspace_form_service(),
+            form_template_service: FormTemplateService = container.form_template_service(),
+            *args,
+            **kwargs
     ):
         super().__init__(*args, **kwargs)
         self.workspace_form_service = workspace_form_service
@@ -43,19 +43,19 @@ class FormTemplateRouter(Routable):
 
     @get("/templates", response_model=List[StandardFormTemplate])
     async def get_templates(
-        self,
-        workspace_id: PydanticObjectId = None,
-        user: User = Depends(get_logged_user),
+            self,
+            workspace_id: PydanticObjectId = None,
+            user: User = Depends(get_logged_user),
     ):
         response = await self.form_template_service.get_templates(workspace_id, user)
         return response
 
     @get("/templates/{template_id}", response_model=StandardFormTemplate)
     async def get_template_by_id(
-        self,
-        template_id: PydanticObjectId,
-        workspace_id: PydanticObjectId = None,
-        user: User = Depends(get_logged_user),
+            self,
+            template_id: PydanticObjectId,
+            workspace_id: PydanticObjectId = None,
+            user: User = Depends(get_logged_user),
     ):
         response = await self.form_template_service.get_template_by_id(
             workspace_id=workspace_id, user=user, template_id=template_id
@@ -64,10 +64,10 @@ class FormTemplateRouter(Routable):
 
     @post("/workspaces/{workspace_id}/form/{form_id}/template")
     async def create_template_from_form(
-        self,
-        workspace_id: PydanticObjectId,
-        form_id: PydanticObjectId,
-        user: User = Depends(get_logged_user),
+            self,
+            workspace_id: PydanticObjectId,
+            form_id: PydanticObjectId,
+            user: User = Depends(get_logged_user),
     ):
         response = await self.workspace_form_service.duplicate_form(
             workspace_id=workspace_id, form_id=form_id, is_template=True, user=user
@@ -76,12 +76,12 @@ class FormTemplateRouter(Routable):
 
     @post("/workspaces/{workspace_id}/template", response_model=StandardFormTemplate)
     async def create_new_template(
-        self,
-        workspace_id: PydanticObjectId,
-        template_body: str = Form(),
-        logo: UploadFile = None,
-        cover_image: UploadFile = None,
-        user: User = Depends(get_logged_user),
+            self,
+            workspace_id: PydanticObjectId,
+            template_body: str = Form(),
+            logo: UploadFile = None,
+            cover_image: UploadFile = None,
+            user: User = Depends(get_logged_user),
     ):
         template = json.loads(template_body)
         response = await self.form_template_service.create_new_template(
@@ -93,12 +93,12 @@ class FormTemplateRouter(Routable):
         )
         return response
 
-    @post("/workspaces/{workspace_id}/template/import")
+    @post("/workspaces/{workspace_id}/template/{template_id}/import")
     async def import_template_to_workspace(
-        self,
-        workspace_id: PydanticObjectId,
-        template_id: PydanticObjectId,
-        user: User = Depends(get_logged_user),
+            self,
+            workspace_id: PydanticObjectId,
+            template_id: PydanticObjectId,
+            user: User = Depends(get_logged_user),
     ):
         response = await self.form_template_service.import_form_to_workspace(
             workspace_id, user, template_id
@@ -109,10 +109,10 @@ class FormTemplateRouter(Routable):
         "/workspaces/{workspace_id}/template/{template_id}", response_model=MinifiedForm
     )
     async def create_form_from_template(
-        self,
-        workspace_id: PydanticObjectId,
-        template_id: PydanticObjectId,
-        user: User = Depends(get_logged_user),
+            self,
+            workspace_id: PydanticObjectId,
+            template_id: PydanticObjectId,
+            user: User = Depends(get_logged_user),
     ):
         response = await self.form_template_service.create_form_from_template(
             workspace_id=workspace_id, template_id=template_id, user=user
@@ -124,13 +124,13 @@ class FormTemplateRouter(Routable):
         response_model=StandardFormTemplate,
     )
     async def update_template(
-        self,
-        workspace_id: PydanticObjectId,
-        template_id: PydanticObjectId,
-        logo: UploadFile = None,
-        cover_image: UploadFile = None,
-        template_body: str = Form(),
-        user: User = Depends(get_logged_user),
+            self,
+            workspace_id: PydanticObjectId,
+            template_id: PydanticObjectId,
+            logo: UploadFile = None,
+            cover_image: UploadFile = None,
+            template_body: str = Form(),
+            user: User = Depends(get_logged_user),
     ):
         template = json.loads(template_body)
         response = await self.form_template_service.update_template(
@@ -145,10 +145,10 @@ class FormTemplateRouter(Routable):
 
     @delete("/workspaces/{workspace_id}/template/{template_id}")
     async def delete_template(
-        self,
-        workspace_id: PydanticObjectId,
-        template_id: PydanticObjectId,
-        user: User = Depends(get_logged_user),
+            self,
+            workspace_id: PydanticObjectId,
+            template_id: PydanticObjectId,
+            user: User = Depends(get_logged_user),
     ):
         response = await self.form_template_service.delete_template(
             workspace_id=workspace_id, template_id=template_id, user=user
