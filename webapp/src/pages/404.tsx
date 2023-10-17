333bb7b7ceb993858ad51ec09491e8b52a81e312
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-10-17 12:55:05 +0545

    Update design for not found page

--- webapp/src/pages/404.tsx
+++ webapp/src/pages/404.tsx
@@ -1,17 +1,20 @@
 import Link from 'next/link';
 
-import Layout from '@app/layouts/_layout';
+import AppButton from '@Components/Common/Input/Button/AppButton';
+import { ButtonVariant } from '@Components/Common/Input/Button/AppButtonProps';
+import HeaderImageWrapper from '@Components/Common/Wrapper/HeaderImageWrapper';
 
 export default function FourOhFour() {
     return (
-        <Layout hideSignIn className="min-h-screen">
-            <div className="!min-h-screen w-full flex flex-col items-center justify-center">
-                <h1>404 - Page Not Found</h1>
-
-                <Link href="/">
-                    <a className="text-blue-500 hover:underline">Go back home</a>
-                </Link>
+        <HeaderImageWrapper className="my-16 gap-4">
+            <span className="h2-new">404 </span>
+            <div className="flex flex-col gap-2 items-center">
+                <span className="h4-new text-black-800">Page Not Found</span>
+                <span className="p2-new text-black-700 text-center">We’re sorry, we couldn’t find the page.</span>
             </div>
-        </Layout>
+            <Link href="/">
+                <AppButton variant={ButtonVariant.Tertiary}>Go back home</AppButton>
+            </Link>
+        </HeaderImageWrapper>
     );
 }
