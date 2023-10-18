dd82369396211a660a5b6fbae00de9c6fff6b2a4
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-10-18 16:37:47 +0545

    Revert cisibility settings while creating form

--- backend/backend/app/services/workspace_form_service.py
+++ backend/backend/app/services/workspace_form_service.py
@@ -302,8 +302,6 @@ class WorkspaceFormService:
         workspace_form_settings = WorkspaceFormSettings(
             custom_url=form.form_id,
             provider="self",
-            private=False,
-            hidden=True,
             privacy_policy_url=form.settings.privacy_policy_url,
             response_expiration=form.settings.response_expiration,
             response_expiration_type=form.settings.response_expiration_type
@@ -486,7 +484,7 @@ class WorkspaceFormService:
             form_id=str(duplicated_form.form_id),
             workspace_id=workspace_id,
             user_id=user.id,
-            settings=WorkspaceFormSettings(hidden=True, private=False),
+            settings=WorkspaceFormSettings(),
         )
         workspace_form.settings.provider = "self"
         workspace_form.settings.custom_url = str(duplicated_form.id)
