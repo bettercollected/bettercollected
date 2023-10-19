30663e373dc0493a4c8c55b1449b443bda5a94e1
Sital999 <sitalnagarkoti123@gmail.com>
2023-10-19 13:31:09 +0545

    Set imported_from as workspace_title replacing id

--- backend/backend/app/repositories/template.py
+++ backend/backend/app/repositories/template.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from beanie import PydanticObjectId
+from common.constants import MESSAGE_NOT_FOUND
 from common.models.user import User
 
 from backend.app.exceptions import HTTPException
@@ -9,18 +10,58 @@ from backend.app.schemas.template import FormTemplateDocument
 
 
 class FormTemplateRepository:
-    async def get_templates(self, workspace_id: PydanticObjectId):
+    async def get_templates_with_creator(
+            self, workspace_id: PydanticObjectId,
+            template_id: PydanticObjectId = None, predefined_workspace: bool = False
+    ):
+        query = {"workspace_id": workspace_id}
+        if predefined_workspace:
+            query["settings"] = {"is_public": True}
+        if template_id:
+            query["template_id"] = template_id
         return (
-            await FormTemplateDocument.find({"workspace_id": workspace_id})
-            .aggregate([{"$set": {"id": "$_id"}}])
+            await FormTemplateDocument.find(query)
+            .aggregate([
+                {
+                    "$set": {"id": "$_id"}
+                },
+                {
+                    "$lookup": {
+                        "from": "workspaces",
+                        "localField": "imported_from",
+                        "foreignField": "_id",
+                        "as": "workspace",
+                    }
+                },
+                {
+                    "$unwind": {
+                        "path": "$workspace",
+                        "preserveNullAndEmptyArrays": True
+                    }
+                },
+                {
+                    "$set": {
+                        "imported_from": "$workspace.title"
+                    }
+                }
+
+            ])
             .to_list()
         )
 
     async def get_template_by_id(self, template_id: PydanticObjectId):
         return await FormTemplateDocument.find_one({"_id": template_id})
 
+    async def get_template_by_id_with_creator(self, workspace_id: PydanticObjectId, template_id: PydanticObjectId):
+        templates = await self.get_templates_with_creator(workspace_id=workspace_id, template_id=template_id)
+
+        if len(templates) > 0:
+            return templates[0]
+
+        raise HTTPException(HTTPStatus.NOT_FOUND, content=MESSAGE_NOT_FOUND)
+
     async def import_template_to_workspace(
-        self, workspace_id: PydanticObjectId, template_id: PydanticObjectId
+            self, workspace_id: PydanticObjectId, template_id: PydanticObjectId
     ):
         template = await self.get_template_by_id(template_id)
         imported_template = FormTemplateDocument(**template.dict())
@@ -31,10 +72,10 @@ class FormTemplateRepository:
         return imported_template
 
     async def create_new_template(
-        self,
-        workspace_id: PydanticObjectId,
-        template_body: StandardFormTemplate,
-        user: User,
+            self,
+            workspace_id: PydanticObjectId,
+            template_body: StandardFormTemplate,
+            user: User,
     ):
         template = FormTemplateDocument(**template_body.dict())
         template.workspace_id = workspace_id
@@ -42,7 +83,7 @@ class FormTemplateRepository:
         return await template.save()
 
     async def update_template(
-        self, template_id: PydanticObjectId, template_body: StandardFormTemplate
+            self, template_id: PydanticObjectId, template_body: StandardFormTemplate
     ):
         template = await FormTemplateDocument.find_one({"_id": template_id})
         template.fields = template_body.fields
