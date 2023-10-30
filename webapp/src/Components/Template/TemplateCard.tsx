40df5833aab84af489c1360a075c7ebe5a3b191f
Sital999 <sitalnagarkoti123@gmail.com>
2023-10-30 16:33:43 +0545

    Add `Edit Icon` as item of MenuDropDown

--- webapp/src/Components/Template/TemplateCard.tsx
+++ webapp/src/Components/Template/TemplateCard.tsx
@@ -5,13 +5,12 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 import DeleteIcon from '@Components/Common/Icons/Delete';
+import EditIcon from '@Components/Common/Icons/Edit';
 import EllipsisOption from '@Components/Common/Icons/EllipsisOption';
-import AppButton from '@Components/Common/Input/Button/AppButton';
 import MenuDropdown from '@Components/Common/Navigation/MenuDropdown/MenuDropdown';
 import { ListItemIcon, MenuItem } from '@mui/material';
 
 import { useModal } from '@app/components/modal-views/context';
-import { formConstant } from '@app/constants/locales/form';
 import { IFormTemplateDto } from '@app/models/dtos/template';
 import { useAppSelector } from '@app/store/hooks';
 import { selectWorkspace } from '@app/store/workspaces/slice';
@@ -32,6 +31,10 @@ const TemplateCard = ({ template, isPredefinedTemplate }: ITemplateCardProps) =>
         router.push(`/${workspace.workspaceName}/templates/${template.id}`);
     };
 
+    const handleClickEditCard = () => {
+        router.push(`/${workspace.workspaceName}/templates/${template.id}/edit`);
+    };
+
     return (
         <div className={'flex flex-col gap-2 '}>
             <div className={'h-[192px] w-[186px] cursor-pointer relative border-black-200 border rounded'} onClick={handleClickCard}>
@@ -41,6 +44,12 @@ const TemplateCard = ({ template, isPredefinedTemplate }: ITemplateCardProps) =>
                 <span className={'h5-new font-semibold max-w-[150px] truncate text-black-800'}>{template.title}</span>
                 {!isPredefinedTemplate && (
                     <MenuDropdown width={220} showExpandMore={false} id="template-options" menuTitle={''} menuContent={<EllipsisOption />}>
+                        <MenuItem onClick={handleClickEditCard} className="body4">
+                            <ListItemIcon>
+                                <EditIcon width={20} height={20} className="text-black-800" strokeWidth={1} />
+                            </ListItemIcon>
+                            <span>Edit</span>
+                        </MenuItem>
                         <MenuItem onClick={() => openModal('DELETE_TEMPLATE_CONFIRMATION_MODAL_VIEW', { template })} sx={{ paddingX: '20px', paddingY: '10px', height: '36px' }} className="body4">
                             <ListItemIcon>
                                 <DeleteIcon width={20} height={20} className="text-black-800" strokeWidth={1} />
