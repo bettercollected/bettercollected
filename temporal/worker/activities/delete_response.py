73735fbb02d8396dc45ba4c8415164bf170804c9
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-11-02 15:21:49 +0545

    Change API key header name

--- temporal/worker/activities/delete_response.py
+++ temporal/worker/activities/delete_response.py
@@ -9,7 +9,7 @@ with workflow.unsafe.imports_passed_through():
 
 @activity.defn(name="delete_response")
 async def delete_response(delete_response_params: DeleteResponseParams):
-    headers = {"api_key": settings.api_key}
+    headers = {"api-key": settings.api_key}
     response = requests.post(
         url=settings.server_url
             + f"/temporal/delete/submissions/{delete_response_params.response_id}",
