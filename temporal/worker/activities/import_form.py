c7002304427e2c165d9520a709076e4a9ab13ac6
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-11-07 15:48:47 +0545

    Use async lib for sending requests

--- temporal/worker/activities/import_form.py
+++ temporal/worker/activities/import_form.py
@@ -4,14 +4,15 @@ from models.ImportFormParams import ImportFormParams
 from settings.application import settings
 
 with workflow.unsafe.imports_passed_through():
-    import requests
+    import httpx
 
 
 @activity.defn(name="import_form")
 async def import_form(import_form_params: ImportFormParams):
-    headers = {"api-key": settings.api_key}
-    response = requests.post(
-        url=settings.server_url
-            + f"/temporal/import/{import_form_params.workspace_id}/form/{import_form_params.form_id}",
-        headers=headers
-    )
+    async with httpx.AsyncClient() as client:
+        headers = {"api-key": settings.api_key}
+        response = await client.post(
+            url=settings.server_url
+                + f"/temporal/import/{import_form_params.workspace_id}/form/{import_form_params.form_id}",
+            headers=headers
+        )
--- temporal/worker/activities/save_preview.py
+++ temporal/worker/activities/save_preview.py
@@ -7,7 +7,7 @@ from settings.application import settings
 
 with workflow.unsafe.imports_passed_through():
     from selenium import webdriver
-    import requests
+    import httpx
     from configs.crypto import crypto
     from io import BytesIO
     from models.user_tokens import UserTokens
@@ -17,27 +17,28 @@ with workflow.unsafe.imports_passed_through():
 
 @activity.defn(name="save_preview")
 async def save_preview(save_preview_params: SavePreviewParams):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(options=options)
-    driver.set_window_size(640, 620)
-    decrypted_token = crypto.decrypt(save_preview_params.token)
-    user_token = UserTokens(**json.loads(decrypted_token))
-    auth_cookie = {"name": "Authorization", "value": user_token.access_token, "domain": settings.cookie_domain}
-    driver.get(save_preview_params.template_url)
-    driver.add_cookie(auth_cookie)
-    driver.refresh()
-    await sleep(20)
+    async with httpx.AsyncClient() as client:
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(options=options)
+        driver.set_window_size(640, 620)
+        decrypted_token = crypto.decrypt(save_preview_params.token)
+        user_token = UserTokens(**json.loads(decrypted_token))
+        auth_cookie = {"name": "Authorization", "value": user_token.access_token, "domain": settings.cookie_domain}
+        driver.get(save_preview_params.template_url)
+        driver.add_cookie(auth_cookie)
+        driver.refresh()
+        await sleep(20)
 
-    screenshot = driver.get_screenshot_as_png()
-    driver.close()
-    image_file = BytesIO(screenshot)
-    headers = {"api-key": settings.api_key}
-    url = settings.server_url + f"/template/{save_preview_params.template_id}/preview"
-    files = {"preview_image": ("preview_image.png", image_file, "image/png")}
-    response = requests.patch(url, files=files, headers=headers)
+        screenshot = driver.get_screenshot_as_png()
+        driver.close()
+        image_file = BytesIO(screenshot)
+        headers = {"api-key": settings.api_key}
+        url = settings.server_url + f"/template/{save_preview_params.template_id}/preview"
+        files = {"preview_image": ("preview_image.png", image_file, "image/png")}
+        response = await client.patch(url, files=files, headers=headers)
 
-    if response.status_code != 200:
-        raise FailureError("Preview Image Patch not successful")
+        if response.status_code != 200:
+            raise FailureError("Preview Image Patch not successful")
