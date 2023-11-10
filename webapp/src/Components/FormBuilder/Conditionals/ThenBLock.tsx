c40e0b1e5a32c5c2bdc46bebf6522ea28493479f
Sital999 <sitalnagarkoti123@gmail.com>
2023-11-10 11:06:10 +0545

    Add multiple field selection and add,remove nested actions

--- webapp/src/Components/FormBuilder/Conditionals/ThenBLock.tsx
+++ webapp/src/Components/FormBuilder/Conditionals/ThenBLock.tsx
@@ -3,13 +3,15 @@ import React, { useEffect, useState } from 'react';
 import _ from 'lodash';
 
 import ConditionalListDropDown from '@Components/FormBuilder/Conditionals/ConditionalListDropDown';
+import ConditionalOptionsDropdown from '@Components/FormBuilder/Conditionals/ConditionalOptionsDropdown';
+import { uuidv4 } from '@mswjs/interceptors/lib/utils/uuid';
 
 import { FormBuilderTagNames, LabelFormBuilderTagNames } from '@app/models/enums/formBuilder';
-import { updateAction } from '@app/store/form-builder/actions';
+import { setUpdateField, updateAction } from '@app/store/form-builder/actions';
 import { selectBuilderState, selectFields } from '@app/store/form-builder/selectors';
 import { ActionType, ConditionalActions, IFormFieldState } from '@app/store/form-builder/types';
 import { useAppDispatch, useAppSelector } from '@app/store/hooks';
-import { getNextField, getPreviousField } from '@app/utils/formBuilderBlockUtils';
+import { createNewAction, getNextField, getPreviousField } from '@app/utils/formBuilderBlockUtils';
 
 const actions = [
     { type: ActionType.SHOW_FIELDS, value: 'Show Field' },
@@ -56,31 +58,43 @@ const ThenBlock = ({ field, action }: { field: IFormFieldState; action: Conditio
         setInputFields(filteredFields);
     }, [formFields]);
 
-    const onPayloadChange = (payload: any) => {
+    function handleFieldSelection(payload: any) {
         const previousSelectedFields = field?.properties?.actions && field?.properties?.actions[action.id]?.payload;
         const currentSelectedFields = payload.map((item: any) => item.fieldId);
         let selectedFields: Array<any> = [];
         payload.forEach((state: any) => {
             const selectedField = fields.find((field) => field.id == state.fieldId);
+
             if (selectedField?.type !== FormBuilderTagNames.CONDITIONAL) {
                 selectedFields.push(state);
             }
             if (selectedField?.type === FormBuilderTagNames.LAYOUT_LABEL) {
+                // for selecting 2 fields at one selection
                 const nextField = selectedField && getNextField(fields, selectedField);
-                if (!nextField.type.startsWith('input_')) {
+                if (!nextField?.type.startsWith('input_')) {
                     return;
-                } else if (previousSelectedFields?.includes(nextField?.id) && !currentSelectedFields.includes(nextField?.id)) {
+                }
+                if (previousSelectedFields?.includes(nextField?.id) && !currentSelectedFields.includes(nextField?.id)) {
                     return;
                 } else {
                     selectedFields.push(convertFieldForConditionalDropDownState(getNextField(fields, selectedField)));
                 }
             } else {
-                const previousfield = selectedField && getPreviousField(fields, selectedField);
-                if (previousfield?.type === FormBuilderTagNames.LAYOUT_LABEL && previousSelectedFields?.includes(previousfield?.id) && !currentSelectedFields.includes(previousfield?.id)) {
+                // for removing 2 fields at one deSelection
+                const previousField = selectedField && getPreviousField(fields, selectedField);
+                if (!selectedField?.type.startsWith('input_')) {
+                    return;
+                }
+                if (previousField?.type === FormBuilderTagNames.LAYOUT_LABEL && previousSelectedFields?.includes(previousField?.id) && !currentSelectedFields.includes(previousField?.id)) {
                     selectedFields.splice(selectedFields.indexOf(selectedField), 1);
                 }
             }
         });
+        return selectedFields;
+    }
+
+    const onPayloadChange = (payload: any) => {
+        const selectedFields = handleFieldSelection(payload);
         dispatch(
             updateAction({
                 fieldId: field.id,
@@ -101,6 +115,45 @@ const ThenBlock = ({ field, action }: { field: IFormFieldState; action: Conditio
     };
 
     const selectedFields = inputFields.filter((item: any) => field?.properties?.actions && field?.properties?.actions[action.id]?.payload?.includes(item?.fieldId));
+    const addCondition = () => {
+        if (field?.properties?.actions) {
+            const id = uuidv4();
+            const position = action.position + 1;
+            let actionsArray = Object.values(field?.properties?.actions);
+            const newAction = createNewAction(id, position);
+            actionsArray.splice(position, 0, newAction, ...actionsArray.slice(position));
+            let newActionMap: any = {};
+            actionsArray.forEach((item: any, index: number) => {
+                const action = { ...item };
+                action.position = index;
+                newActionMap[action.id] = item;
+            });
+            dispatch(
+                setUpdateField({
+                    ...field,
+                    properties: { ...field.properties, actions: newActionMap }
+                })
+            );
+        }
+    };
+    const removeCondition = (action: any) => {
+        if (field?.properties?.actions) {
+            const actions = { ...field?.properties?.actions };
+            delete actions[action.id];
+            let actionsArray = Object.values(actions);
+            actionsArray.forEach((item: any, index: number) => {
+                const action = { ...item };
+                action.position = index;
+                actions[action.id] = action;
+            });
+            dispatch(
+                setUpdateField({
+                    ...field,
+                    properties: { ...field.properties, actions }
+                })
+            );
+        }
+    };
 
     return (
         <div className={'flex flex-col gap-2 p-4 bg-new-white-200 rounded-lg'}>
@@ -108,6 +161,7 @@ const ThenBlock = ({ field, action }: { field: IFormFieldState; action: Conditio
             <div className={'flex flex-row gap-2 '}>
                 <ConditionalListDropDown size={'small'} value={actions.find((state) => state.type == action.type)} onChange={onActionTypeChange} items={actions} />
                 <ConditionalListDropDown value={selectedFields} onChange={onPayloadChange} items={inputFields} multiple />
+                <ConditionalOptionsDropdown addOption={addCondition} removeOption={() => removeCondition(action)} text={'action'} />
             </div>
         </div>
     );
