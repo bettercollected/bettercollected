6dc63d70722a0bd539871c5f73f105f4970144bc
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-11-27 14:28:41 +0545

    Add search feature and keyboard events for field selector

--- webapp/src/Components/FormBuilder/BuilderBlock/FormBuilderFieldSelector.tsx
+++ webapp/src/Components/FormBuilder/BuilderBlock/FormBuilderFieldSelector.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { batch } from 'react-redux';
 
+import useArrowsToSelectOption from '@app/lib/hooks/use-arrows-to-select-option';
 import useClickOutsideMenu from '@app/lib/hooks/use-click-outside-menu';
 import { FormBuilderTagNames, LabelFormBuilderTagNames } from '@app/models/enums/formBuilder';
 import { resetBuilderMenuState, setUpdateField } from '@app/store/form-builder/actions';
@@ -10,7 +11,7 @@ import { IFormFieldState } from '@app/store/form-builder/types';
 import { useAppDispatch, useAppSelector } from '@app/store/hooks';
 import { convertPlaceholderToDisplayValue, getPreviousField } from '@app/utils/formBuilderBlockUtils';
 
-export default function FormBuilderFieldSelector({ field }: { field: IFormFieldState }) {
+export default function FormBuilderFieldSelector({ field, searchQuery = '' }: { field: IFormFieldState; searchQuery: string }) {
     const pipingFieldMenuState: any = useAppSelector(selectMenuState('pipingFields'));
 
     const formFields = useAppSelector(selectFields);
@@ -20,6 +21,8 @@ export default function FormBuilderFieldSelector({ field }: { field: IFormFieldS
     const activeFieldPosition = useAppSelector(selectActiveFieldIndex);
 
     useClickOutsideMenu('field-selector');
+
+    const [selectedFieldIndex, setSelectedFieldIndex] = useState(0);
     const getFilteredInputFields = () => {
         const filteredFields: Array<any> = [];
         fields.forEach((field) => {
@@ -34,12 +37,50 @@ export default function FormBuilderFieldSelector({ field }: { field: IFormFieldS
                 }
                 x.value = convertPlaceholderToDisplayValue(fields, text);
                 x.type = field.type;
-                filteredFields.push(x);
+                if (x.value.match(searchQuery || '')) {
+                    filteredFields.push(x);
+                }
             }
         });
         return filteredFields;
     };
 
+    const [selectionFields, setSelectionFields] = useState(getFilteredInputFields());
+
+    const scrollToSelectedItem = (index: number | string) => {
+        const selectedItem = document.getElementById('list-item-' + index);
+        if (selectedItem) {
+            selectedItem.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+        }
+    };
+
+    const handleNext = useCallback(
+        (e: any) => {
+            const newIndex = (selectedFieldIndex + 1) % selectionFields.length;
+            setSelectedFieldIndex(newIndex);
+            scrollToSelectedItem(newIndex);
+        },
+        [selectionFields, selectedFieldIndex]
+    );
+
+    const handlePrevious = useCallback(
+        (e: any) => {
+            const newIndex = (selectedFieldIndex - 1 + selectionFields.length) % selectionFields.length;
+            setSelectedFieldIndex(newIndex);
+            scrollToSelectedItem(newIndex);
+        },
+        [selectionFields, selectedFieldIndex]
+    );
+
+    const handleSelectField = (e: any) => {
+        onClickField(selectionFields[selectedFieldIndex]);
+    };
+
+    useArrowsToSelectOption(handleSelectField, handleNext, handlePrevious);
+
+    useEffect(() => {
+        setSelectionFields(getFilteredInputFields());
+    }, [searchQuery]);
     const replaceLastAt = (inputString: string, replacement: string) => {
         const lastAtIndex = inputString.lastIndexOf('@');
 
@@ -69,13 +110,14 @@ export default function FormBuilderFieldSelector({ field }: { field: IFormFieldS
                     left: pipingFieldMenuState?.pos?.left,
                     top: pipingFieldMenuState?.pos?.top + 24
                 }}
-                className={`max-h-48 p-2 rounded absolute w-[300px] bg-white drop-shadow-lg top-full z-[100]`}
+                className={`max-h-48 p-2 rounded absolute w-[300px] bg-white overflow-auto drop-shadow-lg top-full z-[100]`}
             >
                 <div className="text-gray-600 font-bold px-4 py-2 ">Mention a field</div>
                 <div className="flex flex-col">
-                    {getFilteredInputFields().map((inputField: any) => (
+                    {selectionFields.map((inputField: any, index) => (
                         <div
-                            className={`px-4 py-2 cursor-pointer truncate hover:bg-black-100 text-base font-normal bg-white text-black-800`}
+                            id={'list-item-' + index}
+                            className={`px-4 py-2 cursor-pointer truncate bg-white hover:bg-black-200 ${selectedFieldIndex === index ? '!bg-black-100' : ''} text-base font-normal  text-black-800`}
                             key={inputField.fieldId}
                             onClick={() => {
                                 onClickField(inputField);
