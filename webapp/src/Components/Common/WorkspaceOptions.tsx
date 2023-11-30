094c953716cfd62b884de345fddeeb3fa4e0b917
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-11-30 10:02:57 +0545

    Update Workspace options

--- webapp/src/Components/Common/WorkspaceOptions.tsx
+++ webapp/src/Components/Common/WorkspaceOptions.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
 
 import EditIcon from '@Components/Common/Icons/Edit';
 import EllipsisOption from '@Components/Common/Icons/EllipsisOption';
+import SettingsIcon from '@Components/Common/Icons/Settings';
 import ShareIcon from '@Components/Common/Icons/ShareIcon';
 import MenuDropdown from '@Components/Common/Navigation/MenuDropdown/MenuDropdown';
 import { ListItemIcon, MenuItem } from '@mui/material';
@@ -19,14 +21,15 @@ interface IWorkspaceOptionsProps {
 }
 
 export default function WorkspaceOptions({ isAdmin, onClickEdit, onShareWorkspace, workspaceUrl }: IWorkspaceOptionsProps) {
+    const { t } = useTranslation();
     return (
         <MenuDropdown id="workspace-dropdown" showExpandMore={false} menuTitle="" menuContent={<EllipsisOption />}>
             {isAdmin && (
                 <MenuItem onClick={onClickEdit}>
                     <ListItemIcon>
-                        <EditIcon width={20} height={20} />
+                        <SettingsIcon strokeWidth={1} width={20} height={20} />
                     </ListItemIcon>
-                    <span>Edit</span>
+                    <span> {t('SETTINGS')}</span>
                 </MenuItem>
             )}
             <MenuItem>
@@ -35,13 +38,13 @@ export default function WorkspaceOptions({ isAdmin, onClickEdit, onShareWorkspac
                         <EyeIcon height={20} width={20} />
                     </ListItemIcon>
                 </ActiveLink>
-                <span>Open Workspace</span>
+                <span> {t('BUTTON.OPEN_WORKSPACE')}</span>
             </MenuItem>
             <MenuItem onClick={onShareWorkspace}>
                 <ListItemIcon>
-                    <ShareIcon height={20} width={20} />
+                    <ShareIcon strokeWidth={1} height={20} width={20} />
                 </ListItemIcon>
-                <span>Share Workspace</span>
+                <span> {t('SHARE_WORKSPACE')}</span>
             </MenuItem>
         </MenuDropdown>
     );
