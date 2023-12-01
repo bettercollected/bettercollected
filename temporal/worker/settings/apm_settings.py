e3626d17a607688154b403d25757038c82d97b79
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-12-01 12:32:46 +0545

    Add apm wrapper for async client

--- temporal/worker/settings/apm_settings.py
+++ temporal/worker/settings/apm_settings.py
@@ -5,6 +5,7 @@ class APMSettings(BaseSettings):
     api_key: str = "api-key"
     service_name: str = "temporal-worker"
     server_url: str = "https://apm.sireto.io"
+    enabled: bool = False
 
     class Config:
         env_prefix = "ELASTIC_APM_"
