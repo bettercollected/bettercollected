8d8ce111d4d0f4bf35f4ef49d636d8c2e2ee831d
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-12-05 16:58:43 +0545

    Add functions to send mail and webhook data

--- temporal/actions-executor/activities/run_action_code.py
+++ temporal/actions-executor/activities/run_action_code.py
@@ -1,8 +1,7 @@
 from temporalio import activity, workflow
 
-from utilities.actions import run_action
-
 with workflow.unsafe.imports_passed_through():
+    from utilities.actions import run_action
     from models.run_action_code_params import RunActionCodeParams
     from wrappers.apm_wrapper import APMAsyncHttpClient
 
@@ -10,5 +9,6 @@ with workflow.unsafe.imports_passed_through():
 @activity.defn(name="run_action_code")
 async def run_action_code(run_action_code_params: RunActionCodeParams):
     async with APMAsyncHttpClient("save_preview") as client:
-        return await run_action(action=run_action_code_params.action, form=run_action_code_params.form,
+        return await run_action(action=run_action_code_params.action,
+                                form=run_action_code_params.form,
                                 response=run_action_code_params.response)
--- temporal/actions-executor/settings/application.py
+++ temporal/actions-executor/settings/application.py
@@ -1,12 +1,12 @@
-import pydantic
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
 from settings.apm_settings import APMSettings
 
 load_dotenv()
 
 
-class ApplicationSettings(pydantic.BaseSettings):
+class ApplicationSettings(BaseSettings):
     temporal_server_url: str = "localhost:7233"
     namespace: str = "default"
     server_url: str = "http://backend:8000/api/v1"
