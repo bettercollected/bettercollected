edfab794bd556779735fc820a0f4b1090f24cc8a
Sital999 <sitalnagarkoti123@gmail.com>
2023-12-06 16:20:14 +0545

    Update AppBtn on different state for danger btn

--- webapp/src/Components/Common/Input/Button/AppButton.tsx
+++ webapp/src/Components/Common/Input/Button/AppButton.tsx
@@ -18,11 +18,11 @@ export default function AppButton({ children, className, disabled, isLoading, ic
             case ButtonVariant.Primary:
                 return disabled ? 'bg-black-300 text-black-500' : 'bg-brand-500 text-white hover:bg-brand-600 active:bg-brand-800 focus:ring';
             case ButtonVariant.Secondary:
-                return disabled ? 'bg-black-300 text-black-500' : 'bg-black-800 text-white hover:bg-black-900 focus:ring-blue-500 focus-ring active:bg-black-900';
+                return disabled ? 'bg-black-300 text-black-500' : 'bg-black-800 text-white hover:bg-black-900 focus:ring focus:ring-brand-500 active:bg-black-800';
             case ButtonVariant.Tertiary:
                 return disabled ? 'bg-transparent border text-black-500 border-black-300' : 'text-blue-500 border border-brand-500 hover:bg-brand-500 hover:text-white focus:ring focus:ring-blue-500 active:bg-brand-600';
             case ButtonVariant.Danger:
-                return disabled ? 'bg-black-300 text-black-500' : 'bg-red-400 text-white hover:bg-red-500 ';
+                return disabled ? 'bg-black-300 text-black-500' : 'bg-red-400 text-white hover:bg-red-500 active:bg-red-400 focus:ring focus:ring-blue-700';
             case ButtonVariant.Ghost:
                 return disabled ? 'bg-transparent text-black-500' : 'text-brand-500 border border-transparent hover:bg-black-200 outline-none active:border-brand-500 active:bg-black-300';
             case ButtonVariant.DangerGhost:
