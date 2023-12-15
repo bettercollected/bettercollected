3a6c06a14902f628b7d1d2b68971aed128d16c78
Sital999 <sitalnagarkoti123@gmail.com>
2023-12-15 16:34:57 +0545

    Add story for SearchInputField

--- webapp/src/stories/Button.stories.tsx
+++ webapp/src/stories/Button.stories.tsx
@@ -6,25 +6,18 @@ import { ButtonVariant } from '@Components/Common/Input/Button/AppButtonProps';
 import { StoryFn } from '@storybook/react';
 import type { Meta, StoryObj } from '@storybook/react';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof AppButton> = {
     title: 'Common/Button',
     component: AppButton,
     parameters: {
-        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
         layout: 'centered'
     },
-    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
     tags: ['autodocs'],
-    // More on argTypes: https://storybook.js.org/docs/api/argtypes
     argTypes: {}
 };
 
 export default meta;
 type Story = StoryObj<typeof AppButton>;
-
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-
 const Template: StoryFn<AppButtonProps> = (args) => <AppButton {...args} />;
 
 export const ButtonWithIcon = Template.bind({});
--- webapp/src/stories/Textfield.stories.tsx
+++ webapp/src/stories/Textfield.stories.tsx
@@ -3,23 +3,17 @@ import AppTextField from '@Components/Common/Input/AppTextField';
 import type { Meta, StoryObj } from '@storybook/react';
 import { StoryFn } from '@storybook/react';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof AppTextField> = {
     title: 'Common/TextField',
     component: AppTextField,
     parameters: {
-        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
         layout: 'centered'
     },
-    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
     tags: ['autodocs'],
-    // More on argTypes: https://storybook.js.org/docs/api/argtypes
     argTypes: {}
 };
 
 export default meta;
-
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 
 const Template: StoryFn<typeof AppTextField> = (args) => <AppTextField {...args} />;
 
