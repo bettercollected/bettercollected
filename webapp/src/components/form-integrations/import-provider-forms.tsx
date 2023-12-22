d83443df8717f226042aaed03360feddc25b3a77
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-12-22 10:27:25 +0545

    Refactor Oauth verification flow with new design

--- webapp/src/components/form-integrations/import-provider-forms.tsx
+++ webapp/src/components/form-integrations/import-provider-forms.tsx
@@ -1,54 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-import {useTranslation} from 'next-i18next';
-import {useRouter} from 'next/router';
+import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
 
-import Divider from '@Components/Common/DataDisplay/Divider';
+import AppTextField from '@Components/Common/Input/AppTextField';
 import AppButton from '@Components/Common/Input/Button/AppButton';
-import {ButtonSize} from '@Components/Common/Input/Button/AppButtonProps';
+import { ButtonSize } from '@Components/Common/Input/Button/AppButtonProps';
 import Joyride from '@Components/Joyride';
-import {JoyrideStepContent, JoyrideStepTitle} from '@Components/Joyride/JoyrideStepTitleAndContent';
-import {Autocomplete, Box, TextField, createFilterOptions} from '@mui/material';
-import MuiButton from '@mui/material/Button';
-import {toast} from 'react-toastify';
+import { JoyrideStepContent, JoyrideStepTitle } from '@Components/Joyride/JoyrideStepTitleAndContent';
+import { toast } from 'react-toastify';
 
 import ImportErrorView from '@app/components/form-integrations/import-error-view';
-import {TypeformIcon} from '@app/components/icons/brands/typeform';
-import {Close} from '@app/components/icons/close';
-import {GoogleFormIcon} from '@app/components/icons/google-form-icon';
-import {useModal} from '@app/components/modal-views/context';
-import FullScreenLoader from '@app/components/ui/fullscreen-loader';
-import ActiveLink from '@app/components/ui/links/active-link';
+import { useModal } from '@app/components/modal-views/context';
+import Loader from '@app/components/ui/loader';
 import environments from '@app/configs/environments';
-import {buttonConstant} from '@app/constants/locales/button';
-import {importFormConstant} from '@app/constants/locales/import-form';
-import {toastMessage} from '@app/constants/locales/toast-message';
-import {Provider} from '@app/models/enums/provider';
-import {setBuilderState} from '@app/store/form-builder/actions';
-import {initBuilderState} from '@app/store/form-builder/builderSlice';
-import {useAppDispatch, useAppSelector} from '@app/store/hooks';
-import {JOYRIDE_CLASS, JOYRIDE_ID} from '@app/store/tours/types';
-import {
-    useCreateFormMutation,
-    useImportFormMutation,
-    useLazyGetMinifiedFormsQuery,
-    useLazyGetSingleFormFromProviderQuery, useLazyVerifyFormTokenQuery, useVerifyFormTokenQuery
-} from '@app/store/workspaces/api';
-import AppTextField from "@Components/Common/Input/AppTextField";
-import {setForm} from "@app/store/forms/slice";
-import Loader from "@app/components/ui/loader";
-
-interface IIntegrations {
-    provider: string;
-    icon: React.ReactElement;
-    name: string;
-    onClick: any;
-}
-
-interface IAutoCompleteFormValueProps {
-    label: string;
-    formId: string;
-}
+import { buttonConstant } from '@app/constants/locales/button';
+import { toastMessage } from '@app/constants/locales/toast-message';
+import { Provider } from '@app/models/enums/provider';
+import { setBuilderState } from '@app/store/form-builder/actions';
+import { initBuilderState } from '@app/store/form-builder/builderSlice';
+import { useAppDispatch, useAppSelector } from '@app/store/hooks';
+import { JOYRIDE_CLASS, JOYRIDE_ID } from '@app/store/tours/types';
+import { useCreateFormMutation, useImportFormMutation, useLazyGetSingleFormFromProviderQuery, useVerifyFormTokenQuery } from '@app/store/workspaces/api';
 
 interface IAutoCompleteFormFieldProps {
     label: string;
@@ -56,31 +29,30 @@ interface IAutoCompleteFormFieldProps {
 }
 
 export default function ImportProviderForms(props: any) {
-    const {closeModal} = useModal();
-    const {t} = useTranslation();
+    const { closeModal } = useModal();
+    const { t } = useTranslation();
     const [provider, setProvider] = useState(props?.provider ?? 'google');
 
     const [responseDataOwner, setResponseDataOwner] = useState<IAutoCompleteFormFieldProps | null>(null);
 
     const workspace = useAppSelector((state) => state.workspace);
 
-
     const [singleFormFromProviderTrigger, singleFormFromProviderResult] = useLazyGetSingleFormFromProviderQuery();
 
     const [importForm, importFormResult] = useImportFormMutation();
 
-    const {data, isLoading, error: verificationError} = useVerifyFormTokenQuery({provider: "google"})
+    const { data, isLoading, error: verificationError } = useVerifyFormTokenQuery({ provider: 'google' });
 
-    const [formId, setFormId] = useState("")
-    const [error, setError] = useState(false)
+    const [formId, setFormId] = useState('');
+    const [error, setError] = useState(false);
 
-    const [importFormLink, setImportFormLink] = useState("")
+    const [importFormLink, setImportFormLink] = useState('');
 
     const router = useRouter();
-    const [postCreateForm, {isLoading: posting}] = useCreateFormMutation();
+    const [postCreateForm, { isLoading: posting }] = useCreateFormMutation();
     const dispatch = useAppDispatch();
 
-    const [showErrorView, setShowError] = useState(false)
+    const [showErrorView, setShowError] = useState(false);
 
     useEffect(() => {
         const pattern = /https:\/\/docs\.google\.com\/forms\/d\/([^/]+)\/edit/;
@@ -89,20 +61,20 @@ export default function ImportProviderForms(props: any) {
 
         if (match) {
             const formId = match[1];
-            setFormId(formId)
-            setError(false)
+            setFormId(formId);
+            setError(false);
         } else {
-            setFormId("");
-            setError(true)
+            setFormId('');
+            setError(true);
         }
-    }, [importFormLink])
+    }, [importFormLink]);
 
     useEffect(() => {
         if (verificationError || data?.status_code === 400) {
-            setShowError(true)
+            setShowError(true);
         }
         if (data?.status_code === 200) {
-            router.push(`/${workspace?.workspaceName}/dashboard/forms/import`)
+            router.push(`/${workspace?.workspaceName}/dashboard/forms/import`);
         }
     }, [verificationError, data]);
 
@@ -112,15 +84,15 @@ export default function ImportProviderForms(props: any) {
     };
 
     const handleImportForm = async () => {
-        const singleForm = await singleFormFromProviderTrigger({provider: "google", formId: formId})
-        const form: any = {...singleForm?.data, provider};
+        const singleForm = await singleFormFromProviderTrigger({ provider: 'google', formId: formId });
+        const form: any = { ...singleForm?.data, provider };
         delete form['clientFormItems'];
         if (singleForm.error) {
-            setShowError(true)
-            return
+            setShowError(true);
+            return;
         }
         const response: any = await importForm({
-            body: {form, response_data_owner: responseDataOwner?.questionId ?? ''},
+            body: { form, response_data_owner: responseDataOwner?.questionId ?? '' },
             provider,
             workspaceId: workspace.id
         });
@@ -143,12 +115,12 @@ export default function ImportProviderForms(props: any) {
         postReq.consent = [];
         postReq.settings = {};
         formData.append('form_body', JSON.stringify(postReq));
-        const apiObj: any = {workspaceId: workspace.id, body: formData};
+        const apiObj: any = { workspaceId: workspace.id, body: formData };
         const response: any = await postCreateForm(apiObj);
 
         if (response?.data) {
             router.push(`/${workspace?.workspaceName}/dashboard/forms/${response?.data?.formId}/edit`);
-            dispatch(setBuilderState({isFormDirty: false}));
+            dispatch(setBuilderState({ isFormDirty: false }));
         }
     };
 
@@ -162,12 +134,11 @@ export default function ImportProviderForms(props: any) {
                     scrollOffset={0}
                     steps={[
                         {
-                            title: <JoyrideStepTitle text="Select your form"/>,
+                            title: <JoyrideStepTitle text="Select your form" />,
                             content: (
                                 <JoyrideStepContent>
-                                    Select the form that you want to import. <br/>
-                                    <br/> If you do not see any forms in the list, then you may need to create some
-                                    forms within the form provider account that you selected (Google Forms, Typeform).
+                                    Select the form that you want to import. <br />
+                                    <br /> If you do not see any forms in the list, then you may need to create some forms within the form provider account that you selected (Google Forms, Typeform).
                                 </JoyrideStepContent>
                             ),
                             target: `.${JOYRIDE_CLASS.WORKSPACE_ADMIN_FORM_IMPORT_LIST_FORMS}`,
@@ -179,34 +150,28 @@ export default function ImportProviderForms(props: any) {
             )}
             <h4 className="h6 !text-black-800 !font-medium text-center">{provider === Provider.google ? 'Copy Google Form Edit Link' : 'Select TypeForm'}</h4>
             <div className={'text-sm font-normal text-black-700 '}>
-                Paste the edit page link of your google form. If there
-                are no existing forms, you can always
-                <span onClick={createNewForm}
-                      className="pl-1 body4 !not-italic !text-brand-500 hover:!text-brand-600 cursor-pointer">
+                Paste the edit page link of your google form. If there are no existing forms, you can always
+                <span onClick={createNewForm} className="pl-1 body4 !not-italic !text-brand-500 hover:!text-brand-600 cursor-pointer">
                     Create bettercollected Form.
                 </span>
             </div>
             <div className="flex flex-col w-full h-full gap-4 mt-4">
-                <AppTextField value={importFormLink} onChange={(event) => {
-                    setImportFormLink(event.target.value)
-                }}/>
-                <AppButton
-                    className={'!w-full'}
-                    isLoading={!!importFormResult?.isLoading || !!singleFormFromProviderResult?.isLoading}
-                    onClick={handleImportForm}
-                    disabled={error}
-                    size={ButtonSize.Medium}
-                >
+                <AppTextField
+                    value={importFormLink}
+                    onChange={(event) => {
+                        setImportFormLink(event.target.value);
+                    }}
+                />
+                <AppButton className={'!w-full'} isLoading={!!importFormResult?.isLoading || !!singleFormFromProviderResult?.isLoading} onClick={handleImportForm} disabled={error} size={ButtonSize.Medium}>
                     {t(buttonConstant.importNow)}
                 </AppButton>
             </div>
         </>
     );
 
+    if (showErrorView) return <ImportErrorView provider={provider} />;
 
-    if (showErrorView) return <ImportErrorView provider={provider}/>;
-
-    if (isLoading) return <Loader variant={'blink'}/>
+    if (isLoading) return <Loader variant={'blink'} />;
 
     return (
         // <div
