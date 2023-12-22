dcee23118bb3628552ede6e903848c1d316b7acc
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-12-22 14:04:43 +0545

    Add change slug component after form import

--- webapp/src/Components/ImportForm/ChangeSlugComponent.tsx
+++ webapp/src/Components/ImportForm/ChangeSlugComponent.tsx
@@ -1,26 +1,38 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+
+import { useRouter } from 'next/router';
 
 import CheckedCircle from '@Components/Common/Icons/Common/CheckedCircle';
+import AppButton from '@Components/Common/Input/Button/AppButton';
+import { ButtonSize, ButtonVariant } from '@Components/Common/Input/Button/AppButtonProps';
 
 import FormSettingsTab from '@app/components/dashboard/form-settings';
-import { resetSingleForm } from '@app/store/forms/slice';
-import { useAppDispatch } from '@app/store/hooks';
+import { useAppSelector } from '@app/store/hooks';
+import { selectWorkspace } from '@app/store/workspaces/slice';
 
 export default function ChangeSlugComponent() {
-    const dispatch = useAppDispatch();
-    useEffect(() => {
-        return () => {
-            dispatch(resetSingleForm());
-        };
-    }, []);
-
+    const workspace = useAppSelector(selectWorkspace);
+    const router = useRouter();
     return (
-        <>
-            <div className="flex gap-4 mb-[72px]">
+        <div className="md:w-[500px] mx-5">
+            <div className="flex gap-4 items-center justify-center mb-[72px]">
                 <CheckedCircle />
                 <span className="h3-new text-black-900">Form Imported Successfully!</span>
             </div>
+            <div className="h4-new">Form Link</div>
             <FormSettingsTab view="LINKS" />
-        </>
+            <div className="mt-[72px] flex flex-col">
+                <div className="text-black-700 mb-4 p2-new">You can always customize your form link later</div>
+                <AppButton
+                    variant={ButtonVariant.Primary}
+                    size={ButtonSize.Medium}
+                    onClick={() => {
+                        router.push(`/${workspace.workspaceName}/dashboard`);
+                    }}
+                >
+                    Done, Go to Dashboard
+                </AppButton>
+            </div>
+        </div>
     );
 }
