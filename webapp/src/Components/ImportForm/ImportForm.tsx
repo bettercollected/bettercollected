494b83b2b7637ea52b6479bf10156c74ca45e019
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-12-26 11:38:38 +0545

    Add confetti effect after successful import

--- webapp/src/Components/ImportForm/ImportForm.tsx
+++ webapp/src/Components/ImportForm/ImportForm.tsx
@@ -8,13 +8,14 @@ import _ from 'lodash';
 import AppTextField from '@Components/Common/Input/AppTextField';
 import ChangeSlugComponent from '@Components/ImportForm/ChangeSlugComponent';
 import { CircularProgress } from '@mui/material';
+import confetti from 'canvas-confetti';
 import { toast } from 'react-toastify';
 
 import GoogleFolder from '@app/assets/images/google_folder.png';
 import GoogleForm from '@app/assets/images/google_form.png';
 import { LinkIcon } from '@app/components/icons/link-icon';
 import { toastMessage } from '@app/constants/locales/toast-message';
-import { resetSingleForm, selectForm, setForm } from '@app/store/forms/slice';
+import { selectForm, setForm } from '@app/store/forms/slice';
 import { useAppDispatch, useAppSelector } from '@app/store/hooks';
 import { useImportFormMutation, useLazyGetSingleFormFromProviderQuery } from '@app/store/workspaces/api';
 
@@ -81,6 +82,7 @@ const ImportForm = () => {
         if (response.data) {
             toast.success(t(toastMessage.formImportedSuccessfully).toString());
             dispatch(setForm(response?.data));
+            fireworks();
         } else {
             setError(true);
             setErrorMessage('error');
@@ -137,3 +139,26 @@ const LoadingIconWithText = ({ text }: { text: string }) => {
         </div>
     );
 };
+
+function fireworks() {
+    let duration = 3 * 1000;
+    let animationEnd = Date.now() + duration;
+    let defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min: number, max: number) {
+        return Math.random() * (max - min) + min;
+    }
+
+    let interval: NodeJS.Timer = setInterval(function () {
+        let timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+            return clearInterval(interval);
+        }
+
+        let particleCount = 100 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+    }, 250);
+}
