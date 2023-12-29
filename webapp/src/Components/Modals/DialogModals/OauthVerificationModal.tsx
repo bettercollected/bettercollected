c115a486e2f6b29a5c9801cb53246789b13afdbc
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2023-12-29 12:05:28 +0545

    Fix route on back form import forms view and close modal on route change

--- webapp/src/Components/Modals/DialogModals/OauthVerificationModal.tsx
+++ webapp/src/Components/Modals/DialogModals/OauthVerificationModal.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react';
 
+import { useRouter } from 'next/router';
+
 import ImportErrorView from '@app/components/form-integrations/import-error-view';
+import { useModal } from '@app/components/modal-views/context';
 
 export default function OauthVerificationModal({ provider = 'google' }: { provider?: string }) {
+    const { closeModal } = useModal();
+
+    const router = useRouter();
     useEffect(() => {
         const blockCloseOnEscape = (event: KeyboardEvent) => {
             console.log();
@@ -11,8 +17,15 @@ export default function OauthVerificationModal({ provider = 'google' }: { provid
                 event.preventDefault();
             }
         };
+        const closeModalHandler = () => {
+            closeModal();
+        };
         document.addEventListener('keydown', blockCloseOnEscape);
+        // close search modal when route change
+        router.events.on('routeChangeStart', closeModalHandler);
         return () => {
+            router.events.off('routeChangeStart', closeModalHandler);
+
             document.removeEventListener('keydown', blockCloseOnEscape);
         };
     }, []);
