c4bc250180fd8a2a25975a8e1a9958a02f42533b
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-01-03 13:19:21 +0545

    Add google picker API

--- webapp/src/components/form-integrations/import-forms-button.tsx
+++ webapp/src/components/form-integrations/import-forms-button.tsx
@@ -6,16 +6,15 @@ import { useRouter } from 'next/router';
 import Tooltip from '@Components/Common/DataDisplay/Tooltip';
 import AppButton from '@Components/Common/Input/Button/AppButton';
 import { ButtonSize, ButtonVariant } from '@Components/Common/Input/Button/AppButtonProps';
+import useDrivePicker from '@fyelci/react-google-drive-picker';
 
 import FormProviderContext from '@app/Contexts/FormProviderContext';
-import { useModal } from '@app/components/modal-views/context';
 import { buttonConstant } from '@app/constants/locales/button';
 import { toolTipConstant } from '@app/constants/locales/tooltip';
 import { useAppSelector } from '@app/store/hooks';
 import { selectWorkspace } from '@app/store/workspaces/slice';
 
 export default function ImportFormsButton({ size, className = '' }: { size?: ButtonSize; className?: string }) {
-    const { openModal } = useModal();
     const router = useRouter();
     const { t } = useTranslation();
     const formProviders = useContext(FormProviderContext);
@@ -27,8 +26,23 @@ export default function ImportFormsButton({ size, className = '' }: { size?: But
         typeform: false
     });
 
+    const [openPicker] = useDrivePicker();
+
     const handleClick = () => {
-        router.push(`/${workspace?.workspaceName}/dashboard/forms/import`);
+        // router.push(`/${workspace?.workspaceName}/dashboard/forms/import`);
+        openPicker({
+            clientId: '132120488980-hdf9tjq86k4km9kad2et532si5khuri9.apps.googleusercontent.com',
+            developerKey: 'AIzaSyA6OLL3bCqL2q3A5pl6CoPLq9LLO-p2-ok',
+            viewId: 'FORMS',
+            // token: 'ya29.a0AfB_byBmSGCAwYH-j19Ay2xd9pwghLNfB-qgrDzIdtZsjfgGRK9A1eG0LreT28QURwG8rMw7q06NYsqciBIzuzqXHzMOZrAUrrEOZ6hO3vZ1jgZFp3V0mCxXLUeG2cvXbp4mv-2lmgDITKWD-XvmRXKk91KukMrW5zpcbQaCgYKARISARMSFQHGX2MiKG-chxR44odTwCjOlPuUeQ0173',
+            customScopes: ['https://www.googleapis.com/auth/drive.file'],
+            callbackFunction: (data) => {
+                console.log(data);
+                if (data.action === 'cancel') {
+                    router.push(router.asPath);
+                }
+            }
+        });
     };
 
     useEffect(() => {
