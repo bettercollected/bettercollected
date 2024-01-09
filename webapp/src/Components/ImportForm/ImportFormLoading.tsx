5e43a6200426720d092e2861c1517e40fd7f5a35
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-01-09 15:50:02 +0545

    Update import form workflow to latest design

--- webapp/src/Components/ImportForm/ImportFormLoading.tsx
+++ webapp/src/Components/ImportForm/ImportFormLoading.tsx
@@ -13,7 +13,7 @@ interface ImportFormLoadingProps {
 
 export default function ImportFormLoading({ loadingText, formTitle }: ImportFormLoadingProps) {
     return (
-        <div className="flex flex-col h-full justify-center items-center">
+        <div className="flex flex-col h-full min-h-[480px] md:min-h-[480px] md:w-[777px] lg:w-[1052px] lg:h-[650px] justify-center items-center">
             <Image className={'pb-1'} src={GoogleFolder} alt={'GoogleFolder'} />
             <div className=" h3-new mt-4">{loadingText}</div>
             <div className=" mt-2">{formTitle}</div>
