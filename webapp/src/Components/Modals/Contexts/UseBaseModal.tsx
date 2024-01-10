0569c71a0405751b4ed978107e388bc05b115d05
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-01-10 10:51:23 +0545

    Refactor useModal hook

---
+++ webapp/src/Components/Modals/Contexts/UseBaseModal.tsx
@@ -0,0 +1,27 @@
+import { PrimitiveAtom, useAtom } from 'jotai';
+
+export function useBaseModal<T>(
+    atom: PrimitiveAtom<{
+        isOpen: boolean;
+        view: T | '';
+        modalProps: any;
+    }>
+) {
+    const [state, setState] = useAtom(atom);
+    const openModal = (view: T, modalProps: any = null) =>
+        setState({
+            ...state,
+            isOpen: true,
+            view,
+            modalProps
+        });
+    const closeModal = () => {
+        setState({ ...state, isOpen: false, modalProps: null, view: '' });
+    };
+
+    return {
+        ...state,
+        openModal,
+        closeModal
+    };
+}
