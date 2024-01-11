fe506dfe1c1b95d6d0f7718d611b2c7e2af79d3e
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-01-11 16:34:48 +0545

    Add function and api to reinitialize form import schedules

--- backend/backend/config/temporal_settings.py
+++ backend/backend/config/temporal_settings.py
@@ -8,6 +8,7 @@ class TemporalSettings(BaseSettings):
     worker_queue: str = "default"
     action_queue: str = "actions"
     csv_queue: str = "csv_worker"
+    add_import_schedules: bool = False
 
     class Config:
         env_prefix = "TEMPORAL_"
