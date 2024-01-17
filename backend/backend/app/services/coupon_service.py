57256549ff9ee819ec1fc63de87bcda5ffe9ca55
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-01-17 15:18:38 +0545

    Refresh tokens upon code redeem successful

--- backend/backend/app/services/coupon_service.py
+++ backend/backend/app/services/coupon_service.py
@@ -3,11 +3,13 @@ from http import HTTPStatus
 from common.models.user import User
 
 from backend.app.exceptions import HTTPException
+from backend.app.models.dtos.kafka_event_dto import UserEventType
 from backend.app.models.enum.coupon_status import CouponStatus
 from backend.app.models.types.coupon_code import CouponCode
 from backend.app.repositories.coupon_repository import CouponRepository
 from backend.app.schemas.coupon_codes import CouponCodeDocument
 from backend.app.services.auth_service import AuthService
+from backend.app.services.kafka_service import event_logger_service
 from backend.app.services.workspace_service import WorkspaceService
 
 
@@ -32,6 +34,7 @@ class CouponService:
             raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, content="Invalid coupon code")
         await self.auth_service.upgrade_user_to_pro(user=user)
         await self.workspace_service.upgrade_user_workspace(user_id=user.id)
+        await event_logger_service.send_event(UserEventType.USER_UPGRADED_TO_PRO, user_id=user.id, email=user.sub)
         coupon_document.status = CouponStatus.USED
         coupon_document.used_by = user.id
         await coupon_document.save()
