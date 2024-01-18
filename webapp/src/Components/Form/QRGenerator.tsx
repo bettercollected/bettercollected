392127f174a924062afae0d5c9e53197982d589f
Sital999 <sitalnagarkoti123@gmail.com>
2024-01-18 10:10:15 +0545

    ADd component for generating QR Code

---
+++ webapp/src/Components/Form/QRGenerator.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {useQRCode} from "next-qrcode";
+import {getFormUrl} from "@app/utils/urlUtils";
+import {useAppSelector} from "@app/store/hooks";
+import {selectForm} from "@app/store/forms/slice";
+import {selectWorkspace} from "@app/store/workspaces/slice";
+
+const QRGenerator = () => {
+    const {Canvas} = useQRCode();
+    const workspaceForm = useAppSelector(selectForm)
+    const workspace = useAppSelector(selectWorkspace)
+    const formUrl = getFormUrl(workspaceForm, workspace)
+
+    return <Canvas
+        text={formUrl}
+        options={{
+            errorCorrectionLevel: 'M',
+            margin: 3,
+            scale: 8,
+            width: 250,
+            color: {
+                light: '#DBDBDB',
+                dark: '#0764EB',
+            },
+        }}
+    />
+}
+
+export default QRGenerator
