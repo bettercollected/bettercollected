c80a3d64c99746ae38d85eb2c4decc403dcd77ee
Sital999 <sitalnagarkoti123@gmail.com>
2024-01-19 13:45:42 +0545

    Fix the width of workspaceDropdown

--- webapp/src/components/auth/navbar.tsx
+++ webapp/src/components/auth/navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useTranslation } from 'next-i18next';
+import {useTranslation} from 'next-i18next';
 
 import AppButton from '@Components/Common/Input/Button/AppButton';
 
@@ -8,10 +8,10 @@ import AuthAccountMenuDropdown from '@app/components/auth/account-menu-dropdown'
 import ProPlanHoc from '@app/components/hoc/pro-plan-hoc';
 import Hamburger from '@app/components/ui/hamburger';
 import Logo from '@app/components/ui/logo';
-import { buttonConstant } from '@app/constants/locales/button';
-import { useBreakpoint, useIsMobile } from '@app/lib/hooks/use-breakpoint';
-import { useIsMounted } from '@app/lib/hooks/use-is-mounted';
-import { useWindowScroll } from '@app/lib/hooks/use-window-scroll';
+import {buttonConstant} from '@app/constants/locales/button';
+import {useBreakpoint, useIsMobile} from '@app/lib/hooks/use-breakpoint';
+import {useIsMounted} from '@app/lib/hooks/use-is-mounted';
+import {useWindowScroll} from '@app/lib/hooks/use-window-scroll';
 
 import LocaleDropdownUi from '../ui/locale-dropdown-ui';
 
@@ -36,7 +36,8 @@ AuthNavbar.defaultProps = {
     showHamburgerIcon: true,
     isMobileView: false,
     showAuthAccount: true,
-    handleDrawerToggle: () => {}
+    handleDrawerToggle: () => {
+    }
 };
 
 export function Header(props: any) {
@@ -46,23 +47,36 @@ export function Header(props: any) {
     const propClassNames = props?.className ?? '';
     const navClassNames = isMounted && windowScroll.y > 10 ? 'bg-white shadow-card dark:from-dark dark:to-dark/80' : 'border-b-[0.5px] border-neutral-100 dark:border-neutral-700 bg-white dark:bg-dark';
 
-    return <nav className={`!fixed top-0 !z-30 border-b-[1px] border-black-400 flex w-full items-center justify-between px-5 transition-all duration-300 ltr:right-0 rtl:left-0 h-[68px] ${navClassNames} ${propClassNames}`}>{props.children}</nav>;
+    return <nav
+        className={`!fixed top-0 !z-30 border-b-[1px] border-black-400 flex w-full items-center justify-between px-5 transition-all duration-300 ltr:right-0 rtl:left-0 h-[68px] ${navClassNames} ${propClassNames}`}>{props.children}</nav>;
 }
 
-function AuthNavbar({ showHamburgerIcon, showPlans, mobileOpen, handleDrawerToggle, isCustomDomain = false, isFooter = false, isClientDomain = false, hideMenu = false, showAuthAccount }: IAuthNavbarProps) {
-    const { t } = useTranslation();
+function AuthNavbar({
+                        showHamburgerIcon,
+                        showPlans,
+                        mobileOpen,
+                        handleDrawerToggle,
+                        isCustomDomain = false,
+                        isFooter = false,
+                        isClientDomain = false,
+                        hideMenu = false,
+                        showAuthAccount
+                    }: IAuthNavbarProps) {
+    const {t} = useTranslation();
     const inMobile = useIsMobile();
     return (
         <Header className="!z-[1300]">
             <div className="flex flex-row w-full h-full py-2 md:py-0 justify-between items-center">
                 <div className="flex gap-4">
-                    {inMobile && showHamburgerIcon && <Hamburger isOpen={mobileOpen} className="!shadow-none mr-2 !bg-white hover:!bg-white !text-black-900 !flex !justify-start" onClick={handleDrawerToggle} />}
-                    <Logo isCustomDomain={isCustomDomain} isFooter={isFooter} isClientDomain={isClientDomain} />
+                    {inMobile && showHamburgerIcon && <Hamburger isOpen={mobileOpen}
+                                                                 className="!shadow-none mr-2 !bg-white hover:!bg-white !text-black-900 !flex !justify-start"
+                                                                 onClick={handleDrawerToggle}/>}
+                    <Logo isCustomDomain={isCustomDomain} isFooter={isFooter} isClientDomain={isClientDomain}/>
                 </div>
                 <div className="flex items-center justify-center gap-7">
                     {!inMobile && (
                         <>
-                            <LocaleDropdownUi />
+                            <LocaleDropdownUi/>
                             {showPlans && (
                                 <ProPlanHoc hideChildrenIfPro={true}>
                                     <AppButton>{t(buttonConstant.upgrade)}</AppButton>
@@ -70,7 +84,8 @@ function AuthNavbar({ showHamburgerIcon, showPlans, mobileOpen, handleDrawerTogg
                             )}
                         </>
                     )}
-                    {showAuthAccount && <AuthAccountMenuDropdown hideMenu={hideMenu} isClientDomain={isClientDomain} />}
+                    {showAuthAccount &&
+                        <AuthAccountMenuDropdown hideMenu={hideMenu} isClientDomain={isClientDomain}/>}
                 </div>
             </div>
         </Header>
