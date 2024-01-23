96af96e87afe06d3bf2324ac16923447b6ae3f4a
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-01-23 17:00:29 +0545

    Integrate collect emails flow

---
+++ webapp/src/Components/Modals/DialogModals/SignInToFillFormModal.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import AppButton from '@Components/Common/Input/Button/AppButton';
+import { ButtonSize } from '@Components/Common/Input/Button/AppButtonProps';
+import HeaderModalWrapper from '@Components/Modals/ModalWrappers/HeaderModalWrapper';
+
+import { useModal } from '@app/components/modal-views/context';
+import { useFullScreenModal } from '@app/components/modal-views/full-screen-modal-context';
+
+export default function SignInToFillFormModal() {
+    const { closeModal } = useModal();
+    const { openModal: openFullScreenModal } = useFullScreenModal();
+    const onClickSignInButton = (event: React.MouseEvent<HTMLButtonElement>) => {
+        closeModal();
+        openFullScreenModal('LOGIN_VIEW', { nonClosable: true });
+    };
+
+    return (
+        <HeaderModalWrapper showClose={false} headerTitle="Sign In">
+            <span className="text-black-800 text-base font-semibold">Get Form Access</span>
+            <span className="p2-new text-sm mt-2 mb-6 text-black-700">Sign in to get access to the form.</span>
+            <AppButton size={ButtonSize.Medium} onClick={onClickSignInButton}>
+                Sign In
+            </AppButton>
+        </HeaderModalWrapper>
+    );
+}
