991eafb66a4d69277ba3ee38f604e513df86847d
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-02-08 20:49:12 +0545

    UI updates and fixes

--- webapp/src/stories/ParamTabs.stories.tsx
+++ webapp/src/stories/ParamTabs.stories.tsx
@@ -1,9 +1,6 @@
-import type {Meta, StoryObj} from '@storybook/react';
-import GenericHalfModal from "@Components/Common/Modals/GenericHalfModal";
-import ParamTab, {TabPanel} from "@app/components/ui/param-tab";
-import WorkspaceDetails from "@Components/Workspace/Settings/WorkspaceDetails";
-import ManageURLs from "@Components/Workspace/Settings/ManageURLs";
+import type { Meta, StoryObj } from '@storybook/react';
 
+import ParamTab, { TabPanel } from '@app/components/ui/param-tab';
 
 const meta: Meta<typeof ParamTab> = {
     title: 'Common/ParamTab',
@@ -13,23 +10,31 @@ const meta: Meta<typeof ParamTab> = {
     },
     tags: ['autodocs'],
     argTypes: {},
-    decorators: (Story) => <div className={'bg-white p-2'}><Story/></div>
+    decorators: (Story) => (
+        <div className={'bg-white p-2'}>
+            <Story />
+        </div>
+    )
 };
 
 export default meta;
 type Story = StoryObj<typeof ParamTab>;
 
-const Content1 = () => <div>
-    <p className={'text-sm text-black-700 '}>It is content 1 of param-tabs component to render on storybook.</p>
-</div>
-const Content2 = () => <div>
-    <p className={'text-sm text-black-700 '}>It is content 2 of param-tabs component to render on storybook.</p>
-
-</div>
-const Content3 = () => <div>
-    <p className={'text-sm text-black-700 '}>It is content 3 of param-tabs component to render on storybook.</p>
-
-</div>
+const Content1 = () => (
+    <div>
+        <p className={'text-sm text-black-700 '}>It is content 1 of param-tabs component to render on storybook.</p>
+    </div>
+);
+const Content2 = () => (
+    <div>
+        <p className={'text-sm text-black-700 '}>It is content 2 of param-tabs component to render on storybook.</p>
+    </div>
+);
+const Content3 = () => (
+    <div>
+        <p className={'text-sm text-black-700 '}>It is content 3 of param-tabs component to render on storybook.</p>
+    </div>
+);
 
 const tabMenu = [
     {
@@ -46,34 +51,33 @@ const tabMenu = [
     }
 ];
 
-const ChildrenComponent = () => <div className={'p-2'}>
-    <TabPanel>
-        <Content1/>
-    </TabPanel>
-    <TabPanel>
-        <Content2/>
-    </TabPanel>
-    <TabPanel>
-        <Content3/>
-    </TabPanel>
-</div>
+const ChildrenComponent = () => (
+    <div className={'p-2'}>
+        <TabPanel>
+            <Content1 />
+        </TabPanel>
+        <TabPanel>
+            <Content2 />
+        </TabPanel>
+        <TabPanel>
+            <Content3 />
+        </TabPanel>
+    </div>
+);
 
 export const ParamTabsStory: Story = {
     args: {
         initialIndex: 0,
         tabMenu: tabMenu,
         isRouteChangeable: false,
-        children: <ChildrenComponent/>
+        children: <ChildrenComponent />
     }
-}
+};
 export const ParamTabsStoryWithLastIndex: Story = {
     args: {
         initialIndex: 2,
         tabMenu: tabMenu,
         isRouteChangeable: false,
-        children: <ChildrenComponent/>
+        children: <ChildrenComponent />
     }
-}
-
-
-
+};
