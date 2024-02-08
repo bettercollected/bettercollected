1684f23349c52245f375d622bd544209ec0db3cf
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-02-08 20:49:12 +0545

    UI updates and fixes

--- webapp/src/components/icons/flags/globe.tsx
+++ webapp/src/components/icons/flags/globe.tsx
@@ -6,7 +6,7 @@ export default function Globe(props: React.SVGAttributes<{}>) {
             <path
                 d="M21 12C21 16.9706 16.9706 21 12 21M21 12C21 7.02944 16.9706 3 12 3M21 12H3M12 21C7.02944 21 3 16.9706 3 12M12 21C12 21 16 18 16 12C16 6 12 3 12 3M12 21C12 21 8 18 8 12C8 6 12 3 12 3M3 12C3 7.02944 7.02944 3 12 3"
                 stroke="currentColor"
-                strokeWidth="2"
+                strokeWidth={props.strokeWidth || 2}
             />
         </svg>
     );
