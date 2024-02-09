bd12f603c3ec9933f6e505d97fba6ba7520190ee
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2024-02-09 10:47:59 -0500

    Fix nearby element detection boundary lines position

--- webbuilder/src/app/page.tsx
+++ webbuilder/src/app/page.tsx
@@ -2,6 +2,7 @@ import SlideCanvas from '@app/views/molecules/SlideCanvas';
 
 export default function Home() {
     return (
+        // DO NOT REMOVE OR ADD ANY PADDING OR MARGIN HERE, PADDING IS USED TO SUBTRACT IN THE NEARBY ELEMENT DETECTION ON CANVAS
         <main className="flex min-h-screen flex-col items-center justify-between bg-black-100 p-4">
             <SlideCanvas />
         </main>
