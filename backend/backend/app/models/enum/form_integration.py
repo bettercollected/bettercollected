c8ecc105cf07ef61784257dff15f59d631987360
Sital999 <sitalnagarkoti123@gmail.com>
2024-02-12 15:39:48 +0545

    Add FormActionType enum and update value of FormIntegrationType.GOOGLE_SHEET

--- backend/backend/app/models/enum/form_integration.py
+++ backend/backend/app/models/enum/form_integration.py
@@ -4,5 +4,9 @@ import enum
 class FormIntegrationType(str, enum.Enum):
     """Enum representing the different form providers that are supported."""
 
-    GOOGLE_SHEET: str = "google_sheet"
+    GOOGLE_SHEET: str = "integrate_google_sheets"
 
+
+class FormActionType(str, enum.Enum):
+    INTERNAL: str = "Internal"
+    EXTERNAL: str = "External"
