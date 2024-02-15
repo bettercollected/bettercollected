026eb91b2991f910327b6d54deb380f1e3cf5daa
Sital999 <sitalnagarkoti123@gmail.com>
2024-02-15 11:55:00 +0545

    Add button as atom

---
+++ webbuilder/src/models/enums/button.tsx
@@ -0,0 +1,15 @@
+export enum ButtonVariant {
+    Primary = 'primary',
+    Secondary = 'secondary',
+    Tertiary = 'tertiary',
+    Danger = 'danger',
+    Ghost = 'ghost',
+    DangerGhost = 'danger_ghost'
+}
+
+export enum ButtonSize {
+    Small = 'small',
+    Medium = 'medium',
+    Big = 'big',
+    Tiny = 'tiny'
+}
