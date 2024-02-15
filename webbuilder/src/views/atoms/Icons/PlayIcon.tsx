14c33bc5e3b5fc98aaeb63547dd5879239bfe819
Sital999 <sitalnagarkoti123@gmail.com>
2024-02-15 11:54:07 +0545

    Add new icon

---
+++ webbuilder/src/views/atoms/Icons/PlayIcon.tsx
@@ -0,0 +1,8 @@
+export default function PlayIcon(props: React.SVGAttributes<any>) {
+    return <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path
+            d="M0 12.0384V1.88005C0 1.07904 0.894742 0.603122 1.55894 1.05084L8.81489 5.94189C9.39068 6.33001 9.40534 7.17242 8.8434 7.58035L1.58746 12.8476C0.926393 13.3275 0 12.8553 0 12.0384Z"
+            fill="#858585"/>
+    </svg>
+
+}
