12c29fee41e14dbb2396d36ce83b580e83d1923d
Sital999 <sitalnagarkoti123@gmail.com>
2024-02-16 09:58:55 +0545

    Add FieldSection to display fields

---
+++ webbuilder/src/views/organism/FieldSection.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import React from 'react';
+
+import useFieldSelectorAtom from "@app/store/jotai/fieldSelector";
+import TextField from '@mui/material/TextField';
+import {FieldTypes, FormField} from "@app/models/dtos/form";
+
+const FieldSection = () => {
+    const {fields} = useFieldSelectorAtom();
+
+    function renderField(field: FormField) {
+        switch (field.type) {
+            case (FieldTypes.EMAIL):
+            case (FieldTypes.NUMBER):
+            case (FieldTypes.SHORT_TEXT):
+                return <InputField value={`Enter Your ${field.type}`} field={field}/>
+
+        }
+    }
+
+    return <div className=" h-min w-full aspect-video bg-white">
+        <div className={'flex flex-col gap-20 px-20 py-10 overflow-y-scroll h-full justify-center'}>
+            {Array.isArray(fields) && fields.length ? fields.map((field, index) => {
+                return <>
+                    {renderField(field)}
+                </>
+            }) : <></>}
+        </div>
+    </div>
+}
+export default FieldSection
+
+const InputField = ({field, value}: { field: FormField, value: string }) => {
+    const {updateTitle, updateFieldPlaceholder} = useFieldSelectorAtom();
+
+    return <div className={'flex flex-col items-start'}>
+        <input id={`input-${field.id}`} type="text" className={'px-0 -left-1 border-0 text-2xl'} value={field.title}
+               onChange={(e: any) => updateTitle(field.id, e.target.value)}/>
+        <TextField sx={{
+            '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                    border: 'none',
+                    borderBottom: '1px solid #407270',
+                },
+                '&:hover fieldset': {
+                    border: 'none',
+                    borderBottom: '1px solid #407270',
+                },
+                '&.Mui-focused fieldset': {
+                    border: 'none',
+                    borderBottom: '1px solid #407270',
+                },
+                '& .MuiInputBase-input': {
+                    color: '#407270',
+                    fontSize: 24,
+                    paddingLeft: 0
+                },
+            },
+        }} type={field.type} value={field.properties?.placeholder}
+                   onChange={(e: any) => updateFieldPlaceholder(field.id, e.target.value)}
+                   className={'w-2/3 border-0 border-b-[1px] border-cyan-500'}/>
+    </div>
+}
