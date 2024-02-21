7bb7f0d82eaf6a2116c98dee8d686e5bd6937491
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2024-02-21 10:15:16 -0500

    Merge conflicting changes and add scaling hooks with custom canvas scaler

--- webbuilder/src/views/molecules/SlideCanvas/index.tsx
+++ webbuilder/src/views/molecules/SlideCanvas/index.tsx
@@ -2,12 +2,18 @@
 
 import React from 'react';
 
+import { useScaling } from '@app/lib/hooks/useScaling';
+
 import './Canvas.css';
 import SlideDraggableWrapper from './SlideDraggableWrapper';
 
 const SlideCanvas = () => {
+    const scale = useScaling();
+
+    console.log('scale', scale);
+
     return (
-        <div id="canvas" className="canvas-grid container my-5">
+        <div id="canvas" className="canvas-grid container my-5" style={{ scale }}>
             <SlideDraggableWrapper>
                 <p className="!m-0 !p-0">Drag first p tag</p>
             </SlideDraggableWrapper>
