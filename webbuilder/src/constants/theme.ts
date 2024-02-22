9ea1b97c3991c0593e07beda6c28b1776b56741a
sital999 <sitalnagarkoti123@gmail.com>
2024-02-22 21:38:31 +0545

    Make const for Themecolor

---
+++ webbuilder/src/constants/theme.ts
@@ -0,0 +1,16 @@
+export const ThemeColor = {
+    accent: '#F2F7FF',
+    tertiary: '#A2C5F8',
+    secondary: '#0764EB',
+    primary: '#2E2E2E'
+};
+
+export const ThemeColors = [
+    { name: "Default", primary: "#2E2E2E", secondary: "#0764EB", tertiary: "#A2C5F8", accent: "#F2F7FF" },
+    { name: "Blue", primary: "#2E2E2E", secondary: "#337FC2", tertiary: "#61A9E9", accent: "#B0DAFF" },
+    { name: "Green", primary: "#2E2E2E", secondary: "#459E73", tertiary: "#81D8AE", accent: "#D7F6E7" },
+    { name: "Red", primary: "#2E2E2E", secondary: "#BE3032", tertiary: "#E75759", accent: "#FFB2B3" },
+    { name: "Black", primary: "#2E2E2E", secondary: "#DBDBDB", tertiary: "#DBDBDB", accent: "#FFFFFF" },
+    { name: "Orange", primary: "#2E2E2E", secondary: "#DA8C0B", tertiary: "#F1B85A", accent: "#FFEFCC" },
+    { name: "Purple", primary: "#2E2E2E", secondary: "#533CAF", tertiary: "#846BEC", accent: "#CFC3FF" },
+]
