a27cb95245d3efd460bbc6ac2747e83c52761494
Ankit Sapkota <ankit@office.sireto.io>
2024-02-26 20:29:07 +0545

    Add Dialog Modal to app

---
+++ webbuilder/src/lib/hooks/useDialogModal.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import React from 'react';
+
+import { atom, useAtom } from 'jotai';
+
+import {
+    Dialog,
+    DialogContent
+} from '@app/shadcn/components/ui/dialog';
+
+export type DIALOG_MODALS = 'ADD_FORM_TITLE' | '';
+
+export interface ModalState {
+    isOpen: boolean;
+    view?: DIALOG_MODALS | '';
+    props?: any;
+}
+
+const dialogModalAtom = atom<ModalState>({ isOpen: false });
+
+export const useDialogModal = () => {
+    const [modalState, setModalState] = useAtom(dialogModalAtom);
+
+    const closeDialogModal = () => {
+        setModalState({ isOpen: false, view: undefined, props: undefined });
+    };
+
+    const openDialogModal = (view: DIALOG_MODALS, props?: any) => {
+        setModalState({ isOpen: true, view: view, props: props });
+    };
+
+    return {
+        isOpen: modalState.isOpen,
+        view: modalState.view,
+        props: modalState.props,
+        openDialogModal,
+        closeDialogModal
+    };
+};
+
+const getModalToRender = (view?: DIALOG_MODALS, props?: any) => {
+    switch (view) {
+        case 'ADD_FORM_TITLE':
+            return <>Add Content to Add Form Title</>;
+        default:
+            return <></>;
+    }
+};
+
+export function DialogModalContainer() {
+    const { isOpen, view, closeDialogModal, props } = useDialogModal();
+
+    return (
+        <Dialog
+            open={isOpen}
+            onOpenChange={() => {
+                closeDialogModal();
+            }}
+        >
+            <DialogContent className="!bg-white">
+                <div className="">{getModalToRender(view, props)}</div>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export function DialogModalTrigger({
+    view,
+    children,
+    ...props
+}: {
+    view: DIALOG_MODALS;
+    children: React.ReactNode;
+    [key: string]: any;
+}) {
+    const { openDialogModal } = useDialogModal();
+    return (
+        <div
+            onClick={(event) => {
+                event.preventDefault();
+                openDialogModal(view, props);
+            }}
+            className="h-full w-full"
+        >
+            {children}
+        </div>
+    );
+}
