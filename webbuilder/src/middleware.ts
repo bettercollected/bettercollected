a229de56186d8686f065d4c0dd718d1c210fb76a
Ankit Sapkota <ankit.sapkota555@gmail.com>
2024-03-01 13:01:47 +0545

    Add env variable to check auth

--- webbuilder/src/middleware.ts
+++ webbuilder/src/middleware.ts
@@ -1,29 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import environments from './configs/environments';
+
 export async function middleware(request: NextRequest) {
-    const Authorization = request.cookies.get('Authorization')?.value;
-    const RefreshToken = request.cookies.get('RefreshToken')?.value;
+    if (environments.AUTH_ENABLED) {
+        const Authorization = request.cookies.get('Authorization')?.value;
+        const RefreshToken = request.cookies.get('RefreshToken')?.value;
 
-    const auth = !!Authorization ? `Authorization=${Authorization}` : '';
-    const refresh = !!RefreshToken ? `RefreshToken=${RefreshToken}` : '';
+        const auth = !!Authorization ? `Authorization=${Authorization}` : '';
+        const refresh = !!RefreshToken ? `RefreshToken=${RefreshToken}` : '';
 
-    const config = {
-        method: 'GET',
-        headers: {
-            cookie: `${auth};${refresh}`
-        }
-    };
-    try {
-        const statusResponse = await fetch(
-            process.env.API_ENDPOINT_HOST + '/auth/status',
-            config
-        );
-        const user = await statusResponse.json();
-        if (statusResponse.status !== 200) {
+        const config = {
+            method: 'GET',
+            headers: {
+                cookie: `${auth};${refresh}`
+            }
+        };
+        try {
+            const statusResponse = await fetch(
+                process.env.API_ENDPOINT_HOST + '/auth/status',
+                config
+            );
+            const user = await statusResponse.json();
+            if (statusResponse.status !== 200) {
+                return NextResponse.redirect(new URL('/login', request.url));
+            }
+        } catch (e) {
             return NextResponse.redirect(new URL('/login', request.url));
         }
-    } catch (e) {
-        return NextResponse.redirect(new URL('/login', request.url));
     }
 
     const response = NextResponse.next();
