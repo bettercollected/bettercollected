68b00ef978870721d8728017580461b95b32d9a7
Ankit Sapkota <ankit.sapkota555@gmail.com>
2024-03-13 00:51:20 +0545

    Fix Tiptap bubble menu to to be active when editor is focused

--- webbuilder/src/views/molecules/RichTextEditor/index.tsx
+++ webbuilder/src/views/molecules/RichTextEditor/index.tsx
@@ -1,20 +1,13 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
 import Color from '@tiptap/extension-color';
 import TextStyle from '@tiptap/extension-text-style';
 import Underline from '@tiptap/extension-underline';
-import { Transaction } from '@tiptap/pm/state';
-import {
-    BubbleMenu,
-    Editor,
-    EditorContent,
-    generateHTML,
-    useEditor
-} from '@tiptap/react';
+import { EditorProvider, useCurrentEditor, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 
 import { FieldTypes, FormField } from '@app/models/dtos/form';
-import useFormFieldsAtom from '@app/store/jotai/fieldSelector';
+import { cn } from '@app/shadcn/util/lib';
 import { FontSize } from '@app/utils/richTextEditorExtenstion/fontSize';
 import { getHtmlFromJson } from '@app/utils/richTextEditorExtenstion/getHtmlFromJson';
 import { ArrowDown } from '@app/views/atoms/Icons/ArrowDown';
@@ -55,26 +48,37 @@ export function RichTextEditor({
     field: FormField;
     onUpdate: (editor: any) => void;
 }) {
-    const FontSizes = [8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 40, 48, 64];
-    const [size, setSize] = useState(5);
-
     const getContentForEditor = () => {
         return field.title
             ? getHtmlFromJson(field.title ?? '')
             : getPlaceholderValueForTitle(field.type || FieldTypes.SHORT_TEXT);
     };
-    const editor = useEditor({
-        extensions: Extenstions,
-        content: getContentForEditor(),
-        editorProps: {
-            attributes: {
-                class: 'w-[400px] font-semibold text-3xl focus:outline-none'
-            }
-        },
-        onUpdate: ({ editor }) => {
-            onUpdate(editor);
-        }
-    });
+
+    return (
+        <div className="tiptap group relative">
+            <EditorProvider
+                content={getContentForEditor()}
+                extensions={Extenstions}
+                slotBefore={<TiptapMenuBar />}
+                editorProps={{ attributes: { class: 'outline-none' } }}
+            >
+                {''}
+            </EditorProvider>
+        </div>
+    );
+}
+
+const TiptapMenuBar = ({ focused }: any) => {
+    const editorRef = useCurrentEditor();
+    const FontSizes = [8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 40, 48, 64];
+    const [size, setSize] = useState(5);
+
+    const editor = editorRef.editor;
+
+    if (!editor) {
+        return null;
+    }
+
     const handleUpdateFontSize = (value = 0) => {
         setSize((prevSize) => {
             const newSize = prevSize + value;
@@ -84,27 +88,32 @@ export function RichTextEditor({
     };
 
     return (
-        <div className="tiptap">
-            <EditorContent editor={editor} />
-            {editor && (
-                <BubbleMenu
-                    className={`relative flex flex-row gap-4 rounded-lg bg-white px-4 py-2 shadow-tooltip`}
-                    editor={editor}
-                >
-                    <button onClick={() => editor?.chain().focus().toggleBold().run()}>
-                        <strong className="text-2xl">B</strong>
-                    </button>
-                    <button
-                        onClick={() => editor?.chain().focus().toggleItalic().run()}
-                    >
-                        <em className="text-2xl">I</em>
-                    </button>
-                    <button
-                        onClick={() => editor?.chain().focus().toggleUnderline().run()}
-                    >
-                        <u className="text-2xl">U</u>
-                    </button>
-                    {/* <button
+        <button
+            className={cn(
+                `absolute -top-16 mb-2 flex hidden items-center rounded-lg bg-white px-4 py-2 shadow-tooltip`,
+                'group-focus-within:flex'
+            )}
+            tabIndex={0}
+            onClick={() => {
+                return true;
+            }}
+        >
+            <button
+                onClick={() => {
+                    console.log('BOlded');
+
+                    editor?.chain().focus().toggleBold().run();
+                }}
+            >
+                <strong className="px-2 text-2xl">B</strong>
+            </button>
+            <button onClick={() => editor?.chain().focus().toggleItalic().run()}>
+                <em className="px-2 text-2xl">I</em>
+            </button>
+            <button onClick={() => editor?.chain().focus().toggleUnderline().run()}>
+                <u className="px-2 text-2xl">U</u>
+            </button>
+            {/* <button
                         onClick={() => {
                             setShowColorPicker(true);
                             editor?.chain().focus().setColor(color).run();
@@ -122,35 +131,39 @@ export function RichTextEditor({
                             className="z-100 absolute"
                         />
                     )} */}
+            <button
+                onClick={() => editor?.chain().focus().undo().run()}
+                disabled={!editor.can().undo()}
+                className="!px-2 disabled:cursor-not-allowed disabled:text-black-900/60"
+            >
+                Undo
+            </button>
+            <button
+                onClick={() => editor?.chain().focus().redo().run()}
+                disabled={!editor.can().redo()}
+                className="!px-2 disabled:cursor-not-allowed disabled:text-black-900/60"
+            >
+                Redo
+            </button>
+            <div className="flex flex-row items-center justify-center gap-2">
+                <span>{FontSizes[size]}</span>
+                <div className="flex flex-col">
                     <button
-                        onClick={() => editor?.chain().focus().undo().run()}
-                        disabled={!editor.can().undo()}
-                        className="disabled:cursor-not-allowed disabled:text-black-900/60"
+                        onClick={() => {
+                            handleUpdateFontSize(+1);
+                        }}
                     >
-                        Undo
+                        <ArrowDown className="h-4 w-4 rotate-180" />
                     </button>
                     <button
-                        onClick={() => editor?.chain().focus().redo().run()}
-                        disabled={!editor.can().redo()}
-                        className="disabled:cursor-not-allowed disabled:text-black-900/60"
+                        onClick={() => {
+                            handleUpdateFontSize(-1);
+                        }}
                     >
-                        Redo
+                        <ArrowDown className="h-4 w-4" />
                     </button>
-                    <div className="flex flex-row items-center justify-center gap-2">
-                        <span>{FontSizes[size]}</span>
-                        <div className="flex flex-col">
-                            <ArrowDown
-                                className="h-4 w-4 rotate-180"
-                                onClick={() => handleUpdateFontSize(+1)}
-                            />
-                            <ArrowDown
-                                className="h-4 w-4"
-                                onClick={() => handleUpdateFontSize(-1)}
-                            />
-                        </div>
-                    </div>
-                </BubbleMenu>
-            )}
-        </div>
+                </div>
+            </div>
+        </button>
     );
-}
+};
