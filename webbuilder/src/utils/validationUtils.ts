57ae1e51ddac0f8ac07a55caab6779b5610be658
Ankit Sapkota <ankit.sapkota555@gmail.com>
2024-03-14 14:02:26 +0545

    Validate slide before moving to next one

--- webbuilder/src/utils/validationUtils.ts
+++ webbuilder/src/utils/validationUtils.ts
@@ -3,7 +3,7 @@ import { FormField } from '@app/models/dtos/form';
 export function validateSlide(slide: FormField, answers: Record<string, any>) {
     const invalidFields: Record<string, Array<Invalidations>> = {};
     slide?.properties?.fields?.forEach((field) => {
-        if (!answers[field.id]) {
+        if (field?.validations?.required && !answers[field.id]) {
             invalidFields[field.id] = [Invalidations.REQUIRED];
         }
     });
