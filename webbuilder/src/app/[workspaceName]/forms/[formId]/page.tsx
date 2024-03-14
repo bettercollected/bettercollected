1af7dd7e199325a51df78e43be2f029ea36dd96a
Ankit Sapkota <ankit.sapkota555@gmail.com>
2024-03-14 16:34:37 +0545

    Add submit response api and form preview page

--- webbuilder/src/app/[workspaceName]/forms/[formId]/page.tsx
+++ webbuilder/src/app/[workspaceName]/forms/[formId]/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
 
-import { useStandardForm } from '@app/store/jotai/fetchedForm';
 import { useFormResponse } from '@app/store/jotai/responderFormResponse';
 import FormComponent from '@app/views/organism/Form/FormComponent';
 
