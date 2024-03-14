441378c344d0eb0bc7b25ab2fa952fb2f41dcc19
sital999 <sitalnagarkoti123@gmail.com>
2024-03-14 12:54:04 +0545

    Make multipleChoice Field select multiple choices

--- webbuilder/src/store/jotai/responderFormResponse.ts
+++ webbuilder/src/store/jotai/responderFormResponse.ts
@@ -196,6 +196,7 @@ export const useFormResponse = () => {
         nextSlide,
         previousSlide,
         setFormResponse,
+        addFieldTextAnswer,
         addFieldBooleanAnswer,
         addFieldChoiceAnswer,
         addFieldDateAnswer,
