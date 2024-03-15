43aa3cb9dc7ea2761748c3bf4dbea1acd98bdbb2
Ankit Sapkota <ankit.sapkota555@gmail.com>
2024-03-15 14:43:36 +0545

    Manage choices options in response answers for multiple choice with multiple selections

---
+++ webbuilder/src/views/atoms/ResponderFormFields/Choice.tsx
@@ -0,0 +1,29 @@
+import { MouseEvent } from 'react';
+
+import { Check } from 'lucide-react';
+
+import { FormTheme } from '@app/constants/theme';
+import { FieldChoice } from '@app/models/dtos/form';
+
+interface ChoiceProps {
+    isSelected?: boolean;
+    theme?: FormTheme;
+    choice: FieldChoice;
+    onClick?: (choiceId: string) => any;
+}
+
+export default function Choice({ isSelected, theme, choice, onClick }: ChoiceProps) {
+    return (
+        <div
+            style={{
+                background: isSelected ? theme?.tertiary : '',
+                borderColor: theme?.tertiary
+            }}
+            className="flex cursor-pointer justify-between rounded-xl border p-2 px-4"
+            key={choice.id}
+            onClick={() => onClick && onClick(choice.id || '')}
+        >
+            {choice.value} {isSelected && <Check />}
+        </div>
+    );
+}
