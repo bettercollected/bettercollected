c0643dbd4b86d413c48c1fd4ba7b454b6a260d0e
Ankit Sapkota <ankit.sapkota555@gmail.com>
2024-03-19 23:20:00 +0545

    Add animation for changing fields

--- webbuilder/src/shadcn/components/ui/scroll-area.tsx
+++ webbuilder/src/shadcn/components/ui/scroll-area.tsx
@@ -9,13 +9,16 @@ import { cn } from '@app/shadcn/util/lib';
 const ScrollArea = React.forwardRef<
     React.ElementRef<typeof ScrollAreaPrimitive.Root>,
     React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
+>(({ className, children, asChild, ...props }, ref) => (
     <ScrollAreaPrimitive.Root
         ref={ref}
         className={cn('relative overflow-hidden', className)}
         {...props}
     >
-        <ScrollAreaPrimitive.Viewport asChild className="h-full w-full rounded-[inherit]">
+        <ScrollAreaPrimitive.Viewport
+            asChild={asChild}
+            className="h-full w-full rounded-[inherit]"
+        >
             {children}
         </ScrollAreaPrimitive.Viewport>
         <ScrollBar />
