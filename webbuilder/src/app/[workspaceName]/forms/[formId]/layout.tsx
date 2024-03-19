f84f1024e3049b4cb8a9cf5b1f7c07d92d1be939
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2024-03-19 16:12:35 -0400

    Update default layout to single with no background if no layout exists

--- webbuilder/src/app/[workspaceName]/forms/[formId]/layout.tsx
+++ webbuilder/src/app/[workspaceName]/forms/[formId]/layout.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 
 import environments from '@app/configs/environments';
+import { FormSlideLayout } from '@app/models/enums/form';
 import { store } from '@app/store/store';
 import fetchWithCookies from '@app/utils/fetchUtils';
 import FullScreenLoader from '@app/views/atoms/Loaders/FullScreenLoader';
@@ -47,10 +48,28 @@ async function FormWrapper({
         config
     );
 
+    // TODO: [Save in db] Set default layout if it's not saved in database (for now)
+    const updatedFormFields: any = [];
+    if (form && Array.isArray(form.fields)) {
+        form.fields.forEach((field: any) => {
+            if (field.type === 'slide') {
+                const properties = field.properties;
+                if (!properties?.layout) {
+                    properties.layout = FormSlideLayout.SINGLE_COLUMN_NO_BACKGROUND;
+                }
+
+                field.properties = properties;
+                updatedFormFields.push(field);
+            }
+        });
+    }
+
+    const newForm = { ...form, fields: updatedFormFields };
+
     return (
         <>
             <Suspense fallback={<FullScreenLoader />}>
-                <FormDispatcher form={form}>{children}</FormDispatcher>
+                <FormDispatcher form={newForm}>{children}</FormDispatcher>
             </Suspense>
         </>
     );
