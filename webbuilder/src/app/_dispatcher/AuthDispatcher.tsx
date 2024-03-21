035e05dffbfbed35f1598a588a4492ed556d4987
sital999 <sitalnagarkoti123@gmail.com>
2024-03-21 13:24:45 +0545

    Add AuthDispatcher and Authprovider

---
+++ webbuilder/src/app/_dispatcher/AuthDispatcher.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { initialAuthState, useAuthAtom } from '@app/store/jotai/auth';
+
+const AuthDispatcher = ({
+    auth,
+    children
+}: {
+    auth: any;
+    children: React.ReactNode;
+}) => {
+    const { authState, setAuthState } = useAuthAtom();
+    useEffect(() => {
+        if (auth instanceof Object) {
+            setAuthState(auth);
+        } else {
+            setAuthState(initialAuthState);
+        }
+    }, [auth]);
+    return <>{children}</>;
+};
+
+export default AuthDispatcher;
