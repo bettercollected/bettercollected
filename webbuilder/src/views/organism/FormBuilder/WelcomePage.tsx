78e336e13aea4046f6fba4eacf6baad6a33614a4
sital999 <sitalnagarkoti123@gmail.com>
2024-03-25 12:45:40 +0545

    Add GreetingLayout in greetingPages and update className for different layouts

--- webbuilder/src/views/organism/FormBuilder/WelcomePage.tsx
+++ webbuilder/src/views/organism/FormBuilder/WelcomePage.tsx
@@ -2,93 +2,65 @@ import cn from 'classnames';
 
 import { FormSlideLayout } from '@app/models/enums/form';
 import { Button } from '@app/shadcn/components/ui/button';
-import useFormFieldsAtom from '@app/store/jotai/fieldSelector';
 import { useFormState } from '@app/store/jotai/form';
+
+import GreetingLayoutWrapper from '../Layout/GreetingLayoutWrapper';
 
 const WelcomeSlide = ({ disabled }: { disabled?: boolean }) => {
     const { theme, formState, setFormDescription, setWelcomeTitle } = useFormState();
 
-    const { activeSlide } = useFormFieldsAtom();
-
     return (
-        <div
-            style={{
-                background: theme?.accent
-            }}
-            // TODO: Change this to apply layout from other layout
-            className={`grid aspect-video h-min w-full grid-cols-1 bg-blue-100 h-[${93.28 * 4}px] w-[${165.83 * 4}px] ${disabled ? 'pointer-events-none overflow-hidden' : ''}`}
-        >
+        <GreetingLayoutWrapper disabled={disabled} greetingIndex={-10}>
             <div
+                style={{
+                    background: theme?.accent
+                }}
+                // TODO: Change this to apply layout from other layout
                 className={cn(
-                    'grid-flow-col grid-cols-1 items-center justify-center gap-12 self-center px-12',
-                    activeSlide &&
-                        activeSlide.properties?.layout ===
+                    'h-[${93.28 * 4}px] w-[${165.83 * 4}px] grid aspect-video h-min w-full grid-cols-1 bg-blue-100',
+                    disabled ? 'pointer-events-none overflow-hidden' : '',
+                    formState.welcomePage?.layout &&
+                        formState.welcomePage?.layout ===
                             FormSlideLayout.TWO_COLUMN_IMAGE_LEFT
                         ? 'order-1'
-                        : activeSlide &&
-                            activeSlide.properties?.layout ===
+                        : formState.welcomePage?.layout &&
+                            formState.welcomePage?.layout ===
                                 FormSlideLayout.TWO_COLUMN_IMAGE_RIGHT
                           ? 'order-0'
                           : ''
                 )}
             >
-                <div className="flex flex-col">
-                    <input
-                        type="text"
-                        placeholder="Form Title"
-                        className="border-0 px-0 text-[40px] font-bold"
-                        value={formState.welcomeTitle}
-                        onChange={(event) => {
-                            setWelcomeTitle(event.target.value);
-                        }}
-                    />
-                    {formState.description !== undefined ? (
+                <div className={cn('flex flex-col items-center justify-center px-12')}>
+                    <div className="flex flex-col items-start">
                         <input
                             type="text"
-                            placeholder="Add description"
-                            value={formState.description}
-                            className="border-0 px-0 text-base"
-                            onChange={(e: any) => setFormDescription(e.target.value)}
+                            placeholder="Form Title"
+                            className="border-0 px-0 text-[40px] font-bold"
+                            value={formState.welcomeTitle}
+                            onChange={(event) => {
+                                setWelcomeTitle(event.target.value);
+                            }}
                         />
-                    ) : (
-                        <></>
-                    )}
+                        {formState.description !== undefined ? (
+                            <input
+                                type="text"
+                                placeholder="Add description"
+                                value={formState.description}
+                                className="border-0 px-0 text-base"
+                                onChange={(e: any) =>
+                                    setFormDescription(e.target.value)
+                                }
+                            />
+                        ) : (
+                            <></>
+                        )}
+                        <Button size={'medium'} className="mt-12">
+                            {formState.buttonText || 'Start'}
+                        </Button>
+                    </div>
                 </div>
-                <Button size={'medium'}>{formState.buttonText || 'Start'}</Button>
             </div>
-            {/* <div
-                className={cn(
-                    'grid-cols-1',
-                    activeSlide &&
-                        activeSlide.properties?.layout ===
-                            FormSlideLayout.TWO_COLUMN_IMAGE_LEFT
-                        ? 'order-0'
-                        : activeSlide &&
-                            activeSlide.properties?.layout ===
-                                FormSlideLayout.TWO_COLUMN_IMAGE_RIGHT
-                          ? 'order-1'
-                          : ''
-                )}
-            >
-                <Image
-                    objectFit="cover"
-                    className={cn(
-                        'h-full w-full',
-                        activeSlide &&
-                            activeSlide.properties?.layout ===
-                                FormSlideLayout.TWO_COLUMN_IMAGE_LEFT
-                            ? 'order-0'
-                            : activeSlide &&
-                                activeSlide.properties?.layout ===
-                                    FormSlideLayout.TWO_COLUMN_IMAGE_RIGHT
-                              ? 'order-1'
-                              : ''
-                    )}
-                    src={RectangleImage}
-                    alt="LayoutImage"
-                />
-            </div> */}
-        </div>
+        </GreetingLayoutWrapper>
     );
 };
 
