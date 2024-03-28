dd75ae5ecbc95e5de03e0b4506d4471ffd06125d
sital999 <sitalnagarkoti123@gmail.com>
2024-03-28 16:10:30 +0545

    Add navbar for preview section

--- webbuilder/src/shadcn/components/ui/sheet.tsx
+++ webbuilder/src/shadcn/components/ui/sheet.tsx
@@ -51,31 +51,46 @@ const sheetVariants = cva(
 interface SheetContentProps
     extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
         VariantProps<typeof sheetVariants> {
-    onClickCloseIcon: () => void;
+    onClickCloseIcon?: () => void;
+    hideCloseIcon?: boolean;
 }
 
 const SheetContent = React.forwardRef<
     React.ElementRef<typeof SheetPrimitive.Content>,
     SheetContentProps
->(({ side = 'right', onClickCloseIcon, className, children, ...props }, ref) => (
-    <SheetPortal>
-        <SheetOverlay />
-        <SheetPrimitive.Content
-            ref={ref}
-            className={cn(sheetVariants({ side }), className)}
-            {...props}
-        >
-            {children}
-            <SheetPrimitive.Close
-                onClick={onClickCloseIcon}
-                className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm border-[1px] border-white p-4 opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none"
+>(
+    (
+        {
+            side = 'right',
+            onClickCloseIcon,
+            hideCloseIcon,
+            className,
+            children,
+            ...props
+        },
+        ref
+    ) => (
+        <SheetPortal>
+            <SheetOverlay />
+            <SheetPrimitive.Content
+                ref={ref}
+                className={cn(sheetVariants({ side }), className)}
+                {...props}
             >
-                <X className="h-4 w-4" />
-                <span className="sr-only">Close</span>
-            </SheetPrimitive.Close>
-        </SheetPrimitive.Content>
-    </SheetPortal>
-));
+                {children}
+                {!hideCloseIcon && (
+                    <SheetPrimitive.Close
+                        onClick={onClickCloseIcon && onClickCloseIcon}
+                        className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm border-[1px] border-white p-4 opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none"
+                    >
+                        <X className="h-4 w-4" />
+                        <span className="sr-only">Close</span>
+                    </SheetPrimitive.Close>
+                )}
+            </SheetPrimitive.Content>
+        </SheetPortal>
+    )
+);
 SheetContent.displayName = SheetPrimitive.Content.displayName;
 
 const SheetHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
--- webbuilder/src/views/molecules/FormBuilder/BackButton.tsx
+++ webbuilder/src/views/molecules/FormBuilder/BackButton.tsx
@@ -1,17 +1,26 @@
-
+import { cn } from '@app/shadcn/util/lib';
 import Back from '@app/views/atoms/Icons/Back';
 
-export default function BackButton({ handleClick }: { handleClick: () => void }) {
+export default function BackButton({
+    handleClick,
+    className
+}: {
+    handleClick?: () => void;
+    className?: string;
+}) {
     const handleBackClick = async () => {
-        await handleClick();
+        handleClick && (await handleClick());
     };
     return (
         <div
-            className="paragraph flex cursor-pointer items-center gap-1 py-4 text-black-900 hover:text-brand-500 hover:underline"
+            className={cn(
+                'flex cursor-pointer items-center gap-1 py-4 text-black-900 hover:text-brand-500 hover:underline',
+                className
+            )}
             onClick={handleBackClick}
         >
             <Back />
-            <span>Back</span>
+            <span className="text-sm font-normal">Back</span>
         </div>
     );
 }
