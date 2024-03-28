54f0efc488962c8e6cd9235234bd29ef9752761b
sital999 <sitalnagarkoti123@gmail.com>
2024-03-28 18:09:58 +0545

    Add PreviewWrapper for preview modal

---
+++ webbuilder/src/views/molecules/FormBuilder/PreviewWrapper.tsx
@@ -0,0 +1,74 @@
+import environments from "@app/configs/environments";
+import { Separator } from "@app/shadcn/components/ui/separator";
+import { cn } from "@app/shadcn/util/lib";
+import { useStandardForm } from "@app/store/jotai/fetchedForm";
+import useWorkspace from "@app/store/jotai/workspace";
+import { DesktopIcon } from "@app/views/atoms/Icons/DesktopIcon";
+import { MobileIcon } from "@app/views/atoms/Icons/MobileIcon";
+import { useState } from "react";
+
+const PreviewWrapper = ({
+    children,
+    handleResetResponderState
+}: {
+    children: React.ReactNode;
+    handleResetResponderState: () => void;
+}) => {
+    const [isDesktopView, setIsDesktopView] = useState(true);
+    const { standardForm } = useStandardForm();
+    const { workspace } = useWorkspace();
+    const mobileViewPreviewUrl = `${environments.NEXT_PUBLIC_HTTP_SCHEME}://${environments.NEXT_PUBLIC_V2_CLIENT_ENDPOINT_DOMAIN}/${workspace.workspaceName}/forms/${standardForm.formId}?isPreview=true`;
+    return (
+        <div className=" h-full w-full bg-white">
+            <nav className="flex h-14 flex-row justify-between px-4 py-2">
+                <div></div>
+                <div className="flex items-center gap-4">
+                    <div
+                        onClick={() => setIsDesktopView(true)}
+                        className={cn(
+                            'flex cursor-pointer items-center gap-1',
+                            isDesktopView
+                                ? 'rounded-lg bg-black-200 p-2 text-black-800'
+                                : 'text-black-500'
+                        )}
+                    >
+                        <DesktopIcon />
+                        Desktop
+                    </div>
+                    <div
+                        onClick={() => setIsDesktopView(false)}
+                        className={cn(
+                            'flex cursor-pointer items-center gap-1',
+                            isDesktopView
+                                ? 'text-black-500'
+                                : 'rounded-lg bg-black-200 p-2 text-black-800'
+                        )}
+                    >
+                        <MobileIcon />
+                        Mobile
+                    </div>
+                </div>
+                <div
+                    onClick={handleResetResponderState}
+                    className="flex cursor-pointer items-center rounded-lg border border-black-300 p-1 px-4"
+                >
+                    Restart
+                </div>
+            </nav>
+            <Separator />
+            <div className="flex h-full w-full items-center justify-center px-32 py-10 drop-shadow-xl">
+                {isDesktopView ? (
+                    <>{children}</>
+                ) : (
+                    <iframe
+                        title="responder-mobile-view"
+                        className="h-full  w-[400px] pb-20 drop-shadow-xl"
+                        src={mobileViewPreviewUrl}
+                    />
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default PreviewWrapper
