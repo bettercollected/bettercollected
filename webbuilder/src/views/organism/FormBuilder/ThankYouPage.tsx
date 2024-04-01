dbdc498d67c79b7c7fded770b8e2d1e316b5e150
sital999 <sitalnagarkoti123@gmail.com>
2024-04-01 14:59:56 +0545

    Add new layout and update its functionality

--- webbuilder/src/views/organism/FormBuilder/ThankYouPage.tsx
+++ webbuilder/src/views/organism/FormBuilder/ThankYouPage.tsx
@@ -1,3 +1,4 @@
+import { FormSlideLayout } from '@app/models/enums/form';
 import { Button } from '@app/shadcn/components/ui/button';
 import { cn } from '@app/shadcn/util/lib';
 import { useActiveThankYouPageComponent } from '@app/store/jotai/activeBuilderComponent';
@@ -8,10 +9,23 @@ import GreetingLayoutWrapper from '../Layout/GreetingLayoutWrapper';
 const ThankYouSlide = ({ disabled }: { disabled?: boolean }) => {
     const { formState, theme, setThankYouPageDescription } = useFormState();
     const { activeThankYouPageComponent } = useActiveThankYouPageComponent();
-
     return (
         <GreetingLayoutWrapper disabled={disabled} greetingIndex={-20} theme={theme}>
-            <div className={cn('flex w-full max-w-[800px] items-start self-center')}>
+            <div
+                className={cn(
+                    'flex w-full max-w-[800px] items-center',
+                    formState.thankyouPage![activeThankYouPageComponent?.index || 0]
+                        ?.layout &&
+                        (formState.thankyouPage![
+                            activeThankYouPageComponent?.index || 0
+                        ]?.layout === FormSlideLayout.SINGLE_COLUMN_IMAGE_BACKGROUND ||
+                            formState.thankyouPage![
+                                activeThankYouPageComponent?.index || 0
+                            ]?.layout === FormSlideLayout.SINGLE_COLUMN_NO_BACKGROUND)
+                        ? 'justify-center'
+                        : 'justify-start '
+                )}
+            >
                 <div className="flex flex-col items-start">
                     <h1 className="text-2xl font-semibold">Thank You!</h1>
                     {formState.thankyouPage &&
