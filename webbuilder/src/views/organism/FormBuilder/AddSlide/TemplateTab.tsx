f009aca00177ec23141bc9277d64876c449eea5b
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-04-05 15:32:18 +0545

    Add scroll bar for selecting templates  and pages

--- webbuilder/src/views/organism/FormBuilder/AddSlide/TemplateTab.tsx
+++ webbuilder/src/views/organism/FormBuilder/AddSlide/TemplateTab.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import { ChevronLeft } from 'lucide-react';
 
+import { ScrollArea } from '@app/shadcn/components/ui/scroll-area';
 import useFormFieldsAtom from '@app/store/jotai/fieldSelector';
 import { useFormState } from '@app/store/jotai/form';
 import { useGetTemplatesQuery } from '@app/store/redux/templateApi';
@@ -36,65 +37,78 @@ export default function TemplateTab({ closePopover }: { closePopover: () => void
                             {selectedTemplate?.title || 'Untitled'}
                         </span>
                     </div>
-                    <div className="flex w-full flex-row flex-wrap gap-2 ">
-                        {selectedTemplate?.fields?.map((slide) => (
-                            <div
-                                onClick={() => {
-                                    addSlideFormTemplate(slide);
-                                    closePopover();
-                                }}
-                                className="flex cursor-pointer flex-col  rounded-lg border border-transparent p-1 hover:border-pink-500"
-                                key={slide?.id}
-                            >
-                                <div className="relative aspect-video w-[160px] overflow-hidden rounded-md border border-black-300">
-                                    <div
-                                        className="pointer-events-none h-[720px] w-[1280px] scale-[0.125]"
-                                        style={{ transformOrigin: 'top left' }}
-                                    >
-                                        <FormSlidePreview slide={slide} theme={theme} />
+                    <ScrollArea className="h-[495px] overflow-auto">
+                        <div className="flex !h-full w-full flex-row flex-wrap gap-2 overflow-auto">
+                            {selectedTemplate?.fields?.map((slide) => (
+                                <div
+                                    onClick={() => {
+                                        addSlideFormTemplate(slide);
+                                        closePopover();
+                                    }}
+                                    className="mb-2 flex w-min cursor-pointer flex-col  rounded-lg border border-transparent p-1 hover:border-pink-500"
+                                    key={slide?.id}
+                                >
+                                    <div className="relative aspect-video w-[160px] overflow-hidden rounded-md border border-black-300">
+                                        <div
+                                            className="pointer-events-none h-[720px] w-[1280px] scale-[0.125]"
+                                            style={{ transformOrigin: 'top left' }}
+                                        >
+                                            <FormSlidePreview
+                                                slide={slide}
+                                                theme={theme}
+                                            />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    </ScrollArea>
                 </>
             )}
             {!selectedTemplate && (
                 <>
-                    <div className="mb-2 text-xs font-medium ">Templates</div>
-                    <div className="flex w-full flex-row flex-wrap ">
-                        {templates?.map((template) => (
-                            <div
-                                className="flex cursor-pointer flex-col rounded-lg border border-transparent p-1 hover:border-pink-500"
-                                key={template?.id}
-                                onClick={() => {
-                                    setSelectedTemplate(template);
-                                }}
-                            >
-                                <div className="relative h-[94px] w-[168px] overflow-hidden rounded-md border border-black-300">
-                                    <div
-                                        className="pointer-events-none h-[810px] w-[1440px] scale-[0.116666667]"
-                                        style={{ transformOrigin: 'top left' }}
-                                    >
-                                        <LayoutWrapper
-                                            theme={theme}
-                                            disabled
-                                            layout={template.welcomePage?.layout}
-                                            imageUrl={template?.welcomePage?.imageUrl}
+                    <div className="mb-2 flex h-6 items-center text-xs font-medium">
+                        Templates
+                    </div>
+                    <ScrollArea className="h-[495px] overflow-auto">
+                        <div className="flex w-full flex-row flex-wrap ">
+                            {templates?.map((template) => (
+                                <div
+                                    className="mb-2 flex cursor-pointer flex-col rounded-lg border border-transparent p-1 hover:border-pink-500"
+                                    key={template?.id}
+                                    onClick={() => {
+                                        setSelectedTemplate(template);
+                                    }}
+                                >
+                                    <div className="relative aspect-video w-[160px] overflow-hidden rounded-md border border-black-300">
+                                        <div
+                                            className="pointer-events-none h-[810px] w-[1440px] scale-[0.116666667]"
+                                            style={{ transformOrigin: 'top left' }}
                                         >
-                                            <WelcomePage
-                                                isPreviewMode
-                                                welcomePageData={template?.welcomePage}
-                                            />
-                                        </LayoutWrapper>
+                                            <LayoutWrapper
+                                                theme={theme}
+                                                disabled
+                                                layout={template.welcomePage?.layout}
+                                                imageUrl={
+                                                    template?.welcomePage?.imageUrl
+                                                }
+                                            >
+                                                <WelcomePage
+                                                    isPreviewMode
+                                                    welcomePageData={
+                                                        template?.welcomePage
+                                                    }
+                                                />
+                                            </LayoutWrapper>
+                                        </div>
+                                    </div>
+                                    <div className="p2-new mt-2 !font-medium">
+                                        {template.title}
                                     </div>
                                 </div>
-                                <div className="p2-new mt-2 !font-medium">
-                                    {template.title}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    </ScrollArea>
                 </>
             )}
         </>
