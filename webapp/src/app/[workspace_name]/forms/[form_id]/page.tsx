7754812dfa3c347e0922e469a1d4517cb2112f53
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-04-10 13:48:02 +0545

    Add wrapper for rendering v1 form in app router

--- webapp/src/app/[workspace_name]/forms/[form_id]/page.tsx
+++ webapp/src/app/[workspace_name]/forms/[form_id]/page.tsx
@@ -3,14 +3,21 @@
 import { useSearchParams } from 'next/navigation';
 
 import Form from '@app/views/organism/Form/Form';
+import ReduxWrapperAppRouter from '@app/containers/ReduxWrapperAppRouter';
+import { useStandardForm } from '@app/store/jotai/fetchedForm';
+import SingleFormPage from '@app/pages/forms/[id]';
+import useWorkspace from '@app/store/jotai/workspace';
 
-export default function FormPage({
-    params
-}: {
-    params: { formId: string; workspaceName: string };
-}) {
+export default function FormPage({ params }: { params: { formId: string; workspaceName: string } }) {
     const searchParams = useSearchParams();
     const isPreviewMode = searchParams?.get('isPreview');
+    const { standardForm } = useStandardForm();
+    const { workspace } = useWorkspace();
 
-    return <Form isMobileView={!!isPreviewMode} />;
+    return (
+        <ReduxWrapperAppRouter>
+            {standardForm?.builderVersion === 'v2' && <Form isMobileView={!!isPreviewMode} />}
+            {standardForm?.builderVersion !== 'v2' && <SingleFormPage form={standardForm} workspace={workspace} />}
+        </ReduxWrapperAppRouter>
+    );
 }
