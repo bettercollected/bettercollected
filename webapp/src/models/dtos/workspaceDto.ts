b415530dae5e49c445504f6fca9e1ffea03da9af
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-04-11 16:03:04 +0545

    Add Workspace wrapper for custom domain

--- webapp/src/models/dtos/workspaceDto.ts
+++ webapp/src/models/dtos/workspaceDto.ts
@@ -36,8 +36,6 @@ export const initWorkspaceDto: WorkspaceDto = {
     },
     privacyPolicy: '',
     termsOfService: '',
-    privacy_policy_url: 'https://bettercollected.com/legal/privacy-policy-2022.pdf',
-    terms_of_service_url: 'https://bettercollected.com/legal/terms-and-conditions-2022.pdf',
     mailSettings: '',
     id: '63ca5518b613f81e118e3d8d',
     isPro: false
