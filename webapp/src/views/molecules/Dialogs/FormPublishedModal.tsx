559957d8dfe665fe41c00b1a991ff34a2f6b3f7e
sital999 <sitalnagarkoti123@gmail.com>
2024-04-12 12:28:17 +0545

    Update PublishModal acc to design

--- webapp/src/views/molecules/Dialogs/FormPublishedModal.tsx
+++ webapp/src/views/molecules/Dialogs/FormPublishedModal.tsx
@@ -10,65 +10,49 @@ export default function FormPublishedModal(props: any) {
     const { workspace } = useWorkspace();
     const { standardForm } = useStandardForm();
     return (
-        <div>
-            <div className="border-b border-b-black-300 p-4">Form Published</div>
+        <div className="w-full">
+            <div className="border-b-black-300 text-black-700 border-b p-4 text-xs">Form Published</div>
             <div className="flex w-full flex-col items-center gap-6 px-5 py-6">
-                <svg
-                    width="76"
-                    height="76"
-                    viewBox="0 0 76 76"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
+                <svg width="76" height="76" viewBox="0 0 76 76" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="38" cy="38" r="38" fill="#D7FFF6" />
                     <rect x="14" y="14" width="48" height="48" rx="24" fill="#2DBB7F" />
-                    <path
-                        d="M31 39.8421L35.0137 43L45 33"
-                        stroke="white"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                    />
+                    <path d="M31 39.8421L35.0137 43L45 33" stroke="white" stroke-width="2" stroke-linecap="round" />
                 </svg>
-                <div className="flex gap-2">
-                    🎉
-                    <span className="h3-new font-bold">
-                        Your form has been published
-                    </span>
+                <div className="flex flex-col items-center gap-1">
+                    <div className="flex items-center gap-2 text-[32px]">
+                        🎉
+                        <span className="h2-new font-bold">Your form has been published</span>
+                    </div>
+                    <span className="p4-new text-black-700">Anyone with the link can fill your form</span>
                 </div>
-                <div className="flex items-center justify-between">
-                    <div className="max-w-[360px] break-all rounded-md px-4 py-3 text-black-700">
+                <div className="flex items-center  gap-2">
+                    <div className="text-black-700 p4-new bg-black-100 rounded-md px-3 py-2">
                         https://forms.bettercollected.io/{workspace.workspaceName}
                         /forms/
-                        <span className="text-pink-500">
-                            {standardForm.settings?.customUrl}
-                        </span>
+                        <span className="text-pink-500">{standardForm.settings?.customUrl}</span>
                     </div>
                     <Button
-                        variant={'secondary'}
+                        variant={'v2Button'}
                         onClick={() => {
-                            navigator.clipboard.writeText(
-                                `${environments.NEXT_PUBLIC_HTTP_SCHEME}://${environments.NEXT_PUBLIC_V2_CLIENT_ENDPOINT_DOMAIN}/${workspace.workspaceName}/forms/${standardForm.formId}`
-                            );
+                            navigator.clipboard.writeText(`${environments.NEXT_PUBLIC_HTTP_SCHEME}://${environments.NEXT_PUBLIC_V2_CLIENT_ENDPOINT_DOMAIN}/${workspace.workspaceName}/forms/${standardForm.formId}`);
                             toast('Copied!');
                         }}
                     >
                         Copy
                     </Button>
                 </div>
-                <div className="p2-new mt-5">
-                    To customize your form link,{' '}
-                    <a
-                        href={`${environments.NEXT_PUBLIC_HTTP_SCHEME}://${environments.NEXT_PUBLIC_DASHBOARD_DOMAIN}/${workspace.workspaceName}/dashboard/forms/${standardForm.formId}?view=FormLinks`}
-                        className="text-blue-500"
-                    >
-                        Go to form settings
-                    </a>
+                <div className="p2-new mt-5 flex flex-col items-center gap-2 md:w-[233px]">
+                    <span className="h5-new">What’s Next?</span>
+                    <span className="p4-new break-words text-center">
+                        Add your custom domain, add integration or change form privacy,{' '}
+                        <a href={`${environments.NEXT_PUBLIC_HTTP_SCHEME}://${environments.NEXT_PUBLIC_DASHBOARD_DOMAIN}/${workspace.workspaceName}/dashboard/forms/${standardForm.formId}?view=FormLinks`} className="text-blue-500">
+                            Go to form settings
+                        </a>
+                    </span>
                 </div>
                 <div className="mb-5 mt-5">
-                    <a
-                        href={`${environments.NEXT_PUBLIC_HTTP_SCHEME}://${environments.NEXT_PUBLIC_DASHBOARD_DOMAIN}/${workspace.workspaceName}/dashboard`}
-                    >
-                        <Button size={ButtonSize.Medium}>Go to dashboard</Button>
+                    <a href={`${environments.NEXT_PUBLIC_HTTP_SCHEME}://${environments.NEXT_PUBLIC_DASHBOARD_DOMAIN}/${workspace.workspaceName}/dashboard`}>
+                        <Button size={ButtonSize.Medium}>Done! Go to dashboard</Button>
                     </a>
                 </div>
             </div>
