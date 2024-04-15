b69f78c8b7e2d4056079a254739575acdcab6194
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-04-15 12:09:04 +0545

    Add animation for slide components

--- webapp/src/views/organism/Form/Form.tsx
+++ webapp/src/views/organism/Form/Form.tsx
@@ -11,7 +11,7 @@ import ThankyouPage from './ThankyouPage';
 import WelcomePage from './WelcomePage';
 
 const Form = ({ isPreviewMode = false, isMobileView = false }: { isPreviewMode?: boolean; isMobileView?: boolean }) => {
-    const { currentSlide } = useResponderState();
+    const { currentSlide, prevActiveSlide: previousSlide } = useResponderState();
 
     const { standardForm } = useStandardForm();
 
@@ -27,50 +27,54 @@ const Form = ({ isPreviewMode = false, isMobileView = false }: { isPreviewMode?:
 
     return (
         <div className={cn(isPreviewMode || isMobileView ? 'h-full w-full  ' : 'h-screen w-screen', 'relative')}>
-            <div className="absolute left-0 right-0 top-0 z-10 bg-green-500">
+            <div className="absolute left-0 right-0 top-0 !z-[80] bg-green-500">
                 <Progress value={getProgressValue()} className="h-2 rounded-none" />
             </div>
-            <AnimatePresence custom={currentSlide} mode="wait">
+            <AnimatePresence custom={currentSlide}>
                 {currentSlide === -1 && (
                     <motion.div
-                        className={cn('relative flex h-full flex-1 flex-col items-center justify-center', isMobileView ? 'aspect-[9/20]' : '')}
+                        className={cn('absolute flex h-full flex-1 flex-col items-center justify-center', isMobileView ? 'aspect-[9/20]' : '')}
                         key={'welcome-page'}
-                        initial={{ opacity: 0, x: -100 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        exit={{ opacity: 0 }}
-                        transition={{ duration: 0.2 }}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 1 }}
+                        transition={{ duration: 0.5 }}
                     >
-                        <LayoutWrapper theme={standardForm.theme} disabled layout={standardForm.welcomePage?.layout} imageUrl={standardForm?.welcomePage?.imageUrl}>
-                            <WelcomePage isPreviewMode={isPreviewMode} />
-                        </LayoutWrapper>
+                        <div className="relative h-full w-full">
+                            <LayoutWrapper theme={standardForm.theme} disabled layout={standardForm.welcomePage?.layout} imageUrl={standardForm?.welcomePage?.imageUrl}>
+                                <WelcomePage isPreviewMode={isPreviewMode} />
+                            </LayoutWrapper>
+                        </div>
                     </motion.div>
                 )}
 
                 {currentSlide >= 0 && (
                     <motion.div
-                        className={cn('relative flex h-full flex-1 flex-col items-center justify-center', isMobileView ? 'aspect-[9/20]' : '')}
+                        className={cn('absolute z-10 flex h-full w-full flex-1 flex-col items-center justify-center', isMobileView ? 'aspect-[9/20]' : '')}
                         key={currentSlide}
-                        initial={{ opacity: 0, x: -100 }}
+                        initial={{ opacity: 1, x: currentSlide >= previousSlide ? '100%' : '-100%' }}
                         animate={{ opacity: 1, x: 0 }}
-                        exit={{ opacity: 0 }}
-                        transition={{ duration: 0.2 }}
+                        exit={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5, ease: 'anticipate' }}
                     >
-                        <FormSlide index={currentSlide} isPreviewMode={isPreviewMode} />
+                        <div className="relative h-full w-full">
+                            <FormSlide index={currentSlide} isPreviewMode={isPreviewMode} />
+                        </div>
                     </motion.div>
                 )}
-
                 {currentSlide === -2 && (
                     <motion.div
-                        className={cn('relative flex h-full flex-1 flex-col items-center justify-center', isMobileView ? 'aspect-[9/20]' : '')}
+                        className={cn('absolute z-20 flex h-full flex-1 flex-col items-center justify-center', isMobileView ? 'aspect-[9/20]' : '')}
                         key={'thank-you-page'}
-                        initial={{ opacity: 0, x: -100 }}
+                        initial={{ opacity: 1, x: '100%' }}
                         animate={{ opacity: 1, x: 0 }}
-                        exit={{ opacity: 0 }}
-                        transition={{ duration: 0.2 }}
+                        transition={{ duration: 0.5, ease: 'anticipate' }}
                     >
-                        <LayoutWrapper theme={standardForm.theme} disabled layout={standardForm?.thankyouPage?.[0]?.layout} imageUrl={standardForm?.thankyouPage?.[0]?.imageUrl}>
-                            <ThankyouPage isPreviewMode={isPreviewMode} />
-                        </LayoutWrapper>
+                        <div className="relative h-full w-full">
+                            <LayoutWrapper theme={standardForm.theme} disabled layout={standardForm?.thankyouPage?.[0]?.layout} imageUrl={standardForm?.thankyouPage?.[0]?.imageUrl}>
+                                <ThankyouPage isPreviewMode={isPreviewMode} />
+                            </LayoutWrapper>
+                        </div>
                     </motion.div>
                 )}
             </AnimatePresence>
