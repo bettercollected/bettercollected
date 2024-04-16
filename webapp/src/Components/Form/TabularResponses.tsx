09fdf9753bf5ae76b3a48b25cd5629d765ab4856
sital999 <sitalnagarkoti123@gmail.com>
2024-04-16 13:06:33 +0545

    Update table UI in tabular response

--- webapp/src/Components/Form/TabularResponses.tsx
+++ webapp/src/Components/Form/TabularResponses.tsx
@@ -12,7 +12,7 @@ import { toast } from 'react-toastify';
 import { dataTableCustomStyles } from '@app/components/datatable/form/datatable-styles';
 import globalConstants from '@app/constants/global';
 import { FieldTypes, StandardFormDto, StandardFormFieldDto, StandardFormResponseDto } from '@app/models/dtos/form';
-import { FormBuilderTagNames, LabelFormBuilderTagNames } from '@app/models/enums/formBuilder';
+import { LabelFormBuilderTagNames } from '@app/models/enums/formBuilder';
 import { useAppSelector } from '@app/store/hooks';
 import { useGetFormsSubmissionsQuery } from '@app/store/workspaces/api';
 import { selectWorkspace } from '@app/store/workspaces/slice';
@@ -21,28 +21,19 @@ import { utcToLocalDateTIme } from '@app/utils/dateUtils';
 import { downloadFile } from '@app/utils/fileUtils';
 import { convertPlaceholderToDisplayValue, getAnswerForField } from '@app/utils/formBuilderBlockUtils';
 import { extractTextfromJSON } from '@app/utils/richTextEditorExtenstion/getHtmlFromJson';
+import { Expand } from 'lucide-react';
+import { ExpandIcon } from '@app/views/atoms/Icons/ExpandIcon';
 
-const customTableStylesForResponderDetail = {
+const customTableStyles = {
     ...dataTableCustomStyles,
     table: {
         ...dataTableCustomStyles.table,
         style: {
             ...dataTableCustomStyles.table.style,
-            width: '250px'
+            borderRight: '1px solid #DBDBDB',
+            boxShadow: '2px 0px 4px 0px rgba(0, 0, 0, 0.15)'
         }
     },
-
-    rows: {
-        ...dataTableCustomStyles.rows,
-        style: {
-            ...dataTableCustomStyles.rows.style,
-            cursor: 'pointer'
-        }
-    }
-};
-
-const customTableStyles = {
-    ...dataTableCustomStyles,
     rows: {
         ...dataTableCustomStyles.rows,
         style: {
@@ -104,22 +95,11 @@ export default function TabularResponses({ form }: TabularResponsesProps) {
         }
     };
 
-    const getResponseForFileUploadField = (response: StandardFormResponseDto, field: StandardFormFieldDto) => {
-        const answer = response.answers[field.id];
-
-        return (
-            <span className="p1 flex w-full cursor-pointer items-center justify-between rounded bg-blue-200 px-3 py-2" onClick={() => downloadFormFile(answer)}>
-                <span className="mr-5 flex-1 truncate">{answer?.file_metadata?.name}</span>
-                <span className="text-sm">{answer?.file_metadata?.size} MB</span>
-            </span>
-        );
-    };
-
     const getAnswerField = (response: StandardFormResponseDto, field: any) => {
         return (
             <>
-                <Typography className={cn('!text-black-600 p2-new  w-[180px] truncate')} noWrap>
-                    {field.type === FieldTypes.FILE_UPLOAD || field.type === FormBuilderTagNames.INPUT_FILE_UPLOAD ? getResponseForFileUploadField(response, field) : getAnswerForField(response, field)}
+                <Typography className={cn('!text-black-600 p2-new  w-[140px] truncate')} noWrap>
+                    {getAnswerForField(response, field)}
                 </Typography>
             </>
         );
@@ -138,7 +118,7 @@ export default function TabularResponses({ form }: TabularResponsesProps) {
 
     const responseDataOwnerField = (response: StandardFormResponseDto) => (
         <div aria-hidden className="flex w-fit flex-col gap-1 ">
-            <Typography className={cn('!text-black-800 p2-new w-[180px] truncate')} noWrap>
+            <Typography className={cn('!text-black-800 p2-new w-[140px] truncate')} noWrap>
                 {response?.dataOwnerIdentifier || '- -'}
             </Typography>
             <span className="text-black-600 text-[10px] font-normal">{utcToLocalDateTIme(response?.createdAt)}</span>
@@ -178,21 +158,31 @@ export default function TabularResponses({ form }: TabularResponsesProps) {
 
     const columnsForResponderDetail: any = [
         {
+            name: '',
+            cell: () => <ExpandIcon />,
+            style: {
+                color: 'rgba(0,0,0,.54)',
+                paddingLeft: '8px',
+                height: 'auto',
+                overflow: 'hidden ',
+                paddingRight: '8px',
+                textOverFlow: 'ellipsis'
+            },
+            width: '40px',
+            minWidth: '0px'
+        },
+        {
             name: t('FORM.RESPONDER') + ' ID',
             cell: (response: StandardFormResponseDto) => responseDataOwnerField(response),
             style: {
                 color: 'rgba(0,0,0,.54)',
                 paddingLeft: '8px',
                 height: 'auto',
-                background: '#FFFFFF',
-                opacity: 100,
-                width: '100px !important',
                 overflow: 'hidden ',
                 paddingRight: '8px',
-                textOverFlow: 'ellipsis',
-                position: 'sticky',
-                top: '0px'
-            }
+                textOverFlow: 'ellipsis'
+            },
+            width: '156px'
         }
     ];
 
@@ -206,10 +196,10 @@ export default function TabularResponses({ form }: TabularResponsesProps) {
                     color: 'rgba(0,0,0,.54)',
                     paddingLeft: '8px',
                     height: 'auto',
-                    width: '200px !important',
                     overflow: 'hidden ',
                     paddingRight: '8px'
-                }
+                },
+                width: '156px !important'
             };
         })
     ];
@@ -222,8 +212,10 @@ export default function TabularResponses({ form }: TabularResponsesProps) {
     return (
         <>
             {Array.isArray(data?.items) && (
-                <div className="flex flex-row">
-                    <DataTable onRowClicked={onRowClicked} className="!w-[300px]" columns={columnsForResponderDetail} selectableRows customStyles={customTableStyles} data={data?.items || []} />
+                <div className="gap- flex flex-row">
+                    <div className=" w-[400px] flex-1">
+                        <DataTable onRowClicked={onRowClicked} columns={columnsForResponderDetail} selectableRows customStyles={customTableStyles} data={data?.items || []} />
+                    </div>
                     <DataTable onRowClicked={onRowClicked} columns={columnForResponseData} customStyles={customTableStyles} data={data?.items || []} />
                 </div>
             )}
