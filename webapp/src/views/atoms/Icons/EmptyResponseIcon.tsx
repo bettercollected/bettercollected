989dffa0e79713a41e497cbddfbeba6e3ba010c5
Sital999 <sitalnagarkoti123@gmail.com>
2024-04-17 17:02:36 +0545

    Add new design for empty responses in ResponseTab

---
+++ webapp/src/views/atoms/Icons/EmptyResponseIcon.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function EmptyResponseIcon(props: React.SVGAttributes<{}>) {
+    return <svg width="45" height="54" viewBox="0 0 45 54" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path fillRule="evenodd" clipRule="evenodd" d="M8.77734 2C4.35907 2 0.777344 5.58172 0.777344 9.99999V46.0097C0.777344 50.428 4.35907 54.0097 8.77735 54.0097H34.2226C38.6408 54.0097 42.2226 50.428 42.2226 46.0097V13.234L36.4204 7.61715L31.0325 2.0001L34.2565 2.00007C34.2452 2.00002 34.2339 2 34.2225 2H8.77734Z" fill="#AAAAAA"/>
+        <g filter="url(#filter0_d_7818_40068)">
+            <path d="M42.2228 13.2341L42.0659 13.2369C35.8463 13.3484 30.8074 8.21661 31.0326 2L42.2228 13.2341Z" fill="#DBDBDB"/>
+        </g>
+        <rect x="14.4546" y="20.7227" width="19.8937" height="4.16078" rx="2" fill="white"/>
+        <rect x="14.4546" y="29.4609" width="19.8937" height="4.16078" rx="2" fill="white"/>
+        <rect x="14.4546" y="38.1992" width="19.8937" height="4.16078" rx="2" fill="white"/>
+        <rect x="9.06616" y="20.7227" width="4.14452" height="4.16078" rx="2.07226" fill="white"/>
+        <rect x="9.06616" y="29.4609" width="4.14452" height="4.16078" rx="2.07226" fill="white"/>
+        <rect x="9.06616" y="38.1992" width="4.14452" height="4.16078" rx="2.07226" fill="white"/>
+        <defs>
+            <filter id="filter0_d_7818_40068" x="25.0254" y="0" width="19.1975" height="19.2383" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                <feOffset dx="-2" dy="2"/>
+                <feGaussianBlur stdDeviation="2"/>
+                <feComposite in2="hardAlpha" operator="out"/>
+                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_7818_40068"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_7818_40068" result="shape"/>
+            </filter>
+        </defs>
+    </svg>
+
+}
