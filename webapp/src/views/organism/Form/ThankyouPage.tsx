7e63035a248857151b8a9d6e66bbc53dad422549
sital999 <sitalnagarkoti123@gmail.com>
2024-04-18 13:59:02 +0545

    Update submisison link and display thnkubtn conditionally

--- webapp/src/views/organism/Form/ThankyouPage.tsx
+++ webapp/src/views/organism/Form/ThankyouPage.tsx
@@ -5,9 +5,17 @@ import { cn } from '@app/shadcn/util/lib';
 import { selectForm } from '@app/store/forms/slice';
 import { useAppSelector } from '@app/store/hooks';
 import UserAvatarDropDown from '@app/views/molecules/UserAvatarDropdown';
+import { Button } from '@app/shadcn/components/ui/button';
+import { selectWorkspace } from '@app/store/workspaces/slice';
+import environments from '@app/configs/environments';
+import { selectAuthStatus } from '@app/store/auth/selectors';
+import { selectAuth } from '@app/store/auth/slice';
 
 export default function ThankyouPage({ isPreviewMode }: { isPreviewMode: boolean }) {
     const standardForm = useAppSelector(selectForm);
+    const workspace = useAppSelector(selectWorkspace);
+    const auth = useAppSelector(selectAuth);
+    const submissionUrl = environments.NEXT_PUBLIC_HTTP_SCHEME + '://' + environments.NEXT_PUBLIC_V2_CLIENT_ENDPOINT_DOMAIN + '/' + workspace.workspaceName;
     return (
         <div
             className={cn('flex h-full w-full flex-col justify-center', standardForm?.thankyouPage![0]?.layout === FormSlideLayout.SINGLE_COLUMN_NO_BACKGROUND_LEFT_ALIGN ? 'items-start' : 'items-center')}
@@ -19,33 +27,24 @@ export default function ThankyouPage({ isPreviewMode }: { isPreviewMode: boolean
                     <div className="flex">
                         <span className="text-[40px] font-bold leading-[48px]">Thank You! 🎉</span>
                     </div>
-                    <div className="p2-new text-black-700 mt-4">{standardForm?.thankyouPage?.[0]?.message || 'Your response is successfully submitted anonymously.'}</div>
-                    {/* <Button
-                        style={{ background: standardForm.theme?.secondary }}
-                        className="mt-14"
-                        size={ButtonSize.Medium}
-                    >
-                        <Link
-                            href={
-                                isPreviewMode
-                                    ? ''
-                                    : standardForm?.buttonLink ||
-                                      'https://bettercollected.com'
-                            }
-                            target="_blank"
-                            referrerPolicy="no-referrer"
-                        >
-                            {standardForm?.buttonText || 'Try Bettercollected'}
-                        </Link>
-                    </Button> */}
-                    <div className="p2-new mt-10 max-w-[400px] rounded-lg bg-white p-4">
-                        <div className=" text-black-700">You can view or request deletion of this response.</div>
-                        <div>
-                            <Link href={isPreviewMode ? '' : 'https://youtube.com'} className="text-blue-500">
-                                See all your submissions
+                    <div className="p2-new text-black-700 mt-4">{standardForm?.thankyouPage?.[0]?.message || 'Your response is successfully submitted.'}</div>
+                    {standardForm?.thankyouPage && standardForm?.thankyouPage[0].buttonText && (
+                        <Button style={{ background: standardForm.theme?.secondary }} className="mt-14">
+                            <Link href={isPreviewMode ? '' : standardForm?.thankyouPage[0].buttonLink || 'https://bettercollected.com'} target="_blank" referrerPolicy="no-referrer">
+                                {standardForm?.thankyouPage[0].buttonText}
                             </Link>
+                        </Button>
+                    )}
+                    {auth.id && (
+                        <div className="p2-new mt-10 max-w-[400px] rounded-lg bg-white p-4">
+                            <div className=" text-black-700">You can view or request deletion of this response.</div>
+                            <div>
+                                <Link href={isPreviewMode ? '' : submissionUrl} className="text-blue-500">
+                                    See all your submissions
+                                </Link>
+                            </div>
                         </div>
-                    </div>
+                    )}
                 </div>
             </div>
         </div>
