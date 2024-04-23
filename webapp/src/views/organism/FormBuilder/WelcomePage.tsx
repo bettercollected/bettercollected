3bcf779841c3e842a025ef10a499e5279d535cae
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-04-23 12:53:23 +0545

    Fix multiple line for form title

--- webapp/src/views/organism/FormBuilder/WelcomePage.tsx
+++ webapp/src/views/organism/FormBuilder/WelcomePage.tsx
@@ -19,45 +19,29 @@ const WelcomeSlide = ({ disabled }: { disabled?: boolean }) => {
 
     return (
         <GreetingLayoutWrapper theme={theme} disabled={disabled} greetingIndex={-10}>
-            <div
-                className={cn(
-                    'flex h-full flex-col justify-center bg-transparent',
-                    formState.welcomePage?.layout ===
-                        FormSlideLayout.SINGLE_COLUMN_NO_BACKGROUND_LEFT_ALIGN
-                        ? ' items-start'
-                        : ' items-center'
-                )}
-            >
+            <div className={cn('flex h-full flex-col justify-center bg-transparent', formState.welcomePage?.layout === FormSlideLayout.SINGLE_COLUMN_NO_BACKGROUND_LEFT_ALIGN ? ' items-start' : ' items-center')}>
                 <div className={cn('flex w-full max-w-[800px] flex-col items-start')}>
-                    <input
-                        type="text"
+                    <AutosizeTextarea
                         placeholder="Form Title"
-                        className="mb-4 w-full border-0 px-0 py-0 text-[40px] font-bold"
+                        className=" w-full border-0 px-0 py-0 text-[40px] font-bold leading-[60px]"
                         value={formState.welcomePage?.title}
                         onChange={(event) => {
                             setWelcomeTitle(event.target.value);
                         }}
                     />
-                    {formState?.welcomePage?.description !== undefined &&
-                    formState?.welcomePage?.description !== null ? (
+                    {formState?.welcomePage?.description !== undefined && formState?.welcomePage?.description !== null ? (
                         <>
                             <AutosizeTextarea
                                 placeholder="Add description"
                                 value={formState?.welcomePage?.description || ''}
-                                className="ring-none w-full border-0 px-0 text-base text-black-600 outline-none"
-                                onChange={(e: any) =>
-                                    setFormDescription(e.target.value)
-                                }
+                                className="ring-none text-black-600 mb-4 w-full border-0 px-0 text-base outline-none"
+                                onChange={(e: any) => setFormDescription(e.target.value)}
                             />
                         </>
                     ) : (
                         <></>
                     )}
-                    <Button
-                        size={'medium'}
-                        className="z-10 mt-12 rounded px-8 py-3"
-                        style={{ background: theme?.secondary }}
-                    >
+                    <Button size={'medium'} className="z-10 mt-12 rounded px-8 py-3" style={{ background: theme?.secondary }}>
                         {formState.welcomePage?.buttonText || 'Start'}
                     </Button>
                 </div>
