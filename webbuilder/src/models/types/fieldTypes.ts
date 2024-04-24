abdd5941c409f171af8dc574cedffbdd20dbc4b2
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-04-24 13:07:17 +0545

    Remove webbuilder

--- webbuilder/src/models/types/fieldTypes.ts
+++
@@ -1,7 +0,0 @@
-export interface FileMetadata {
-    id: string;
-    name?: string;
-    type?: string;
-    size?: number;
-    url?: string;
-}
--- webbuilder/src/store/mutations/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from '@app/store/store';
-
-// @ts-ignore
-export const selectPatchingTemplate = (state: RootState) =>
-    state.mutationStatus.patchTemplate;
