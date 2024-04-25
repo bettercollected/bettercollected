1c2a26938cd447ea674f36c224828452e11158c1
sital999 <sitalnagarkoti123@gmail.com>
2024-04-25 16:42:32 +0545

    Replace Pages btn with Plus Icon btn

--- webapp/src/views/organism/FormBuilder/AddSlide/AddSlidePopover.tsx
+++ webapp/src/views/organism/FormBuilder/AddSlide/AddSlidePopover.tsx
@@ -5,21 +5,12 @@ import { AnimatePresence, motion } from 'framer-motion';
 
 import { ButtonSize } from '@app/models/enums/button';
 import { Button } from '@app/shadcn/components/ui/button';
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger
-} from '@app/shadcn/components/ui/popover';
-import {
-    Tabs,
-    TabsContent,
-    TabsList,
-    TabsTrigger
-} from '@app/shadcn/components/ui/tabs';
+import { Popover, PopoverContent, PopoverTrigger } from '@app/shadcn/components/ui/popover';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@app/shadcn/components/ui/tabs';
 import TemplateTab from '@app/views/organism/FormBuilder/AddSlide/TemplateTab';
 
 import LayoutsTab from './LayoutsTab';
-
+import PlusIcon from '@Components/Common/Icons/Common/Plus';
 
 export default function AddSlidePopover() {
     const [open, setOpen] = useState(false);
@@ -38,45 +29,18 @@ export default function AddSlidePopover() {
                     }}
                 >
                     <PopoverTrigger>
-                        <Button
-                            variant={'v2GhostButton'}
-                            // className="!p-2"
-                            size={ButtonSize.Small}
-                        >
-                            Add Page
-                        </Button>
+                        <Button variant={'v2GhostButton'} size={ButtonSize.Small} icon={<PlusIcon className="text-black-900 h-5 w-5" strokeWidth={'1.5'} />}></Button>
                     </PopoverTrigger>
                     {open && (
-                        <motion.div
-                            className="!z-10"
-                            key="addSlide"
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 0.2 }}
-                        >
-                            <PopoverContent
-                                side="right"
-                                align="start"
-                                asChild
-                                className="p-0"
-                            >
+                        <motion.div className="!z-10" key="addSlide" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.2 }}>
+                            <PopoverContent side="right" align="start" asChild className="p-0">
                                 <div className="!z-[1000] max-h-[640px] w-[390px] rounded-lg bg-white shadow-lg">
-                                    <Tabs
-                                        defaultValue="layout"
-                                        className="h-full w-full"
-                                    >
+                                    <Tabs defaultValue="layout" className="h-full w-full">
                                         <TabsList className=" flex h-auto w-full gap-2 p-4">
-                                            <TabsTrigger
-                                                value="layout"
-                                                className="w-full"
-                                            >
+                                            <TabsTrigger value="layout" className="w-full">
                                                 Layout
                                             </TabsTrigger>
-                                            <TabsTrigger
-                                                value="template"
-                                                className="w-full"
-                                            >
+                                            <TabsTrigger value="template" className="w-full">
                                                 Template
                                             </TabsTrigger>
                                             {/*<TabsTrigger*/}
