a5ecb77b8c5f00191b81dc38caad0971d85b6575
sital999 <sitalnagarkoti123@gmail.com>
2024-04-26 14:18:40 +0545

    Show default image in Unsplash modal

--- webapp/src/views/molecules/UnsplashImagePicker/PhotoSearch.tsx
+++ webapp/src/views/molecules/UnsplashImagePicker/PhotoSearch.tsx
@@ -18,7 +18,7 @@ function PhotoSearch({ setQuery, query, onSearch }: Props) {
         <form onSubmit={handleSubmit} className="flex items-center space-x-2">
             <label className=" w-full">
                 <input
-                    className="placeholder:theme-text-subtitle-1 theme-border-default  focus:theme-border-primary w-full rounded-md border py-2 pl-3 pr-3  placeholder:italic focus:outline-none focus:ring-1 sm:text-sm"
+                    className="placeholder:theme-text-subtitle-1 theme-border-default  focus:theme-border-primary w-full rounded-md border py-2 pl-3 pr-3 focus:outline-none focus:ring-1 sm:text-sm"
                     placeholder="Search for an image"
                     type="text"
                     name="search"
