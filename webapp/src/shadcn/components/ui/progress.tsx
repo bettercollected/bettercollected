f39e88da6b2de2e01767e6ee63cdca5562541175
sital999 <sitalnagarkoti123@gmail.com>
2024-04-26 12:30:43 +0545

    Change progress Bar color to secondary color of theme

--- webapp/src/shadcn/components/ui/progress.tsx
+++ webapp/src/shadcn/components/ui/progress.tsx
@@ -6,22 +6,9 @@ import * as ProgressPrimitive from '@radix-ui/react-progress';
 
 import { cn } from '@app/shadcn/util/lib';
 
-const Progress = React.forwardRef<
-    React.ElementRef<typeof ProgressPrimitive.Root>,
-    React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
-    <ProgressPrimitive.Root
-        ref={ref}
-        className={cn(
-            'relative h-4 w-full overflow-hidden rounded-full bg-white',
-            className
-        )}
-        {...props}
-    >
-        <ProgressPrimitive.Indicator
-            className="h-full w-full flex-1 bg-brand-500 transition-all"
-            style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-        />
+const Progress = React.forwardRef<React.ElementRef<typeof ProgressPrimitive.Root>, React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { indicatorColor?: string }>(({ className, value, indicatorColor, ...props }, ref) => (
+    <ProgressPrimitive.Root ref={ref} className={cn('relative h-2 w-full overflow-hidden rounded-full bg-white', className)} {...props}>
+        <ProgressPrimitive.Indicator className="h-full w-full flex-1 transition-all" style={{ transform: `translateX(-${100 - (value || 0)}%)`, background: indicatorColor }} />
     </ProgressPrimitive.Root>
 ));
 Progress.displayName = ProgressPrimitive.Root.displayName;
