11f71c4b7a841493d14c3987ffc71c84f5bc507d
sital999 <sitalnagarkoti123@gmail.com>
2024-05-01 15:21:51 +0545

    Implement InputWrapper in responder side

--- webapp/src/Components/HOCs/FieldInputWrapper.tsx
+++ webapp/src/Components/HOCs/FieldInputWrapper.tsx
@@ -1,19 +1,17 @@
-import { FieldTypes, StandardFormFieldDto } from '@app/models/dtos/form';
+import { StandardFormFieldDto } from '@app/models/dtos/form';
 import { FieldInput } from '@app/shadcn/components/ui/input';
 import { useFormState } from '@app/store/jotai/form';
-import { getPlaceholderValueForField } from '@app/utils/formUtils';
 import { useEffect, useState } from 'react';
 import { useDebounceValue } from 'usehooks-ts';
 
 interface IFieldInputWrapper extends React.InputHTMLAttributes<HTMLInputElement> {
-    field?: StandardFormFieldDto;
     slide?: StandardFormFieldDto;
-    value?: string;
+    value?: string | number | undefined;
     onChange: any;
     disabled?: boolean;
 }
 
-export const FieldInputWrapper = ({ field, slide, value, onChange, disabled, className, type = 'text' }: IFieldInputWrapper) => {
+export const FieldInputWrapper = ({ id, slide, value, onChange, disabled, className, type = 'text', multiple, placeholder }: IFieldInputWrapper) => {
     const { theme } = useFormState();
     const [inputVal, setInputVal] = useState(value);
     const [debouncedInputValue] = useDebounceValue(inputVal, 300);
@@ -31,15 +29,17 @@ export const FieldInputWrapper = ({ field, slide, value, onChange, disabled, cla
     return (
         <>
             <FieldInput
+                id={id}
                 disabled={disabled}
                 type={type}
                 value={inputVal}
                 style={{
                     color: slide?.properties?.theme?.tertiary || theme?.tertiary
                 }}
-                placeholder={getPlaceholderValueForField(field?.type || FieldTypes.SHORT_TEXT)}
                 onChange={(e: any) => setInputVal(e.target.value)}
                 className={className}
+                multiple={multiple}
+                placeholder={placeholder}
             />
         </>
     );
