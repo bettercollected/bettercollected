c2ff4a5a658e23f77d64df4b8a2dee7e12096ab8
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-05-02 11:55:42 +0545

    Add opacity to header matrix input and scrollbar thumb

--- webapp/src/shadcn/components/ui/scroll-area.tsx
+++ webapp/src/shadcn/components/ui/scroll-area.tsx
@@ -28,7 +28,7 @@ const ScrollBar = React.forwardRef<React.ElementRef<typeof ScrollAreaPrimitive.S
         className={cn('flex touch-none select-none transition-colors', orientation === 'vertical' && 'h-full w-2.5 border-l border-l-transparent p-[1px]', orientation === 'horizontal' && 'h-2.5 flex-col border-t border-t-transparent p-[1px]', className)}
         {...props}
     >
-        <ScrollAreaPrimitive.ScrollAreaThumb className="bg-black-200 relative flex-1 rounded-full" style={{ background: thumbBg }} />
+        <ScrollAreaPrimitive.ScrollAreaThumb className={cn('bg-black-200 relative flex-1 rounded-full ', thumbBg && 'opacity-50')} style={{ background: thumbBg }} />
     </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
