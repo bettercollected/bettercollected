a36a02abf62dcf03e22d52a30f30901d73b3f3e1
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-05-03 14:34:39 +0545

    Show unpublished form in mobile preview

--- webapp/src/app/[workspace_name]/forms/[form_id]/page.tsx
+++ webapp/src/app/[workspace_name]/forms/[form_id]/page.tsx
@@ -4,39 +4,42 @@ import Loader from '@app/components/ui/loader';
 import environments from '@app/configs/environments';
 import ReduxWrapperAppRouter from '@app/containers/ReduxWrapperAppRouter';
 import SingleFormPage from '@app/pages/forms/v1/[id]';
-import { setForm } from '@app/store/forms/slice';
-import { useAppDispatch, useAppSelector } from '@app/store/hooks';
-import { useFormState } from '@app/store/jotai/form';
-import { useGetWorkspaceFormQuery } from '@app/store/workspaces/api';
-import { selectWorkspace } from '@app/store/workspaces/slice';
+import {setForm} from '@app/store/forms/slice';
+import {useAppDispatch, useAppSelector} from '@app/store/hooks';
+import {useFormState} from '@app/store/jotai/form';
+import {useGetWorkspaceFormQuery} from '@app/store/workspaces/api';
+import {selectWorkspace} from '@app/store/workspaces/slice';
 import FullScreenLoader from '@app/views/atoms/Loaders/FullScreenLoader';
 import Form from '@app/views/organism/Form/Form';
-import { useEffect, useRef } from 'react';
+import {useEffect, useRef} from 'react';
+import {useRouter} from "next/navigation";
 
-export default function FormPage({ params }: { params: { form_id: string; workspace_name: string } }) {
+export default function FormPage({params}: { params: { form_id: string; workspace_name: string } }) {
     const slug = params.form_id;
 
     return (
         <ReduxWrapperAppRouter>
-            <FetchFormWrapper slug={slug} />
+            <FetchFormWrapper slug={slug}/>
         </ReduxWrapperAppRouter>
     );
 }
 
-const FetchFormWrapper = ({ slug }: { slug: string }) => {
+const FetchFormWrapper = ({slug}: { slug: string }) => {
     const workspace = useAppSelector(selectWorkspace);
     const dispatch = useAppDispatch();
     const iframeRef = useRef(null);
-    const { updateFormTheme } = useFormState();
+    const {updateFormTheme} = useFormState();
 
-    const { data, isLoading, error } = useGetWorkspaceFormQuery(
+    const {data, isLoading, error} = useGetWorkspaceFormQuery(
         {
             workspace_id: workspace.id,
             custom_url: slug,
             published: true
         },
-        { skip: !workspace.id }
+        {skip: !workspace.id}
     );
+
+    const router = useRouter()
 
     useEffect(() => {
         if (data?.formId) {
@@ -45,12 +48,33 @@ const FetchFormWrapper = ({ slug }: { slug: string }) => {
         }
     }, [data]);
 
+    useEffect(() => {
+        if (data?.importedFormId && data.settings?.showOriginalForm && hasFileUpload(data?.fields || [])) {
+            router.push(data?.settings?.embedUrl || '');
+        }
+    }, [data]);
+
     const hasCustomDomain = window.location.host !== environments.FORM_DOMAIN;
 
-    if (isLoading || error) {
-        return <FullScreenLoader />;
-    }
+
     const responderUri = data?.settings?.embedUrl || '';
+
+    const hasFileUpload = (fields: Array<any>) => {
+        let isUploadField = false;
+        if (fields && Array.isArray(fields) && fields.length > 0) {
+            fields.forEach((field: any) => {
+                if (field && field?.type && field.type === 'file_upload') {
+                    isUploadField = true;
+                }
+            });
+            return isUploadField;
+        }
+        return isUploadField;
+    };
+
+    if (isLoading || error || (data?.importedFormId && data.settings?.showOriginalForm && hasFileUpload(data?.fields))) {
+        return <FullScreenLoader/>;
+    }
 
     if (data?.importedFormId && data.settings?.showOriginalForm) {
         return (
@@ -58,7 +82,7 @@ const FetchFormWrapper = ({ slug }: { slug: string }) => {
                 <div className="relative !min-h-screen">
                     <div className="!m-0' absolute bottom-0 left-0 right-0 top-0 !p-0">
                         <iframe ref={iframeRef} src={`${responderUri}?embedded=true`} width="100%" height="100%">
-                            <Loader />
+                            <Loader/>
                         </iframe>
                     </div>
                 </div>
@@ -68,8 +92,9 @@ const FetchFormWrapper = ({ slug }: { slug: string }) => {
 
     return (
         <div className="h-screen w-screen">
-            {data?.builderVersion === 'v2' && <Form />}
-            {data?.builderVersion !== 'v2' && <SingleFormPage hasCustomDomain={hasCustomDomain} slug={slug} form={data} workspace={workspace} />}
+            {data?.builderVersion === 'v2' && <Form/>}
+            {data?.builderVersion !== 'v2' &&
+                <SingleFormPage hasCustomDomain={hasCustomDomain} slug={slug} form={data} workspace={workspace}/>}
         </div>
     );
 };
--- webapp/src/views/molecules/FormBuilder/PreviewWrapper.tsx
+++ webapp/src/views/molecules/FormBuilder/PreviewWrapper.tsx
@@ -1,18 +1,21 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
 import environments from '@app/configs/environments';
-import { Button } from '@app/shadcn/components/ui/button';
-import { Separator } from '@app/shadcn/components/ui/separator';
-import { cn } from '@app/shadcn/util/lib';
-import { selectForm } from '@app/store/forms/slice';
-import { useAppSelector } from '@app/store/hooks';
-import { selectWorkspace } from '@app/store/workspaces/slice';
-import { DesktopIcon } from '@app/views/atoms/Icons/DesktopIcon';
-import { MobileIcon } from '@app/views/atoms/Icons/MobileIcon';
+import {Button} from '@app/shadcn/components/ui/button';
+import {Separator} from '@app/shadcn/components/ui/separator';
+import {cn} from '@app/shadcn/util/lib';
+import {selectForm} from '@app/store/forms/slice';
+import {useAppSelector} from '@app/store/hooks';
+import {selectWorkspace} from '@app/store/workspaces/slice';
+import {DesktopIcon} from '@app/views/atoms/Icons/DesktopIcon';
+import {MobileIcon} from '@app/views/atoms/Icons/MobileIcon';
 import PublishButton from './PublishButton';
-import { Skeleton } from '@app/shadcn/components/ui/skeleton';
+import {Skeleton} from '@app/shadcn/components/ui/skeleton';
 
-const PreviewWrapper = ({ children, handleResetResponderState }: { children: React.ReactNode; handleResetResponderState: () => void }) => {
+const PreviewWrapper = ({children, handleResetResponderState}: {
+    children: React.ReactNode;
+    handleResetResponderState: () => void
+}) => {
     const [key, setKey] = useState(1);
     const [isDesktopView, setIsDesktopView] = useState(true);
     const [isIframeLoaded, setIFrameLoaded] = useState(false);
@@ -26,18 +29,20 @@ const PreviewWrapper = ({ children, handleResetResponderState }: { children: Rea
 
     console.log(isDesktopView, isIframeLoaded);
 
-    const mobileViewPreviewUrl = `${environments.HTTP_SCHEME}${environments.FORM_DOMAIN}/${workspace.workspaceName}/forms/${standardForm.formId}?isPreview=true`;
+    const mobileViewPreviewUrl = `${environments.HTTP_SCHEME}${environments.FORM_DOMAIN}/${workspace.workspaceName}/forms/${standardForm.formId}/preview`;
     return (
         <div className=" h-full w-full bg-white">
             <nav className="flex h-14 flex-row justify-between px-4 py-2">
                 <div></div>
                 <div className="flex items-center gap-4 text-xs font-semibold">
-                    <div onClick={() => setIsDesktopView(true)} className={cn('flex cursor-pointer items-center gap-1 p-2 text-xs', isDesktopView ? 'bg-black-200 text-black-800  rounded-lg' : 'text-black-500')}>
-                        <DesktopIcon />
+                    <div onClick={() => setIsDesktopView(true)}
+                         className={cn('flex cursor-pointer items-center gap-1 p-2 text-xs', isDesktopView ? 'bg-black-200 text-black-800  rounded-lg' : 'text-black-500')}>
+                        <DesktopIcon/>
                         Desktop
                     </div>
-                    <div onClick={() => setIsDesktopView(false)} className={cn('flex cursor-pointer items-center gap-1 p-2 text-xs', isDesktopView ? 'text-black-500' : 'bg-black-200 text-black-800 rounded-lg p-2')}>
-                        <MobileIcon />
+                    <div onClick={() => setIsDesktopView(false)}
+                         className={cn('flex cursor-pointer items-center gap-1 p-2 text-xs', isDesktopView ? 'text-black-500' : 'bg-black-200 text-black-800 rounded-lg p-2')}>
+                        <MobileIcon/>
                         Mobile
                     </div>
                 </div>
@@ -52,25 +57,32 @@ const PreviewWrapper = ({ children, handleResetResponderState }: { children: Rea
                     >
                         Restart
                     </Button>
-                    <PublishButton />
+                    <PublishButton/>
                 </div>
             </nav>
-            <Separator />
-            <div className=" mx-10 h-full py-10 pb-24 drop-shadow-xl  ">
+            <Separator/>
+            <div className=" mx-10 h-full py-10 pb-24 drop-shadow-xl ">
                 {isDesktopView ? (
                     <div className="mx-auto aspect-video !max-h-full !max-w-full">{children}</div>
                 ) : (
-                    <iframe
-                        onLoad={() => {
-                            setIFrameLoaded(true);
-                        }}
-                        key={key.toString()}
-                        title="responder-mobile-view"
-                        className="mx-auto aspect-[9/16] h-full rounded-lg drop-shadow-xl"
-                        src={mobileViewPreviewUrl}
-                    />
+                    <div
+                        className="mx-auto relative aspect-[9/16] h-full rounded-lg drop-shadow-xl"
+                    >
+                        <iframe
+                            onLoad={() => {
+                                setIFrameLoaded(true);
+                            }}
+                            key={key.toString()}
+                            className="absolute inset-0 aspect-[9/16] h-full"
+                            title="responder-mobile-view"
+                            src={mobileViewPreviewUrl}
+                        />
+                        {!isIframeLoaded &&
+                            <Skeleton className="absolute inset-0 pb-10 h-full"/>}
+
+                    </div>
                 )}
-                {!isDesktopView && !isIframeLoaded && <Skeleton className="mx-auto aspect-[9/16] h-full rounded-lg drop-shadow-xl" />}
+
             </div>
         </div>
     );
