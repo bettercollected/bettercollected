a885f4e69800a87742563c44c9292369571a8178
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-05-06 13:18:08 +0545

    Upate tabs trigger ui in add slide popover

--- webapp/src/views/organism/FormBuilder/AddSlide/AddSlidePopover.tsx
+++ webapp/src/views/organism/FormBuilder/AddSlide/AddSlidePopover.tsx
@@ -3,14 +3,12 @@ import { useState } from 'react';
 import { Divider } from '@mui/material';
 import { AnimatePresence, motion } from 'framer-motion';
 
-import { ButtonSize } from '@app/models/enums/button';
-import { Button } from '@app/shadcn/components/ui/button';
 import { Popover, PopoverContent, PopoverTrigger } from '@app/shadcn/components/ui/popover';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@app/shadcn/components/ui/tabs';
 import TemplateTab from '@app/views/organism/FormBuilder/AddSlide/TemplateTab';
 
-import LayoutsTab from './LayoutsTab';
 import { PlusIcon } from '@app/views/atoms/Icons/PlusIcon';
+import LayoutsTab from './LayoutsTab';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from './AddSlideTabs';
 
 export default function AddSlidePopover() {
     const [open, setOpen] = useState(false);
@@ -33,21 +31,17 @@ export default function AddSlidePopover() {
                             <PlusIcon className=" h-5 w-5" strokeWidth={'1.5'} />
                         </div>
                     </PopoverTrigger>
-                    <PopoverContent side="right" align="start" asChild className="p-0">
+                    <PopoverContent side="right" align="start" asChild className="w-[391px] p-0">
                         {open && (
                             <motion.div key="addSlide" initial={{ opacity: 0, x: '-10%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.2 }}>
-                                <div className="!z-[1000] max-h-[640px] w-[390px] rounded-lg bg-white shadow-lg">
+                                <div className="!z-[1000] max-h-[640px] w-full rounded bg-white shadow-lg">
                                     <Tabs defaultValue="layout" className="h-full w-full">
                                         <TabsList className=" flex h-auto w-full gap-2 p-4">
-                                            <TabsTrigger value="layout" className="w-full">
-                                                Layout
-                                            </TabsTrigger>
-                                            <TabsTrigger value="template" className="w-full">
-                                                Template
-                                            </TabsTrigger>
+                                            <TabsTrigger value="layout">Layout</TabsTrigger>
+                                            <TabsTrigger value="template">Template</TabsTrigger>
                                         </TabsList>
                                         <Divider />
-                                        <TabsContent value="layout" className="p-4">
+                                        <TabsContent value="layout" className="p-4 !pr-2">
                                             <LayoutsTab closePopover={closePopover} />
                                         </TabsContent>
                                         <TabsContent value="template" className="p-4">
