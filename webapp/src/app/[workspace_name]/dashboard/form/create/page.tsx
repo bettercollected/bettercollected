91e92e606cdf2dffa2459300d3b0ba8476b47746
sital999 <sitalnagarkoti123@gmail.com>
2024-05-09 16:16:16 +0545

    Fix responsive and handle for publishBtn in mobile

--- webapp/src/app/[workspace_name]/dashboard/form/create/page.tsx
+++ webapp/src/app/[workspace_name]/dashboard/form/create/page.tsx
@@ -8,6 +8,7 @@ import cn from 'classnames';
 
 import { useModal } from '@app/components/modal-views/context';
 import { defaultForm } from '@app/constants/form';
+import { getDefaultImageFromUnsplash } from '@app/lib/getDefaultImageFromUnsplash';
 import { Sheet, SheetContent, SheetTrigger } from '@app/shadcn/components/ui/sheet';
 import { useAppSelector } from '@app/store/hooks';
 import useFormFieldsAtom from '@app/store/jotai/fieldSelector';
@@ -20,7 +21,7 @@ import NavBar from '@app/views/molecules/FormBuilder/Navbar';
 import WelcomePage from '@app/views/organism/Form/WelcomePage';
 import LayoutWrapper from '@app/views/organism/Layout/LayoutWrapper';
 import useDrivePicker from '@fyelci/react-google-drive-picker';
-import { getDefaultImageFromUnsplash } from '@app/lib/getDefaultImageFromUnsplash';
+import { useIsMobile } from '@app/lib/hooks/use-breakpoint';
 
 const CardVariants = {
     blue: 'text-blue-500 hover:bg-blue-100 transition hover:border-blue-100',
@@ -35,6 +36,7 @@ export default function CreateFormPage() {
     const router = useRouter();
     const [openPicker] = useDrivePicker();
     const { openModal } = useModal();
+    const isMobile = useIsMobile();
 
     const { data: templates } = useGetTemplatesQuery({ v2: true });
 
@@ -43,7 +45,7 @@ export default function CreateFormPage() {
     const handleCreateForm = async (type: string) => {
         const isMultiPage = type === 'Modern Form';
         resetFields();
-        const imageResponse = await getDefaultImageFromUnsplash('minimal');
+        const imageResponse = await getDefaultImageFromUnsplash('New minimal');
         const newPics = imageResponse?.response?.results;
         const randomIndexes = Array.from({ length: 3 }, () => Math.floor(Math.random() * 10));
         const pic = newPics?.filter((item: any, index: number) => randomIndexes.includes(index));
@@ -73,48 +75,56 @@ export default function CreateFormPage() {
     return (
         <div className="min-h-screen bg-white">
             <NavBar />
-            <div className="px-auto flex h-full justify-center ">
+            <div className="px-auto flex h-full flex-col justify-center lg:flex-row ">
                 <div className=" flex max-w-[1330px] flex-col px-5 md:px-10">
                     <div className="h3-new text-black-800 mb-4 mt-6">New Form</div>
-                    <div className="flex flex-wrap gap-6">
-                        <Sheet>
-                            <SheetTrigger asChild>
-                                <Card variant={'blue'} icon={<PlusIcon />} content={'Create New Form'} onClick={() => {}} />
-                            </SheetTrigger>
-                            <SheetContent className=" shadow-v2 h-full w-full p-0 drop-shadow-2xl" side={'top'} hideCloseIcon>
-                                <div className="h-full w-full bg-white ">
-                                    <NavBar isModal />
-                                    <FormTypeSelectionComponent handleCreateForm={handleCreateForm} />
-                                </div>
-                            </SheetContent>
-                        </Sheet>
+                    <div className="flex w-full flex-col flex-wrap gap-6 lg:flex-row">
+                        {isMobile ? (
+                            <Card variant={'pink'} icon={<PlusIcon />} content={'Create New Form'} onClick={() => {}} />
+                        ) : (
+                            <Sheet>
+                                <SheetTrigger asChild>
+                                    <Card variant={'blue'} icon={<PlusIcon />} content={'Create New Form'} onClick={() => {}} />
+                                </SheetTrigger>
+                                <SheetContent className=" shadow-v2 h-full w-full p-0 drop-shadow-2xl" side={'top'} hideCloseIcon>
+                                    <div className="h-full w-full bg-white ">
+                                        <NavBar isModal />
+                                        <FormTypeSelectionComponent handleCreateForm={handleCreateForm} />
+                                    </div>
+                                </SheetContent>
+                            </Sheet>
+                        )}
 
                         <Card
                             variant={'purple'}
-                            icon={<GoogleFormIcon width={30} height={30} className="mb-2 text-purple-500" />}
+                            icon={<GoogleFormIcon width={30} height={30} className="mb-2 w-full text-purple-500" />}
                             content={'Import Google Form'}
                             onClick={() => {
                                 openModal('IMPORT_FORMS', { nonClosable: true });
                             }}
                         />
-                        <Card variant={'pink'} icon={<AIIcon />} content={'Start with AI'} onClick={() => {}} addSoon />
+                        {!isMobile && <Card variant={'pink'} icon={<AIIcon />} content={'Start with AI'} onClick={() => {}} addSoon />}
                     </div>
 
-                    <div className="h3-new text-black-800 mb-4 mt-12">Templates</div>
-                    <div className="flex w-full flex-row flex-wrap gap-x-6 gap-y-10  ">
-                        {templates?.map((template) => (
-                            <div className="flex cursor-pointer flex-col rounded-lg border border-transparent p-1 hover:border-pink-500" key={template?.id} onClick={() => createFormFromTemplate(template.id)}>
-                                <div className="relative h-[157px] w-[281px] overflow-hidden rounded-md">
-                                    <div className="pointer-events-none h-[810px] w-[1440px] scale-[0.195]" style={{ transformOrigin: 'top left' }}>
-                                        <LayoutWrapper theme={template?.theme} disabled layout={template.welcomePage?.layout} imageUrl={template?.welcomePage?.imageUrl}>
-                                            <WelcomePage isPreviewMode theme={template?.theme} welcomePageData={template?.welcomePage} />
-                                        </LayoutWrapper>
+                    {!isMobile && (
+                        <>
+                            <div className="h3-new text-black-800 mb-4 mt-12">Templates</div>
+                            <div className="flex w-full flex-row flex-wrap gap-x-6 gap-y-10  ">
+                                {templates?.map((template) => (
+                                    <div className="flex cursor-pointer flex-col rounded-lg border border-transparent p-1 hover:border-pink-500" key={template?.id} onClick={() => createFormFromTemplate(template.id)}>
+                                        <div className="relative h-[157px] w-[281px] overflow-hidden rounded-md">
+                                            <div className="pointer-events-none h-[810px] w-[1440px] scale-[0.195]" style={{ transformOrigin: 'top left' }}>
+                                                <LayoutWrapper showDesktopLayout theme={template?.theme} disabled layout={template.welcomePage?.layout} imageUrl={template?.welcomePage?.imageUrl}>
+                                                    <WelcomePage isPreviewMode theme={template?.theme} welcomePageData={template?.welcomePage} />
+                                                </LayoutWrapper>
+                                            </div>
+                                        </div>
+                                        <div className="p2-new mt-2 !font-medium">{template.title}</div>
                                     </div>
-                                </div>
-                                <div className="p2-new mt-2 !font-medium">{template.title}</div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
@@ -127,22 +137,33 @@ interface CardWrapperProps {
     onClick: React.MouseEventHandler<HTMLDivElement>;
     variant: 'blue' | 'purple' | 'pink';
     addSoon?: boolean;
+    soonMsg?: string;
 }
 
-const Card = ({ icon, content, onClick, variant, addSoon }: CardWrapperProps) => {
+const Card = ({ icon, content, onClick, variant, addSoon, soonMsg }: CardWrapperProps) => {
     return (
-        <div className={cn('border-black-300 relative flex h-[117px] w-full cursor-pointer flex-col items-center justify-center overflow-hidden rounded-lg border bg-white sm:w-[220px]', CardVariants[variant])} onClick={onClick}>
+        <div className={cn('border-black-300 relative flex h-[170px] w-full cursor-pointer flex-col items-center justify-center overflow-hidden rounded-lg border bg-white lg:h-[117px] lg:w-[220px]', CardVariants[variant])} onClick={onClick}>
             {icon}
             <span className="p3-new text-black-800 mb-1 mt-2">{content}</span>
-            {addSoon && <SoonComponent />}
+            {content === 'Create New Form' && <OnlyAvailableInDesktopVersion />}
+            {addSoon && <SoonComponent msg={soonMsg} />}
         </div>
     );
 };
 
-const SoonComponent = () => {
+const SoonComponent = ({ msg }: { msg?: string }) => {
     return (
-        <div className="bg-new-pink absolute bottom-0 left-1/3 flex h-5 w-full -rotate-[30deg] items-center justify-center text-[10px] font-medium leading-none text-white">
-            <span className=" ml-14 md:ml-4">Soon</span>
+        <div className="bg-new-pink absolute bottom-0 left-1/3 flex h-fit w-full -rotate-[30deg] items-center justify-center text-[10px] font-medium leading-none text-white">
+            <span className=" ml-14 md:ml-4">{msg || 'Soon'}</span>
+        </div>
+    );
+};
+
+const OnlyAvailableInDesktopVersion = () => {
+    return (
+        <div className="bg-new-pink absolute -right-10 bottom-3 flex h-auto w-[200px] -rotate-[30deg] flex-col items-center justify-center gap-1 py-2 text-[10px] font-medium leading-none text-white lg:hidden">
+            <span className="  md:ml-4">Only available in</span>
+            <span className="  md:ml-4">desktop version</span>
         </div>
     );
 };
