d48872357a6d33ed3793326a50fb84166a8f6d1f
sital999 <sitalnagarkoti123@gmail.com>
2024-05-15 17:32:27 +0545

    Update responsive for unsplashPhotoList and refactor PhotoSearch component

--- webapp/src/views/molecules/UnsplashImagePicker/index.tsx
+++ webapp/src/views/molecules/UnsplashImagePicker/index.tsx
@@ -5,12 +5,15 @@ import React from 'react';
 import { useDialogModal } from '@app/lib/hooks/useDialogModal';
 import { Unsplash } from '@app/views/atoms/Icons/Brands/Unsplash';
 
+import { Separator } from '@app/shadcn/components/ui/separator';
 import { selectForm } from '@app/store/forms/slice';
 import { useAppSelector } from '@app/store/hooks';
 import CircularProgressBar from '@app/views/atoms/Loaders/CircularLoadingAnimation';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@app/views/organism/FormBuilder/AddSlide/AddSlideTabs';
 import PhotoList from './PhotoList';
 import SearchBar from './PhotoSearch';
 import Link from 'next/link';
+import UploadMediaComponent from './UploadMediaComponent';
 
 interface IUnsplashImagePickerProps {
     initialPhotoSearchQuery?: string;
@@ -91,49 +94,65 @@ export default function UnsplashImagePicker({ initialPhotoSearchQuery = '', onPh
     }
 
     return (
-        <div className="ImagePicker items-center  rounded bg-white">
+        <div className="ImagePicker h-full  w-full items-center !rounded-lg bg-white">
             {initialLoading ? (
-                <div className="flex h-full w-full justify-center">
+                <div className="flex h-full w-full items-center justify-center">
                     <CircularProgressBar className="h-10 w-10" />
                 </div>
             ) : (
-                <div className="bg-white ">
-                    <div className="Picker relative h-full rounded">
-                        <Link href="https://unsplash.com/?utm_source=bettercollected&utm_medium=referral" target="_blank" referrerPolicy="no-referrer" className="flex items-center gap-4 bg-white px-4 pt-4 text-lg font-bold">
+                <div className="h-full w-full rounded-lg bg-white">
+                    <div className="Picker relative h-full">
+                        <div className="p2-new text-black-800 flex items-center gap-4 rounded-t-lg px-4 py-[10px]">Media</div>
+                        <Separator />
+                        <Tabs defaultValue="upload" className="h-full w-full">
+                            <TabsList className=" flex h-auto w-full gap-4 px-6 py-2">
+                                <TabsTrigger value="upload">Upload</TabsTrigger>
+                                <TabsTrigger value="unsplash">
+                                      <Link href="https://unsplash.com/?utm_source=bettercollected&utm_medium=referral" target="_blank" referrerPolicy="no-referrer" className="flex items-center gap-4 bg-white px-4 pt-4 text-lg font-bold">
                             <Unsplash /> Unsplash
                         </Link>
-                        <div className="bg-white p-4 shadow">
-                            <SearchBar
-                                onSearch={(query: string) => {
-                                    setPics([]);
-                                    fetchPhotos(1, query, true);
-                                }}
-                                query={query}
-                                setQuery={setQuery}
-                            />
-                        </div>
+                                </TabsTrigger>
+                            </TabsList>
+                            <Separator />
+                            <TabsContent value="upload" className="px-6 !pr-0 pt-4">
+                                <UploadMediaComponent updatePageImage={updatePageImage} />
+                            </TabsContent>
+                            <TabsContent value="unsplash" className="px-6 !pr-0 pt-4">
+                                <div className="mb-6 mr-2 ">
+                                    <SearchBar
+                                        initialPhotoSearchQuery={initialPhotoSearchQuery}
+                                        onSearch={(query: string) => {
+                                            setPics([]);
+                                            fetchPhotos(1, query, true);
+                                        }}
+                                        query={query}
+                                        setQuery={setQuery}
+                                    />
+                                </div>
 
-                        <PhotoList
-                            total={total}
-                            photoList={pics}
-                            isLoading={isLoading}
-                            isLoadingMore={isLoadingMore}
-                            loadMore={() => {
-                                query ? fetchPhotos(page + 1, query) : fetchPhotos(page + 1, 'forms');
-                            }}
-                            onPhotoSelect={async (photo: any) => {
-                                try {
-                                    if (photo) {
-                                        updatePageImage(photo.urls.raw);
-                                        setPhotoInLocalStorage(photo);
-                                        triggerUnsplashDownloadEndpoint(photo);
+                                <PhotoList
+                                    total={total}
+                                    photoList={pics}
+                                    isLoading={isLoading}
+                                    isLoadingMore={isLoadingMore}
+                                    loadMore={() => {
+                                        query ? fetchPhotos(page + 1, query) : fetchPhotos(page + 1, 'forms');
+                                    }}
+                                    onPhotoSelect={async (photo: any) => {
+                                        try {
+                                            if (photo) {
+                                                updatePageImage(photo.urls.raw);
+                                                        setPhotoInLocalStorage(photo);
+                                                triggerUnsplashDownloadEndpoint(photo);
                                         closeDialogModal();
                                     }
-                                } catch (error) {
-                                    console.log(error);
-                                }
-                            }}
-                        />
+                                        } catch (error) {
+                                            console.log(error);
+                                        }
+                                    }}
+                                />
+                            </TabsContent>
+                        </Tabs>
                     </div>
                 </div>
             )}
