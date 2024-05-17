dada0badeb2446d478c87755afd75a9579ee9761
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2024-05-17 15:33:30 +0545

    Remove typeform integration

--- integrations/typeform/typeform/app/exceptions/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Application implementation - exceptions."""
-from typeform.app.exceptions.http import HTTPException, http_exception_handler
-
-__all__ = ("HTTPException", "http_exception_handler")
