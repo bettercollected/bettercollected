ff0666cf5767c58663f062b171c6d598feebff87
sital999 <sitalnagarkoti123@gmail.com>
2024-05-17 10:10:15 +0545

    Add Skeleton as loading in Media_library

--- webapp/src/views/molecules/UnsplashImagePicker/MediaItem.tsx
+++ webapp/src/views/molecules/UnsplashImagePicker/MediaItem.tsx
@@ -6,16 +6,25 @@ import { MediaLibrary } from '@app/store/media-library/type';
 import { selectWorkspace } from '@app/store/workspaces/slice';
 import DeleteIcon from '@app/views/atoms/Icons/Delete';
 import Image from 'next/image';
+import { Skeleton } from '@app/shadcn/components/ui/skeleton';
+import { useEffect, useState } from 'react';
 
-const MediaList = ({ media, deletePhotoInLibrary, updatePageImage }: { media: MediaLibrary; deletePhotoInLibrary: (args: any) => void; updatePageImage: (args: any) => void }) => {
+const MediaItem = ({ media, deletePhotoInLibrary, updatePageImage, isAddPhotoInMediaLoading = false }: { media: MediaLibrary; deletePhotoInLibrary: (args: any) => void; updatePageImage: (args: any) => void; isAddPhotoInMediaLoading?: boolean }) => {
     const workspace = useAppSelector(selectWorkspace);
     const { closeDialogModal } = useDialogModal();
+    const [nextImageLoading, setNextImageLoading] = useState(false);
+    useEffect(() => {
+        if (!isAddPhotoInMediaLoading) {
+            setNextImageLoading(false);
+        }
+    }, [isAddPhotoInMediaLoading]);
 
     const handleDeleteMedia = (mediaId: string) => {
         deletePhotoInLibrary({ workspace_id: workspace.id, media_id: mediaId });
     };
+    if (isAddPhotoInMediaLoading || nextImageLoading) return <Skeleton className="bg-black-300 inset-0 h-[130px] w-full pb-10" />;
     return (
-        <div className="group relative cursor-pointer" key={media.mediaId}>
+        <div className="group relative h-auto cursor-pointer" key={media.mediaId}>
             <Image
                 className="cursor-pointer"
                 onClick={() => {
@@ -28,6 +37,7 @@ const MediaList = ({ media, deletePhotoInLibrary, updatePageImage }: { media: Me
                 width={0}
                 height={0}
                 sizes="100vw"
+                onLoad={() => isAddPhotoInMediaLoading && setNextImageLoading(true)}
                 style={{ width: '100%', height: 'auto' }}
             />
             <div className="invisible absolute right-2 top-2 z-[10000] group-hover:visible">
@@ -39,4 +49,4 @@ const MediaList = ({ media, deletePhotoInLibrary, updatePageImage }: { media: Me
     );
 };
 
-export default MediaList;
+export default MediaItem;
