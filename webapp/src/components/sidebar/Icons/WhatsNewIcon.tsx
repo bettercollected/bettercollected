080e52aa72c44c4a62dec07af9205fdb2deb3c18
sital999 <sitalnagarkoti123@gmail.com>
2024-05-30 13:50:11 +0545

    Add floatingHelpButton in dashboard

---
+++ webapp/src/components/sidebar/Icons/WhatsNewIcon.tsx
@@ -0,0 +1,9 @@
+const WhatsNewIcon = (props: React.SVGAttributes<any>) => {
+    return (
+        <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path d="M17.5 6.33594L10.8333 13.0026L7.5 9.66927L2.5 14.6693M17.5 6.33594H12.5M17.5 6.33594V11.3359" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    );
+};
+
+export default WhatsNewIcon;
