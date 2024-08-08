39469af9fcf456b178ecf5fde493897310678279
avinav baral <baralavinav2495@gmail.com>
2024-08-08 14:56:30 +0545

    Enhance Calendar and DatePicker Components; Update PhoneNumber and ScheduleModal

--- webapp/src/shadcn/components/ui/calendar.tsx
+++ webapp/src/shadcn/components/ui/calendar.tsx
@@ -20,8 +20,11 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
                 caption: 'flex justify-center pt-1 relative items-center',
                 caption_label: 'text-sm font-medium',
                 nav: 'space-x-1 flex items-center',
-                nav_button: cn(buttonVariants({ variant: 'outline' }), 'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'),
-                nav_button_previous: 'absolute left-1',
+                nav_button: cn('h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'),
+                nav_button_previous: cn(
+                    'absolute left-1 opacity-50',
+                    'hover:!opacity-50  disabled:cursor-not-allowed'
+                ),
                 nav_button_next: 'absolute right-1',
                 table: 'w-full border-collapse space-y-1 ',
                 head_row: 'flex',
