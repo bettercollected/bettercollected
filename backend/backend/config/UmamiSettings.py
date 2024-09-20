c2e331e67cc2ae00e31f45a2df7610e67b24a9e3
Prakash-coder <chaulagainprakash78@gmail.com>
2024-09-20 15:58:20 +0545

    Modify umami settings

--- backend/backend/config/UmamiSettings.py
+++ backend/backend/config/UmamiSettings.py
@@ -4,13 +4,9 @@ from pydantic import BaseSettings
 
 
 class UmamiSettings(BaseSettings):
-<<<<<<< HEAD
-    USERNAME: Optional[str] = "username"
-    PASSWORD: Optional[str] = "password"
-=======
+
     USERNAME: Optional[str] = ""
     PASSWORD: Optional[str] = ""
->>>>>>> 9a4d10ae (Integrate frontend with backend)
     WEBSITE_ID: Optional[str] = "305e6851-f6fc-4640-8cbf-6f749768d118"
     URL = "https://umami.sireto.io"
 
