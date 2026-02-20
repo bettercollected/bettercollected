656694135efb9cc514cc7f1548f3faa1ce1ba4c3
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2026-02-21 00:30:11 +0545

    Migrated to uv and upgraded deps

--- temporal/worker/workflows/form_import_workflow.py
+++ temporal/worker/workflows/form_import_workflow.py
@@ -1,4 +1,4 @@
-from pydantic.schema import timedelta
+from datetime import timedelta
 from temporalio import workflow
 from temporalio.common import RetryPolicy
 
@@ -10,5 +10,9 @@ from models.import_forms_params import ImportFormParams
 class ImportFormWorkflow:
     @workflow.run
     async def run(self, import_form_param: ImportFormParams):
-        await workflow.execute_activity(import_form, import_form_param, start_to_close_timeout=timedelta(minutes=5),
-                                        retry_policy=RetryPolicy(maximum_attempts=1))
+        await workflow.execute_activity(
+            import_form,
+            import_form_param,
+            start_to_close_timeout=timedelta(minutes=5),
+            retry_policy=RetryPolicy(maximum_attempts=1),
+        )
