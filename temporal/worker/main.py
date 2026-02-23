bc337138c23a607c926f13d9c405b5d7c273f20a
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2026-02-23 11:44:53 +0545

    Removed unused workflows

--- temporal/worker/main.py
+++ temporal/worker/main.py
@@ -10,25 +10,25 @@ from activities.delete_user import delete_user
 from activities.import_form import import_form
 from activities.save_preview import save_preview
 from settings.application import settings
-from workflows.form_import_workflow import ImportFormWorkflow
 from workflows.response_deletion_workflow import DeleteResponseWorkflow
-from workflows.save_template_preview import SaveTemplatePreview
 from workflows.user_deletion_workflow import DeleteUserWorkflow
 
 
 def get_random_string(length=20):
     # choose from all lowercase letter
     letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(length))
+    return "".join(random.choice(letters) for i in range(length))
 
 
 async def run_worker():
-    client = await Client.connect(settings.temporal_server_url, namespace=settings.namespace)
+    client = await Client.connect(
+        settings.temporal_server_url, namespace=settings.namespace
+    )
     worker = Worker(
         client,
         identity=get_random_string(),
         task_queue=settings.worker_queue,
-        workflows=[DeleteUserWorkflow, ImportFormWorkflow, DeleteResponseWorkflow, SaveTemplatePreview],
+        workflows=[DeleteUserWorkflow, DeleteResponseWorkflow],
         activities=[delete_user, import_form, delete_response, save_preview],
     )
     response = await worker.run()
