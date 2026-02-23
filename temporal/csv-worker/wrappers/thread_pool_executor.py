dbc5f4f9bd159532de8ea78d46b1a7ac48463bfa
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2026-02-23 11:45:16 +0545

    Removed unused worker for temporal

--- temporal/csv-worker/wrappers/thread_pool_executor.py
+++
@@ -1,5 +0,0 @@
-from concurrent.futures.thread import ThreadPoolExecutor
-
-from settings.application import settings
-
-thread_pool_executor = ThreadPoolExecutor(max_workers=settings.max_thread_pool_executors)
