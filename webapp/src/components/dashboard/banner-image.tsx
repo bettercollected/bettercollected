0459b73706153bdcbc242b9706a80e44a5c059fc
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2026-02-26 13:03:33 +0545

    Refactored banner and image component

--- webapp/src/components/dashboard/banner-image.tsx
+++ webapp/src/components/dashboard/banner-image.tsx
@@ -4,10 +4,8 @@ import { useTranslation } from 'next-i18next';
 
 import { Button } from '@app/shadcn/components/ui/button';
 import cn from 'classnames';
-import html2canvas from 'html2canvas';
 
 import { useToast } from '@app/shadcn/components/ui/use-toast';
-import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 
 import Image from '@app/components/ui/image';
 import { buttonConstant } from '@app/constants/locales/button';
@@ -19,16 +17,18 @@ import { setWorkspace } from '@app/store/workspaces/slice';
 export default function BannerImageComponent(props: { workspace: any; isFormCreator: boolean; className?: string }) {
     const { workspace, isFormCreator, className } = props;
     const { toast } = useToast();
-    const transformComponentRef = useRef(null);
     const [patchExistingWorkspace, { isLoading }] = usePatchExistingWorkspaceMutation();
     const [image, setImage] = useState('');
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
     const imageInputRef = useRef<HTMLInputElement>(null);
     const { t } = useTranslation();
 
     const dispatch = useAppDispatch();
     const onUploadFileChange = (e: any) => {
         if (!e.target.files.length) return;
-        setImage(URL.createObjectURL(e.target.files[0]));
+        const file = e.target.files[0];
+        setSelectedFile(file);
+        setImage(URL.createObjectURL(file));
     };
 
     const onClickFileUploadButton = () => {
@@ -39,27 +39,24 @@ export default function BannerImageComponent(props: { workspace: any; isFormCrea
 
     const onClickCancelButton = () => {
         setImage('');
+        setSelectedFile(null);
     };
 
-    const onClickFileSaveButton = (e: any) => {
-        const croppedImageDiv: any = document.getElementsByClassName('react-transform-wrapper')[0];
-        if (!croppedImageDiv) return;
-        html2canvas(croppedImageDiv).then((canvas: HTMLCanvasElement) => {
-            canvas.toBlob(async (blob: any) => {
-                const file = new File([blob], 'bannerimage.png', { type: blob.type });
-                const formData = new FormData();
-                formData.append('banner_image', file);
-                const response: any = await patchExistingWorkspace({ workspace_id: workspace.id, body: formData });
-                if (response.error) {
-                    toast({ description: response.error.data || t(toastMessage.somethingWentWrong).toString(), variant: 'destructive' });
-                }
-                if (response.data) {
-                    toast({ description: t(toastMessage.workspaceUpdate).toString() });
-                    setImage('');
-                    dispatch(setWorkspace(response.data));
-                }
-            });
-        });
+    const onClickFileSaveButton = async () => {
+        if (!selectedFile) return;
+
+        const formData = new FormData();
+        formData.append('banner_image', selectedFile);
+        const response: any = await patchExistingWorkspace({ workspace_id: workspace.id, body: formData });
+        if (response.error) {
+            toast({ description: response.error.data || t(toastMessage.somethingWentWrong).toString(), variant: 'destructive' });
+        }
+        if (response.data) {
+            toast({ description: t(toastMessage.workspaceUpdate).toString() });
+            setImage('');
+            setSelectedFile(null);
+            dispatch(setWorkspace(response.data));
+        }
     };
 
     const getUpdateOptionsClassName = () => {
@@ -72,23 +69,7 @@ export default function BannerImageComponent(props: { workspace: any; isFormCrea
     return (
         <div className={cn('relative w-full bannerdiv rounded-t-xl overflow-hidden max-w-[540px] md:max-w-[320px]  aspect-banner', className)}>
             {!!image ? (
-                <TransformWrapper centerOnInit ref={transformComponentRef}>
-                    {({ resetTransform }) => {
-                        return (
-                            <TransformComponent
-                                wrapperStyle={{
-                                    maxHeight: '100%',
-                                    maxWidth: '100%',
-                                    height: '100%',
-                                    width: '100%',
-                                    cursor: 'grabbing'
-                                }}
-                            >
-                                <img style={{ width: '100%', height: '100%', objectFit: 'cover' }} src={image} alt="test" />
-                            </TransformComponent>
-                        );
-                    }}
-                </TransformWrapper>
+                <img style={{ width: '100%', height: '100%', objectFit: 'cover' }} src={image} alt="test" />
             ) : (
                 <>
                     {!!workspace.bannerImage ? (
@@ -98,7 +79,7 @@ export default function BannerImageComponent(props: { workspace: any; isFormCrea
                             <div className="p2-new ml-10 !text-black-700">Add banner image</div>
                         </div>
                     )}
-                    <input ref={imageInputRef} data-testid="file-upload" type="file" accept="image/*" className="hidden" onChange={onUploadFileChange} />
+                    <input ref={imageInputRef} data-testid="file-upload" type="file" accept="image/*" className="hidden" onClick={(event: any) => (event.target.value = null)} onChange={onUploadFileChange} />
                 </>
             )}
             {isFormCreator && (
--- webapp/src/components/dashboard/profile-image.tsx
+++ webapp/src/components/dashboard/profile-image.tsx
@@ -2,9 +2,9 @@ import { useRef, useState } from 'react';
 
 import { useTranslation } from 'next-i18next';
 
-import AvatarEditor from 'react-avatar-editor';
-
+import { buttonConstant } from '@app/constants/locales/button';
 import { toastMessage } from '@app/constants/locales/toast-message';
+import { Button } from '@app/shadcn/components/ui/button';
 import { useToast } from '@app/shadcn/components/ui/use-toast';
 import { useAppDispatch } from '@app/store/hooks';
 import { usePatchExistingWorkspaceMutation } from '@app/store/workspaces/api';
@@ -17,38 +17,47 @@ export default function ProfileImageComponent(props: { workspace: any; isFormCre
     const { t } = useTranslation();
     const { toast } = useToast();
     const [uploadImage, setUploadImage] = useState(workspace.profileImage);
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
     const profileInputRef = useRef<HTMLInputElement>(null);
-    const profileEditorRef = useRef<AvatarEditor>(null);
     const [patchExistingWorkspace, { isLoading }] = usePatchExistingWorkspaceMutation();
     const dispatch = useAppDispatch();
 
-    const updateProfileHandler = async () => {
-        if (!!profileEditorRef.current) {
-            const dataUrl = profileEditorRef.current.getImage().toDataURL();
+    const onUploadFileChange = (e: any) => {
+        if (!!e.target.files.length) {
+            const file = e.target.files[0];
+            setSelectedFile(file);
+            setUploadImage(URL.createObjectURL(file));
+        }
+    };
 
-            const result = await fetch(dataUrl);
-            const blob = await result.blob();
-            const file = new File([blob], 'profileImage.png', { type: blob.type });
-            const formData = new FormData();
-            formData.append('profile_image', file);
+    const onClickSaveButton = async () => {
+        if (!selectedFile) return;
 
-            const response: any = await patchExistingWorkspace({ workspace_id: workspace.id, body: formData });
-            if (response.error) {
-                toast({ description: t(toastMessage.somethingWentWrong).toString(), variant: 'destructive' });
-            }
-            if (response.data) {
-                toast({ description: t(toastMessage.workspaceUpdate).toString() });
+        const formData = new FormData();
+        formData.append('profile_image', selectedFile);
 
-                dispatch(setWorkspace(response.data));
-                setUploadImage(response.data.profileImage);
-                // await router.push(router.asPath, undefined);
-            }
-        } else return;
+        const response: any = await patchExistingWorkspace({ workspace_id: workspace.id, body: formData });
+        if (response.error) {
+            toast({ description: t(toastMessage.somethingWentWrong).toString(), variant: 'destructive' });
+        }
+        if (response.data) {
+            toast({ description: t(toastMessage.workspaceUpdate).toString() });
+
+            dispatch(setWorkspace(response.data));
+            setUploadImage(response.data.profileImage);
+            setSelectedFile(null);
+            // await router.push(router.asPath, undefined);
+        }
+    };
+
+    const onClickCancelButton = () => {
+        setUploadImage(workspace.profileImage);
+        setSelectedFile(null);
     };
 
     return (
         <div className={props?.className ?? ''}>
-            <>
+            <div className="flex flex-col items-center">
                 <div
                     onClick={() => {
                         if (isFormCreator) {
@@ -58,9 +67,19 @@ export default function ProfileImageComponent(props: { workspace: any; isFormCre
                     className={`w-min ${isFormCreator ? 'cursor-pointer' : ''} `}
                 >
                     <AuthAccountProfileImage image={uploadImage} name={workspace.title} size={size ? size : 163} typography="h2" />
-                    <input data-testid="file-upload-profile" type="file" accept="image/*" ref={profileInputRef} className="hidden" onChange={updateProfileHandler} />
+                    <input data-testid="file-upload-profile" type="file" accept="image/*" ref={profileInputRef} className="hidden" onClick={(event: any) => (event.target.value = null)} onChange={onUploadFileChange} />
                 </div>
-            </>
+                {selectedFile && isFormCreator && (
+                    <div className="flex justify-between mt-2">
+                        <Button className="!text-white flex !bg-black-600 hover:!bg-black-700 mr-2" onClick={onClickCancelButton}>
+                            {t(buttonConstant.cancel)}
+                        </Button>
+                        <Button isLoading={isLoading} onClick={onClickSaveButton}>
+                            {isLoading ? t(buttonConstant.saving) : t(buttonConstant.save)}
+                        </Button>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
