fe78bc8627710586b89de93f047f2d7757aec698
ankit.sapkota555 <ankit.sapkota555@gmail.com>
2026-02-27 13:09:34 +0545

    Fixed returning custom domain ip in public config

--- webapp/src/app/api/config/route.ts
+++ webapp/src/app/api/config/route.ts
@@ -3,7 +3,8 @@ const config = {
     GOOGLE_PICKER_API_KEY: process.env.GOOGLE_PICKER_API_KEY,
     DASHBOARD_DOMAIN: process.env.DASHBOARD_DOMAIN || 'admin.bettercollected.com',
     FORM_DOMAIN: process.env.FORM_DOMAIN || 'forms.bettercollected.com',
-    HTTP_SCHEME: process.env.HTTP_SCHEME || 'https://'
+    HTTP_SCHEME: process.env.HTTP_SCHEME || 'https://',
+    CUSTOM_DOMAIN_IP: process.env.CUSTOM_DOMAIN_IP || '135.181.40.62'
 };
 
 export type PublicConfigType = typeof config;
